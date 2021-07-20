f56acfe6c560ad36723ae6f97c78ab17d31e12cf
wchambers <1499870+wchamber@users.noreply.github.com>
2021-07-20 11:29:48 -0400

    PR review changes.

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # tanagra
 Repo for the Tanagra service being developed by the All of Us DRC
+
+# Development
+To get started, run `./gradlew build` to build and test.
+
+## Local Server
+`./gradlew bootRun` starts a local server
+on localhost:8080. See [http://localhost:8080/swagger-ui.html](http://localhost:8080/swagger-ui.html)
+for the Swagger API page.
+
+## Adding dependencies
+We use [Gradle dependency locking](https://docs.gradle.org/current/userguide/dependency_locking.html)
+for building with deterministic dependencies. When adding a new dependency, use
+
+`./gradlew dependencies --write-locks`
+
+to regenerate the `gradle.lockfile` with the added dependencies. If this is not done, building will
+error about
+> Could not resolve all files for configuration...
+> Resolved foo.bar:lib:version which is not part of the dependency lock state
--- src/main/java/bio/terra/tanagra/app/controller/UnauthenticatedApiController.java
+++ src/main/java/bio/terra/tanagra/app/controller/UnauthenticatedApiController.java
@@ -1,9 +1,6 @@
 package bio.terra.tanagra.app.controller;
 
 import bio.terra.tanagra.generated.controller.UnauthenticatedApi;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.util.Optional;
-import javax.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -14,17 +11,5 @@ public class UnauthenticatedApiController implements UnauthenticatedApi {
   @Override
   public ResponseEntity<Void> serviceStatus() {
     return new ResponseEntity<>(HttpStatus.OK);
-  }
-
-  /** Required if using Swagger-CodeGen, but actually we don't need this. */
-  @Override
-  public Optional<ObjectMapper> getObjectMapper() {
-    return Optional.empty();
-  }
-
-  /** Required if using Swagger-CodeGen, but actually we don't need this. */
-  @Override
-  public Optional<HttpServletRequest> getRequest() {
-    return Optional.empty();
   }
 }
