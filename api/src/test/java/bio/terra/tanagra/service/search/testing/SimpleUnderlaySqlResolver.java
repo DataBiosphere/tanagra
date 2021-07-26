7856a2b164ce57f35e2230975e038c3bade8e17a
wchamber <1499870+wchamber@users.noreply.github.com>
2021-07-26 17:01:56 -0400

    Add initial Query construct & SQL generation (#5)

--- api/src/test/java/bio/terra/tanagra/service/search/testing/SimpleUnderlaySqlResolver.java
+++ api/src/test/java/bio/terra/tanagra/service/search/testing/SimpleUnderlaySqlResolver.java
@@ -1,10 +1,20 @@
 package bio.terra.tanagra.service.search.testing;
 
 import bio.terra.tanagra.service.search.Attribute;
+import bio.terra.tanagra.service.search.Entity;
 import bio.terra.tanagra.service.search.UnderlaySqlResolver;
 
 /** A simple {@link UnderlaySqlResolver} for testing. */
 public class SimpleUnderlaySqlResolver implements UnderlaySqlResolver {
+
+  /**
+   * Resolve all entities as directly related to '{underlay name}.{entity name}', as if there were a
+   * direct correspondence between each entity and a dataset.table for it.
+   */
+  @Override
+  public String resolveTable(Entity entity) {
+    return String.format("%s.%s", entity.underlay(), entity.name());
+  }
 
   /**
    * Resolve all attributes as directly related to '{entity name}.{attribute name}', as if there
