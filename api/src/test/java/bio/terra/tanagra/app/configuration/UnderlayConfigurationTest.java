ad132222c4077b95f2634e11f4427d1aa92293f7
wchambers <1499870+wchamber@users.noreply.github.com>
2021-08-05 12:29:49 -0400

    Add a underlay yaml config property. Fix up dependencies with the inclusion of java-protobuf-utils. Unify some duplicate version strings on dependencies.

---
+++ api/src/test/java/bio/terra/tanagra/app/configuration/UnderlayConfigurationTest.java
@@ -0,0 +1,25 @@
+package bio.terra.tanagra.app.configuration;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import bio.terra.tanagra.proto.underlay.Underlay;
+import com.google.common.collect.ImmutableList;
+import java.util.List;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("unit")
+public class UnderlayConfigurationTest {
+  @Test
+  void prototextAndYamlUnderlayEquivalence() throws Exception {
+    UnderlayConfiguration config = new UnderlayConfiguration();
+    config.setUnderlayPrototextFiles(ImmutableList.of("underlays/nautical.pbtext"));
+    config.setUnderlayYamlFiles(ImmutableList.of("underlays/nautical.yaml"));
+
+    List<Underlay> underlays = config.getUnderlays();
+    assertThat(underlays, Matchers.hasSize(2));
+    assertEquals(underlays.get(0), underlays.get(1));
+  }
+}
