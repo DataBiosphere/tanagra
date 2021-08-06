e1b6644b6ac21cc499b5bbf484df8cad99c52af7
wchambers <1499870+wchamber@users.noreply.github.com>
2021-08-06 15:37:53 -0400

    Wip on services & controllers

--- api/src/main/java/bio/terra/tanagra/app/controller/EntitiesFiltersApiController.java
+++ api/src/main/java/bio/terra/tanagra/app/controller/EntitiesFiltersApiController.java
@@ -3,7 +3,9 @@ package bio.terra.tanagra.app.controller;
 import bio.terra.tanagra.generated.controller.EntitiesFiltersApi;
 import bio.terra.tanagra.generated.model.ApiEntityFilter;
 import bio.terra.tanagra.generated.model.ApiSqlQuery;
+import bio.terra.tanagra.service.query.api.ApiConversionService;
 import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
@@ -11,9 +13,18 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class EntitiesFiltersApiController implements EntitiesFiltersApi {
 
+  private final ApiConversionService apiConversionService;
+
+  @Autowired
+  public EntitiesFiltersApiController(ApiConversionService apiConversionService) {
+    this.apiConversionService = apiConversionService;
+  }
+
   @Override
   public ResponseEntity<ApiSqlQuery> generateSqlQuery(
       String underlayName, String entityName, @Valid ApiEntityFilter body) {
+    // TODO authorization check.
+
     return null;
   }
 }
--- api/src/main/java/bio/terra/tanagra/service/query/QueryService.java
+++ api/src/main/java/bio/terra/tanagra/service/query/QueryService.java
@@ -1,2 +1,17 @@
-package bio.terra.tanagra.service.query;public class QueryService {
+package bio.terra.tanagra.service.query;
+
+import bio.terra.tanagra.service.underlay.UnderlayService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+// DO NOT SUBMIT comment me.
+@Component
+public class QueryService {
+
+  private final UnderlayService underlayService;
+
+  @Autowired
+  public QueryService(UnderlayService underlayService) {
+    this.underlayService = underlayService;
+  }
 }
