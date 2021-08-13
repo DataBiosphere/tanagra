8a40738eca601a40c3562b490050c9f9d4b493cc
wchambers <1499870+wchamber@users.noreply.github.com>
2021-08-13 11:14:33 -0400

    tweak variable naming comment

--- api/src/main/java/bio/terra/tanagra/service/search/Variable.java
+++ api/src/main/java/bio/terra/tanagra/service/search/Variable.java
@@ -5,14 +5,16 @@ import com.google.auto.value.AutoValue;
 /**
  * A variable to bound to an entity to allow referring to multiple instances of the same entity.
  *
- * <p>A variable name must be lowercase a-z letters, numbers, and underscores, but must start with a
- * letter.
+ * <p>A variable name must a be letters, numbers, and underscores, but must start with a
+ * lower case letter.
  */
 @AutoValue
 public abstract class Variable {
   public abstract String name();
 
   public static Variable create(String name) {
+    // Use NameUtils for consistency, even though Variables are only ever client generated.
+    // We could consisder having a different naming validation as needed.
     NameUtils.checkName(name, "Variable name");
     return new AutoValue_Variable(name);
   }
