fa838e17608172b92bd81becbcf10aca0d695c01
wchambers <1499870+wchamber@users.noreply.github.com>
2021-08-16 14:27:44 -0400

    tweaks

--- api/src/main/java/bio/terra/tanagra/common/Paginator.java
+++ api/src/main/java/bio/terra/tanagra/common/Paginator.java
@@ -18,7 +18,7 @@ public final class Paginator<T> {
    * Returns a {@link Page} for the results.
    *
    * @param allResults the sorted list of all results being paginated.
-   * @param pageSize the maximumn number of results to have per page
+   * @param pageSize the maximum number of results to have per page.
    * @param pageToken the token of the page to retrieve. If null or empty, the first page is
    *     retrieved.
    */
@@ -40,13 +40,14 @@ public final class Paginator<T> {
 
   public static <T> Page<T> getPageInternal(List<T> allResults, PageSizeOffset pageSizeOffset) {
     Preconditions.checkArgument(
-        pageSizeOffset.getOffest() < allResults.size() || allResults.isEmpty(),
+        pageSizeOffset.getOffest() < allResults.size() || pageSizeOffset.getOffest() == 0,
         "page offset out of bounds.");
     int offset = pageSizeOffset.getOffest();
     int pageLimit = pageSizeOffset.getOffest() + pageSizeOffset.getPageSize();
     if (pageLimit >= allResults.size()) {
       List<T> results = allResults.subList(offset, allResults.size());
-      return Page.createLastPage(results);
+      // This page includes the last of the results. Use an empty string for the next page token.
+      return Page.create(results, "");
     }
     PageSizeOffset nextPageSizeOffset = pageSizeOffset.toBuilder().setOffest(pageLimit).build();
     List<T> results = allResults.subList(offset, pageLimit);
@@ -76,10 +77,6 @@ public final class Paginator<T> {
 
     public static <T> Page<T> create(List<T> results, String nextPageToken) {
       return new AutoValue_Paginator_Page<>(ImmutableList.copyOf(results), nextPageToken);
-    }
-
-    public static <T> Page<T> createLastPage(List<T> results) {
-      return create(results, "");
     }
   }
 }
--- api/src/main/java/bio/terra/tanagra/service/query/SchemaService.java
+++
@@ -1,7 +0,0 @@
-package bio.terra.tanagra.service.query;
-
-import org.springframework.stereotype.Service;
-
-/** A service for retrieving schemas of Tanagra query objects. */
-@Service
-public class SchemaService {}
