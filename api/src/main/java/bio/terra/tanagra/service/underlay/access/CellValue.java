9426562113f6c6345bf2eeff6d3026bd6fd3e8b9
wchambers <1499870+wchamber@users.noreply.github.com>
2021-09-02 19:14:21 -0400

    dirty row/cell interfaces

---
+++ api/src/main/java/bio/terra/tanagra/service/underlay/access/CellValue.java
@@ -0,0 +1,38 @@
+package bio.terra.tanagra.service.underlay.access;
+
+/** An interface for the value of a cell within a row within a result table.
+ * <p>This interface allows us to read data from different databases in a simple but uniform way.
+ * Different database types should implement this for returning values. */
+public interface CellValue {
+
+  /** Returns whether the cell's value is null. */
+  boolean isNull();
+
+  /**
+   Returns this field's value as a long.
+   * @throws ClassCastException if the cell's value is not a long
+   * @throws NullPointerException if {@link #isNull()} returns {@code true}
+   */
+  long getLong();
+
+  /**
+   Returns this field's value as a string.
+   * @throws ClassCastException if the cell's value is not a string
+   * @throws NullPointerException if {@link #isNull()} returns {@code true}
+   */
+  String getString();
+
+  /**
+   Returns this field's value as a string.
+   * @throws ClassCastException if the cell's value is not a string
+   * @throws NullPointerException if {@link #isNull()} returns {@code true}
+   */
+  boolean getBoolean();
+}
+
+// DO NOT SUBMIT
+// For BQ pagination, use job id + next page token to retrieve job -> table results.
+// For Postgres, we need to use offset + limit. That means encoding it in the original query construction as pagination,
+// or rewriting the query during execution.
+// Thought: Add pagination info as input to the Query as optional. For postgres specialization, use
+// that to append the ORDER BY %s OFFSET %s LIMIT %s
