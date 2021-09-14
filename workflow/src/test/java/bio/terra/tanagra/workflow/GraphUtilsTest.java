4de47add1180dfbd4ce2e475e581aa976186da29
wchambers <1499870+wchamber@users.noreply.github.com>
2021-09-14 17:25:51 -0400

    Add tests & comments for transitiveClosure.

---
+++ workflow/src/test/java/bio/terra/tanagra/workflow/GraphUtilsTest.java
@@ -0,0 +1,113 @@
+package bio.terra.tanagra.workflow;
+
+import com.google.common.collect.Multimap;
+import com.google.common.collect.MultimapBuilder;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.apache.beam.sdk.testing.PAssert;
+import org.apache.beam.sdk.testing.TestPipeline;
+import org.apache.beam.sdk.transforms.Create;
+import org.apache.beam.sdk.transforms.Distinct;
+import org.apache.beam.sdk.values.KV;
+import org.apache.beam.sdk.values.PCollection;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class GraphUtilsTest {
+
+  @Rule public final transient TestPipeline pipeline = TestPipeline.create();
+
+  /**
+   * Runs a test pipeline with the input edges and {@link GraphUtils#transitiveClosure}, asserting
+   * the expected transitive edges are produced.
+   */
+  <T> void runTransitiveClosureAndAssert(
+      Multimap<T, T> edges, Multimap<T, T> expectedEdges, int maxPathLength) {
+    PCollection<KV<T, T>> transitiveEdges =
+        GraphUtils.transitiveClosure(pipeline.apply(Create.of(convertToKvs(edges))), maxPathLength)
+            .apply(Distinct.create()); // The transitiveClosure may output duplicates.
+    PAssert.that(transitiveEdges).containsInAnyOrder(convertToKvs(expectedEdges));
+    pipeline.run().waitUntilFinish();
+  }
+
+  /** Convert a multimap to an equivalent list of KVs. */
+  private static <T> List<KV<T, T>> convertToKvs(Multimap<T, T> multimap) {
+    return multimap.entries().stream()
+        .map(entry -> KV.of(entry.getKey(), entry.getValue()))
+        .collect(Collectors.toList());
+  }
+
+  @Test
+  public void transitiveClosureMultipleHeadsAndTails() {
+    // Ascii art of the graph described by edges directed left to right.
+    //  /B-D\ /F
+    // A     E
+    //  \ C / \G
+    Multimap<String, String> edges = MultimapBuilder.hashKeys().arrayListValues().build();
+    edges.putAll("A", List.of("B", "C"));
+    edges.putAll("B", List.of("D"));
+    edges.putAll("C", List.of("E"));
+    edges.putAll("D", List.of("E"));
+    edges.putAll("E", List.of("F", "G"));
+
+    Multimap<String, String> expectedEdges = MultimapBuilder.hashKeys().arrayListValues().build();
+    expectedEdges.putAll("A", List.of("B", "C", "D", "E", "F", "G"));
+    expectedEdges.putAll("B", List.of("D", "E", "F", "G"));
+    expectedEdges.putAll("C", List.of("E", "F", "G"));
+    expectedEdges.putAll("D", List.of("E", "F", "G"));
+    expectedEdges.putAll("E", List.of("F", "G"));
+
+    runTransitiveClosureAndAssert(edges, expectedEdges, 4);
+  }
+
+  @Test
+  public void transitiveClosureLongChain() {
+    // Ascii art of the graph described by edges directed left to right.
+    // A-B-C-D-E-F-G-H-I-J-K
+    //                    \L
+    Multimap<String, String> edges = MultimapBuilder.hashKeys().arrayListValues().build();
+    edges.put("A", "B");
+    edges.put("B", "C");
+    edges.put("C", "D");
+    edges.put("D", "E");
+    edges.put("E", "F");
+    edges.put("F", "G");
+    edges.put("G", "H");
+    edges.put("H", "I");
+    edges.put("I", "J");
+    edges.putAll("J", List.of("K", "L"));
+
+    Multimap<String, String> expectedEdges = MultimapBuilder.hashKeys().arrayListValues().build();
+    expectedEdges.putAll("A", List.of("B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"));
+    expectedEdges.putAll("B", List.of("C", "D", "E", "F", "G", "H", "I", "J", "K", "L"));
+    expectedEdges.putAll("C", List.of("D", "E", "F", "G", "H", "I", "J", "K", "L"));
+    expectedEdges.putAll("D", List.of("E", "F", "G", "H", "I", "J", "K", "L"));
+    expectedEdges.putAll("E", List.of("F", "G", "H", "I", "J", "K", "L"));
+    expectedEdges.putAll("F", List.of("G", "H", "I", "J", "K", "L"));
+    expectedEdges.putAll("G", List.of("H", "I", "J", "K", "L"));
+    expectedEdges.putAll("H", List.of("I", "J", "K", "L"));
+    expectedEdges.putAll("I", List.of("J", "K", "L"));
+    expectedEdges.putAll("J", List.of("K", "L"));
+
+    runTransitiveClosureAndAssert(edges, expectedEdges, 10);
+  }
+
+  @Test
+  public void transitiveClosureLargePathLengthOk() {
+    // Ascii art of the graph described by edges directed left to right.
+    // A-B-C-D-E
+    Multimap<String, String> edges = MultimapBuilder.hashKeys().arrayListValues().build();
+    edges.put("A", "B");
+    edges.put("B", "C");
+    edges.put("C", "D");
+    edges.put("D", "E");
+    Multimap<String, String> expectedEdges = MultimapBuilder.hashKeys().arrayListValues().build();
+    expectedEdges.putAll("A", List.of("B", "C", "D", "E"));
+    expectedEdges.putAll("B", List.of("C", "D", "E"));
+    expectedEdges.putAll("C", List.of("D", "E"));
+    expectedEdges.putAll("D", List.of("E"));
+
+    // Not an error to have maxPathLength be much greater than the longest path, just extra work.
+    runTransitiveClosureAndAssert(edges, expectedEdges, 64);
+  }
+}
