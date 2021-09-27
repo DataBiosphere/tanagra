19a0c7e1705a655b27e952c09a6bd761a797cd1c
wchambers <1499870+wchamber@users.noreply.github.com>
2021-09-27 10:04:38 -0400

    wip connecting

--- workflow/src/main/java/bio/terra/tanagra/workflow/CopyBqTable.java
+++ workflow/src/main/java/bio/terra/tanagra/workflow/CopyBqTable.java
@@ -6,18 +6,27 @@ import bio.terra.tanagra.proto.underlay.Table;
 import bio.terra.tanagra.proto.underlay.Underlay;
 import bio.terra.tanagra.workflow.FlattenHierarchy.FlattenHierarchyOptions;
 import com.google.api.services.bigquery.model.TableReference;
+import com.google.api.services.bigquery.model.TableRow;
+import com.google.common.collect.ImmutableList;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import javax.sql.DataSource;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
 import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO.TypedRead.Method;
 import org.apache.beam.sdk.io.gcp.bigquery.BigQueryOptions;
+import org.apache.beam.sdk.io.jdbc.JdbcIO;
+import org.apache.beam.sdk.io.jdbc.JdbcIO.DataSourceConfiguration;
 import org.apache.beam.sdk.options.Default;
 import org.apache.beam.sdk.options.Description;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.SerializableFunction;
 
 // DO NOT SUBMIT comment me
 public class CopyBqTable {
@@ -26,38 +35,21 @@ public class CopyBqTable {
   /** Options supported by {@link CopyBqTable}. */
   public interface CopyBqTableOptions extends BigQueryOptions {
     @Description(
-        "Path to a BigQuery standard SQL query file to execute to retrieve the hierarchy to be"
-            + "flattened. The result of the query should have two columns, (parent, child) that"
-            + "defines all of the direct relationships of the hierarchy to be flattened.")
-    String getHierarchyQuery();
+        "The name of the CloudSQL database where tables are being copied."
+    )
+    String cloudSqlDatabaseName();
 
-    void setHierarchyQuery(String query);
+    void setCloudSqlDatabaseName(String databaseName);
 
-    @Description(
-        "The maximum depth of ancestors present than the hierarchy. This may be larger "
-            + "than the actual max depth, but if it is smaller the resulting table will be incomplete")
-    @Default.Integer(64)
-    int getMaxHierarchyDepth();
+    @Description("The username for connecting to the CloudSQL database.")
+    String cloudSqlUserName();
 
-    void setMaxHierarchyDepth(int val);
+    void setCloudSqlUserName(String userName);
 
-    @Description(
-        "The  \"[project_id]:[dataset_id].[table_id]\" specification of the ancestor table to create.")
-    String getOutputBigQueryTable();
+    @Description("The full name of the CloudSQL instance where tables are being copied to: {projectId}:{region}:{instanceId}")
+    String cloudSqlInstanceName();
 
-    void setOutputBigQueryTable(String tableRef);
-
-    @Description("What to name the ancestor column on the output BigQuery table.")
-    @Default.String("ancestor")
-    String getOutputAncestorColumn();
-
-    void setOutputAncestorColumn(String ancestorColumn);
-
-    @Description("What to name the descendant column on the output BigQuery table.")
-    @Default.String("descendant")
-    String getOutputDescendantColumn();
-
-    void setOutputDescendantColumn(String descendantColumn);
+    void setCloudSqlInstanceName(String instanceName);
   }
 
 
@@ -74,14 +66,72 @@ public class CopyBqTable {
         .setDatasetId(dataset.getBigQueryDataset().getProjectId())
         .setTableId(table.getName());
 
+    // Don't encode sensitive information in options as they are written in job logs.
+    String dbPassword = System.getenv("POSTGRES_PWD");
+    if (dbPassword.isEmpty()) {
+      // do not submit variable the env var.
+      throw new IllegalArgumentException("database password must be specified with environment variable POSTGRES_PASS");
+    }
+
+    // reWriteBatchedInserts=true DO NOT SUBMIT
+    String connectionUrl=
+        String.format("jdbc:postgresql://google/%s"
+            + "?cloudSqlInstance=%s"
+            + "&socketFactory=com.google.cloud.sql.postgres.SocketFactory"
+            + "&user=%s"
+            + "&password=%s", options.cloudSqlDatabaseName(),
+            options.cloudSqlInstanceName(), options.cloudSqlUserName(), dbPassword
+
+            );
+    SerializableFunction<Void, DataSource> dataSourceProvider = JdbcIO.PoolableDataSourceProvider.of(DataSourceConfiguration.create("org.postgresql.Driver",connectionUrl)
+        // DO NOT SUBMIT is withUsername withPassword needed?
+        .withUsername(options.cloudSqlUserName())
+        .withPassword(dbPassword));
 
     Pipeline pipeline = Pipeline.create(options);
+
+    ImmutableList<String> columnNames = table.getColumnsList().stream().map(Column::getName).collect(
+        ImmutableList.toImmutableList());
+    // Do a postgres upsert so that this is tolerates Beam retries.
+    // INSERT INTO table (column0, ..., columnN) VALUES (?, ..., ?) ON CONFLICT DO UPDATE
+    String statement = String.format("INSERT INTO %s (%s) VALUES( %s) ON CONFLICT DO UPDATE",
+        table.getName(), String.join(", ", columnNames), String.join(", ", Collections.nCopies(columnNames.size(), "?")));
+
+    // DO NOT SUBMIT create table in postgres
 
     pipeline.apply(
         BigQueryIO.readTableRows()
             .from(tableReference)
             .withMethod(Method.DIRECT_READ)
             .withSelectedFields(table.getColumnsList().stream().map(Column::getName).collect(
-                Collectors.toList())));
+                Collectors.toList())))
+    .apply(JdbcIO.<TableRow>write()
+        .withDataSourceProviderFn(dataSourceProvider)
+        .withStatement(statement)
+        .withPreparedStatementSetter(new JdbcIO.PreparedStatementSetter<TableRow>() {
+          public void setParameters(TableRow tableRow, PreparedStatement query)
+              throws SQLException {
+            for (int i = 0; i < table.getColumnsList().size(); ++i) {
+              Column column = table.getColumns(i);
+              String val = tableRow.get(column.getName()).toString();
+              switch (column.getDataType()) {
+                case FLOAT:
+                  query.setFloat(i, Float.parseFloat(val));
+                  break;
+                case INT64:
+                  query.setLong(i, Long.parseLong(val));
+                  break;
+                case STRING:
+                  query.setString(i, val);
+                  break;
+                default:
+                  throw new UnsupportedOperationException(
+                      String.format("Unknown Column DataType %s", column.getDataType()));
+              }
+            }
+          }
+        })
+    );
+    // TODO create additional indexes on new table.
   }
 }
