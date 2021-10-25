9e46363f5d22925cb6f62b009420b2249b74ee91
Timothy Jennison <tjennison@google.com>
2021-10-25 12:25:48 -0400

    Remove initial test criteria

--- ui/src/app.tsx
+++ ui/src/app.tsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import { HashRouter, Route, Switch } from "react-router-dom";
 import "./app.css";
-import { ConceptCriteria } from "./criteria/concept";
-import { Dataset, Group, GroupKind } from "./dataset";
+import { Dataset } from "./dataset";
 import { DatasetUpdaterProvider } from "./datasetUpdaterContext";
 import Edit from "./edit";
 import Overview from "./overview";
@@ -17,28 +16,6 @@ export default function App(props: AppProps) {
     new Dataset(
       props.underlayName,
       props.entityName,
-      [
-        new Group(GroupKind.Included, [
-          new ConceptCriteria(
-            "Contains Conditions Code",
-            "condition_occurrence"
-          ),
-          new ConceptCriteria(
-            "Contains Conditions Code 2",
-            "condition_occurrence"
-          ),
-        ]),
-        new Group(GroupKind.Included, [
-          new ConceptCriteria(
-            "Contains Conditions Code 3",
-            "condition_occurrence"
-          ),
-          new ConceptCriteria(
-            "Contains Conditions Code 4",
-            "condition_occurrence"
-          ),
-        ]),
-      ],
       // TODO(tjennison): Populate from an actual source.
       [
         "person_id",
