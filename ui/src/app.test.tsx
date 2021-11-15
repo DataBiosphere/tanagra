87b66ab1529666dd5db8566907c13ce5d0f38aa7
Timothy Jennison <tjennison@google.com>
2021-11-15 11:40:03 -0500

    Updated integration test to match current code

--- ui/src/app.test.tsx
+++ ui/src/app.test.tsx
@@ -1,9 +1,10 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import React from "react";
 import App from "./app";
 
-test("renders included partipants heading", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/included participants/i);
-  expect(linkElement).toBeInTheDocument();
+test("render included datasets heading", async () => {
+  render(<App underlayNames={["omop_test"]} />);
+  await waitFor(() =>
+    expect(screen.getByText(/datasets/i)).toBeInTheDocument()
+  );
 });
