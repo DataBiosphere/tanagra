097e2fbdc57c2d74f397d9821104cc7f908d05fb
Tim Jennison <32747364+tjennison-google@users.noreply.github.com>
2021-11-30 07:54:20 -0500

    Add test for menu component (#79)

---
+++ ui/src/menu.test.tsx
@@ -0,0 +1,34 @@
+import Button from "@mui/material/Button";
+import MenuItem from "@mui/material/MenuItem";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import React from "react";
+import { useMenu } from "./menu";
+
+function MenuButton(props: { onMenuItem: () => void }) {
+  const [menu, show] = useMenu({
+    children: <MenuItem onClick={props.onMenuItem}>menu-item</MenuItem>,
+  });
+  return (
+    <>
+      <Button onClick={show}>show</Button>
+      {menu}
+    </>
+  );
+}
+
+test("show/hide menu", async () => {
+  const onMenuItem = jest.fn();
+  render(<MenuButton onMenuItem={onMenuItem} />);
+
+  userEvent.click(screen.getByRole("button"));
+
+  userEvent.click(screen.getByText("menu-item"));
+  expect(onMenuItem).toHaveBeenCalled();
+
+  waitForElementToBeRemoved(screen.queryByText("menu-item"));
+});
