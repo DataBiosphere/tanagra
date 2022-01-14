a91e3b012d112c4f7a5418a6a276bfd83b77e85c
marikomedlock <marikomedlock@users.noreply.github.com>
2022-01-14 15:49:36 -0500

    Add brand-ingredient entity relationship for aou_synthetic underlay. (#86)
    * Add IntermediateTable relationship mapping type.
    
    * Nautical underlay tests with intermediate table relationship.
    
    * Add nautical underlay test.
    
    * Add second nautical test, so we have entity1/2 query examples.
    
    * Fix typo.
    
    * Add brand ingredient relationship and tests for aou_synthetic underlay.

--- api/src/test/java/bio/terra/tanagra/aousynthetic/IngredientEntityQueriesTest.java
+++ api/src/test/java/bio/terra/tanagra/aousynthetic/IngredientEntityQueriesTest.java
@@ -6,8 +6,14 @@ import static bio.terra.tanagra.aousynthetic.UnderlayUtils.UNDERLAY_NAME;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import bio.terra.tanagra.app.controller.EntityInstancesApiController;
+import bio.terra.tanagra.generated.model.ApiAttributeValue;
+import bio.terra.tanagra.generated.model.ApiAttributeVariable;
+import bio.terra.tanagra.generated.model.ApiBinaryFilter;
+import bio.terra.tanagra.generated.model.ApiBinaryFilterOperator;
 import bio.terra.tanagra.generated.model.ApiEntityDataset;
+import bio.terra.tanagra.generated.model.ApiFilter;
 import bio.terra.tanagra.generated.model.ApiGenerateDatasetSqlQueryRequest;
+import bio.terra.tanagra.generated.model.ApiRelationshipFilter;
 import bio.terra.tanagra.generated.model.ApiSqlQuery;
 import bio.terra.tanagra.testing.BaseSpringUnitTest;
 import bio.terra.tanagra.testing.GeneratedSqlUtils;
@@ -41,5 +47,49 @@ public class IngredientEntityQueriesTest extends BaseSpringUnitTest {
     String generatedSql = response.getBody().getQuery();
     GeneratedSqlUtils.checkMatchesOrOverwriteGoldenFile(
         generatedSql, "aousynthetic/all-ingredient-entities.sql");
+  }
+
+  @Test
+  @DisplayName(
+      "correct SQL string for listing all ingredient entity instances related to a single brand entity instance")
+  void generateSqlForIngredientEntitiesRelatedToABrandEntity() throws IOException {
+    // filter for "brand" entity instances that have concept_id=19082059
+    // i.e. give me the brand "Tylenol Chest Congestion"
+    ApiBinaryFilter brandIsTylenolChestCongestion =
+        new ApiBinaryFilter()
+            .attributeVariable(
+                new ApiAttributeVariable().variable("brand_alias").name("concept_id"))
+            .operator(ApiBinaryFilterOperator.EQUALS)
+            .attributeValue(new ApiAttributeValue().int64Val(19_082_059L));
+
+    // filter for "ingredient" entity instances that are related to "brand" entity instances that
+    // have
+    // concept_id=19082059
+    // i.e. give me all the ingredients in "Tylenol Chest Congestion"
+    ApiFilter ingredientsInTylenolChestCongestion =
+        new ApiFilter()
+            .relationshipFilter(
+                new ApiRelationshipFilter()
+                    .outerVariable("ingredient_alias")
+                    .newVariable("brand_alias")
+                    .newEntity("brand")
+                    .filter(new ApiFilter().binaryFilter(brandIsTylenolChestCongestion)));
+
+    ResponseEntity<ApiSqlQuery> response =
+        apiController.generateDatasetSqlQuery(
+            UNDERLAY_NAME,
+            INGREDIENT_ENTITY,
+            new ApiGenerateDatasetSqlQueryRequest()
+                .entityDataset(
+                    new ApiEntityDataset()
+                        .entityVariable("ingredient_alias")
+                        .selectedAttributes(ALL_INGREDIENT_ATTRIBUTES)
+                        .filter(ingredientsInTylenolChestCongestion)));
+    assertEquals(HttpStatus.OK, response.getStatusCode());
+    String generatedSql = response.getBody().getQuery();
+    GeneratedSqlUtils.checkMatchesOrOverwriteGoldenFile(
+        generatedSql,
+        "aousynthetic/ingredient-entities-related-to-a-brand.sql",
+        "brand_ingredient");
   }
 }
