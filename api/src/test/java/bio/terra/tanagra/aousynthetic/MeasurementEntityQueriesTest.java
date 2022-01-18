fb3553a3dc5fc900ef48859fed12fcadb5c374e3
Mariko Medlock <mariko.medlock@gmail.com>
2022-01-18 18:12:50 -0500

    Hierarchy definition and test queries for measurement entity.

--- api/src/test/java/bio/terra/tanagra/aousynthetic/MeasurementEntityQueriesTest.java
+++ api/src/test/java/bio/terra/tanagra/aousynthetic/MeasurementEntityQueriesTest.java
@@ -6,7 +6,12 @@ import static bio.terra.tanagra.aousynthetic.UnderlayUtils.UNDERLAY_NAME;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import bio.terra.tanagra.app.controller.EntityInstancesApiController;
+import bio.terra.tanagra.generated.model.ApiAttributeValue;
+import bio.terra.tanagra.generated.model.ApiAttributeVariable;
+import bio.terra.tanagra.generated.model.ApiBinaryFilter;
+import bio.terra.tanagra.generated.model.ApiBinaryFilterOperator;
 import bio.terra.tanagra.generated.model.ApiEntityDataset;
+import bio.terra.tanagra.generated.model.ApiFilter;
 import bio.terra.tanagra.generated.model.ApiGenerateDatasetSqlQueryRequest;
 import bio.terra.tanagra.generated.model.ApiSqlQuery;
 import bio.terra.tanagra.testing.BaseSpringUnitTest;
@@ -41,5 +46,68 @@ public class MeasurementEntityQueriesTest extends BaseSpringUnitTest {
     String generatedSql = response.getBody().getQuery();
     GeneratedSqlUtils.checkMatchesOrOverwriteGoldenFile(
         generatedSql, "aousynthetic/all-measurement-entities.sql");
+  }
+
+  @Test
+  @DisplayName("correct SQL string for listing descendants of a single measurement entity instance")
+  void generateSqlForDescendantsOfAMeasurementEntity() throws IOException {
+    // filter for "measurement" entity instances that are descendants of the "measurement" entity
+    // instance with concept_id=37048668
+    // i.e. give me all the descendants of "Glucose tolerance 2 hours panel"
+    ApiFilter descendantsOfGlucoseTolerance =
+        new ApiFilter()
+            .binaryFilter(
+                new ApiBinaryFilter()
+                    .attributeVariable(
+                        new ApiAttributeVariable().variable("measurement_alias").name("concept_id"))
+                    .operator(ApiBinaryFilterOperator.DESCENDANT_OF_INCLUSIVE)
+                    .attributeValue(new ApiAttributeValue().int64Val(37_048_668L)));
+
+    ResponseEntity<ApiSqlQuery> response =
+        apiController.generateDatasetSqlQuery(
+            UNDERLAY_NAME,
+            MEASUREMENT_ENTITY,
+            new ApiGenerateDatasetSqlQueryRequest()
+                .entityDataset(
+                    new ApiEntityDataset()
+                        .entityVariable("measurement_alias")
+                        .selectedAttributes(ALL_MEASUREMENT_ATTRIBUTES)
+                        .filter(descendantsOfGlucoseTolerance)));
+    assertEquals(HttpStatus.OK, response.getStatusCode());
+    String generatedSql = response.getBody().getQuery();
+    GeneratedSqlUtils.checkMatchesOrOverwriteGoldenFile(
+        generatedSql, "aousynthetic/measurement-entities-descendants-of-a-measurement.sql");
+  }
+
+  @Test
+  @DisplayName("correct SQL string for listing children of a single measurement entity instance")
+  void generateSqlForChildrenOfAMeasurementEntity() throws IOException {
+    // filter for "measurement" entity instances that are children of the "measurement" entity
+    // instance with concept_id=37072239
+    // i.e. give me all the children of "Glucose tolerance 2 hours panel | Serum or Plasma |
+    // Challenge Bank Panels"
+    ApiFilter childrenOfGlucoseToleranceSerumOrPlasma =
+        new ApiFilter()
+            .binaryFilter(
+                new ApiBinaryFilter()
+                    .attributeVariable(
+                        new ApiAttributeVariable().variable("measurement_alias").name("concept_id"))
+                    .operator(ApiBinaryFilterOperator.CHILD_OF)
+                    .attributeValue(new ApiAttributeValue().int64Val(37_072_239L)));
+
+    ResponseEntity<ApiSqlQuery> response =
+        apiController.generateDatasetSqlQuery(
+            UNDERLAY_NAME,
+            MEASUREMENT_ENTITY,
+            new ApiGenerateDatasetSqlQueryRequest()
+                .entityDataset(
+                    new ApiEntityDataset()
+                        .entityVariable("measurement_alias")
+                        .selectedAttributes(ALL_MEASUREMENT_ATTRIBUTES)
+                        .filter(childrenOfGlucoseToleranceSerumOrPlasma)));
+    assertEquals(HttpStatus.OK, response.getStatusCode());
+    String generatedSql = response.getBody().getQuery();
+    GeneratedSqlUtils.checkMatchesOrOverwriteGoldenFile(
+        generatedSql, "aousynthetic/measurement-entities-children-of-a-measurement.sql");
   }
 }
