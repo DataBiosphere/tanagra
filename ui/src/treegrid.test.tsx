8bb26c00459fc3aafc75499c79859786ac918bdf
Tim Jennison <32747364+tjennison-google@users.noreply.github.com>
2022-01-19 12:46:00 -0500

    Add new treegrid component (#89)
    This will replace the existing table views and also provide the basis
    for the hierarchical view.

---
+++ ui/src/treegrid.test.tsx
@@ -0,0 +1,71 @@
+import AccountTreeIcon from "@mui/icons-material/AccountTree";
+import CheckBoxIcon from "@mui/icons-material/CheckBox";
+import IconButton from "@mui/material/IconButton";
+import renderer from "react-test-renderer";
+import { TreeGrid, TreeGridData, TreeGridId } from "treegrid";
+
+test("Table renders correctly", () => {
+  const columns = [
+    {
+      key: "col1",
+      width: "100%",
+      title: "Column 1",
+    },
+    {
+      key: "col2",
+      width: 150,
+      title: "Column 2",
+    },
+    {
+      key: "col3",
+      width: 50,
+      title: <AccountTreeIcon />,
+    },
+  ];
+
+  const data: TreeGridData = {
+    root: {
+      data: {},
+      children: [1, 3],
+    },
+    1: {
+      data: {
+        col1: "1-col1",
+        col2: "1-col2",
+      },
+      children: [2],
+    },
+    2: {
+      data: {
+        col1: "2-col1",
+        col2: "2-col2",
+        col3: "2-col3",
+      },
+    },
+    3: {
+      data: {
+        col1: "3-col1",
+        col2: "3-col2",
+        col3: <AccountTreeIcon />,
+      },
+    },
+  };
+
+  const prefixElements = (id: TreeGridId) => {
+    // Add variety to the rows.
+    if (id !== 2) {
+      return undefined;
+    }
+    return;
+    <IconButton size="small">
+      <CheckBoxIcon fontSize="inherit" />
+    </IconButton>;
+  };
+
+  const tree = renderer
+    .create(
+      <TreeGrid columns={columns} data={data} prefixElements={prefixElements} />
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
---
+++ ui/src/treegrid.tsx
@@ -0,0 +1,257 @@
+import ErrorIcon from "@mui/icons-material/Error";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+import CircularProgress from "@mui/material/CircularProgress";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { ReactNode, useCallback } from "react";
+import { useImmer } from "use-immer";
+
+export type TreeGridId = string | number;
+
+export type TreeGridRowData = {
+  [key: string]: undefined | string | number | boolean | JSX.Element;
+};
+
+export type TreeGridItem = {
+  children?: TreeGridId[];
+  data: TreeGridRowData;
+};
+
+export type TreeGridData = {
+  [key: TreeGridId]: TreeGridItem;
+};
+
+export type TreeGridColumn = {
+  key: string;
+  width: string | number;
+  title?: string | JSX.Element;
+};
+
+export type TreeGridProps = {
+  columns: TreeGridColumn[];
+  data: TreeGridData;
+  prefixElements?: (id: TreeGridId, data: TreeGridRowData) => ReactNode;
+  // The returned string is displayed as an error message for failures and
+  // ignored for successes.
+  loadChildren?: (id: TreeGridId) => Promise<string>;
+};
+
+export function TreeGrid(props: TreeGridProps) {
+  const [state, updateState] = useImmer<TreeGridState>(
+    new Map<TreeGridId, TreeGridItemState>()
+  );
+
+  const toggleExpanded = useCallback(
+    (id: TreeGridId) => {
+      updateState((draft) => {
+        const itemState = draft.get(id) || {
+          status: Status.Collapsed,
+        };
+        switch (itemState.status) {
+          case Status.Loading:
+            return;
+          case Status.Expanded:
+            itemState.status = Status.Collapsed;
+            break;
+          default:
+            if (!props.loadChildren || !!itemState.loaded) {
+              itemState.status = Status.Expanded;
+            } else {
+              itemState.status = Status.Loading;
+              props
+                .loadChildren(id)
+                .then(() => {
+                  updateState((draft) => {
+                    draft.set(id, {
+                      status: Status.Expanded,
+                      loaded: true,
+                    });
+                  });
+                })
+                .catch((error) => {
+                  updateState((draft) => {
+                    draft.set(id, {
+                      status: Status.Failed,
+                      errorMessage: error,
+                    });
+                  });
+                });
+            }
+        }
+        draft.set(id, itemState);
+      });
+    },
+    [props.loadChildren]
+  );
+
+  return (
+    <>
+      <table
+        style={{
+          tableLayout: "fixed",
+        }}
+      >
+        <thead>
+          <tr>
+            {props.columns.map((col, i) => (
+              <td
+                key={i}
+                style={{
+                  ...(col.width && {
+                    maxWidth: col.width,
+                    width: col.width,
+                    minWidth: col.width,
+                  }),
+                }}
+              >
+                <Typography variant="h6">{col.title}</Typography>
+              </td>
+            ))}
+          </tr>
+        </thead>
+      </table>
+      <div style={{ overflowY: "auto", display: "block" }}>
+        <table
+          style={{
+            tableLayout: "fixed",
+          }}
+        >
+          <tbody>
+            {renderChildren(props, state, toggleExpanded, "root", 0, false)}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+}
+
+function renderChildren(
+  props: TreeGridProps,
+  state: TreeGridState,
+  toggleExpanded: (id: TreeGridId) => void,
+  id: TreeGridId,
+  indent: number,
+  collapse: boolean
+): JSX.Element[] {
+  const results: JSX.Element[] = [];
+
+  props.data[id]?.children?.forEach((childId) => {
+    const child = props.data[childId];
+    if (!child) {
+      return;
+    }
+    const childState = state.get(childId);
+
+    results.push(
+      <tr
+        key={childId}
+        style={{
+          ...(collapse && { visibility: "collapse" }),
+        }}
+      >
+        {props.columns.map((col, i) => {
+          let value = child.data[col.key] || "";
+          let title = "";
+          // Stringify values other than Elements.
+          if (!(value instanceof Object)) {
+            value = String(value);
+            title = value;
+          }
+
+          return (
+            <td
+              key={i}
+              style={{
+                ...(col.width && {
+                  maxWidth: col.width,
+                  width: col.width,
+                  minWidth: col.width,
+                }),
+              }}
+            >
+              <div
+                style={{
+                  textOverflow: "ellipsis",
+                  whiteSpace: "nowrap",
+                  overflow: "hidden",
+                  ...(i === 0 && { paddingLeft: `${indent}em` }),
+                }}
+              >
+                <Typography
+                  variant="body1"
+                  noWrap
+                  title={title}
+                  style={{
+                    display: "inline",
+                  }}
+                >
+                  {i === 0 && (
+                    <>
+                      {child.children?.length && (
+                        <IconButton
+                          size="small"
+                          title={childState?.errorMessage}
+                          onClick={() => {
+                            toggleExpanded(childId);
+                          }}
+                        >
+                          <ItemIcon state={childState} />
+                        </IconButton>
+                      )}
+                      {props.prefixElements?.(childId, child.data)}
+                    </>
+                  )}
+                  {value}
+                </Typography>
+              </div>
+            </td>
+          );
+        })}
+      </tr>
+    );
+
+    results.push(
+      ...renderChildren(
+        props,
+        state,
+        toggleExpanded,
+        childId,
+        indent + 1,
+        collapse || childState?.status !== Status.Expanded
+      )
+    );
+  });
+  return results;
+}
+
+enum Status {
+  Collapsed,
+  Expanded,
+  Loading,
+  Failed,
+}
+
+type TreeGridItemState = {
+  status: Status;
+  loaded?: boolean;
+  errorMessage?: string;
+};
+
+type TreeGridState = Map<TreeGridId, TreeGridItemState>;
+
+type ItemIconProps = {
+  state?: TreeGridItemState;
+};
+
+function ItemIcon(props: ItemIconProps) {
+  switch (props.state?.status) {
+    case Status.Expanded:
+      return <KeyboardArrowDownIcon fontSize="inherit" />;
+    case Status.Loading:
+      return <CircularProgress style={{ width: "1em", height: "1em" }} />;
+    case Status.Failed:
+      return <ErrorIcon fontSize="inherit" />;
+  }
+  return <KeyboardArrowRightIcon fontSize="inherit" />;
+}
