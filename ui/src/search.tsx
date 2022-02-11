54b5756e132e652cfbfeeed5725f7c054fc67116
Timothy Jennison <tjennison@google.com>
2022-02-11 11:07:13 -0500

    Add search bar to concept criteria
    Currently only in the list view because search in the hierarchy view has
    different behavior.

---
+++ ui/src/search.tsx
@@ -0,0 +1,38 @@
+import SearchIcon from "@mui/icons-material/Search";
+import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import Stack from "@mui/material/Stack";
+import { TextField } from "mui-rff";
+import { Form } from "react-final-form";
+
+export type SearchProps = {
+  placeholder?: string;
+  onSearch: (query: string) => void;
+};
+
+export function Search(props: SearchProps) {
+  return (
+    <Box m={2}>
+      <Form
+        initialValues={{ query: "" }}
+        onSubmit={({ query }) => props.onSearch(query)}
+        render={({ handleSubmit }) => (
+          <form onSubmit={handleSubmit}>
+            <Stack direction="row" justifyContent="center" alignItems="center">
+              <TextField
+                autoFocus
+                fullWidth
+                name="query"
+                placeholder={props.placeholder}
+                autoComplete="off"
+              />
+              <IconButton type="submit">
+                <SearchIcon />
+              </IconButton>
+            </Stack>
+          </form>
+        )}
+      />
+    </Box>
+  );
+}
