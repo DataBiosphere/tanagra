2d60d64ebd40f7bbfe5e236e2a212f800a2181a2
Mariko Medlock <mariko.medlock@gmail.com>
2022-02-11 00:24:53 -0500

    Comments in random num generator class.

--- api/src/main/java/bio/terra/tanagra/service/search/utils/RandomNumberGenerator.java
+++ api/src/main/java/bio/terra/tanagra/service/search/utils/RandomNumberGenerator.java
@@ -2,26 +2,45 @@ package bio.terra.tanagra.service.search.utils;
 
 import java.util.Random;
 
+/**
+ * Utility method for generating random numbers. Supports testing mode where the generator is
+ * re-seeded for each test, to produce an identical sequence that can be used in expected/actual
+ * comparisons.
+ */
 public final class RandomNumberGenerator {
   private RandomNumberGenerator() {}
 
+  // default un-seeded random number generator used in production
   private static final Random UNSEEDED_RANDOM = new Random();
+  // seeded random number generator used during testing
   private static Random seededRandom = new Random();
+
+  // system property name indicating that we should use a seeded random (set by test harness before
+  // each test method)
+  private static final String USE_RANDOM_SEED_SYSTEM_PROPERTY = "GENERATE_SQL_RANDOM_SEED";
+
+  // whether to use the seeded random (true when testing), or unseeded (false when not testing)
+  private static final boolean USE_SEEDED_RANDOM = useSeededRandom();
+
+  // hard-coded (arbitrary) number to re-seed the generator with at the beginning of each test
   private static final long RANDOM_SEED = 2022;
 
+  /** Return a random number generator. Used in SQL query generation. */
   public static Random getRandom() {
     return USE_SEEDED_RANDOM ? getSeededRandom() : UNSEEDED_RANDOM;
   }
 
-  // System property name indicating that we should use a seeded random (used for testing)
-  private static final String USE_RANDOM_SEED_SYSTEM_PROPERTY = "GENERATE_SQL_RANDOM_SEED";
-  private static final boolean USE_SEEDED_RANDOM = useSeededRandom();
-
+  /**
+   * Check if the system property is set. This method is called to set a final static boolean, so
+   * that we only need to check the system property once in the non-testing case, not each time we
+   * need a random number.
+   */
   private static boolean useSeededRandom() {
     String useRandomSeedSysProp = System.getProperty(USE_RANDOM_SEED_SYSTEM_PROPERTY);
     return useRandomSeedSysProp != null && !useRandomSeedSysProp.isEmpty();
   }
 
+  /** Return a seeded random generator. */
   private static Random getSeededRandom() {
     synchronized (RandomNumberGenerator.class) {
       // check the sys prop to see if we need to re-start the seeded random.
