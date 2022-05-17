99a74277875a2f422ce5e982b002c0ac635cb70a
Neel Ismail <neelismail@google.com>
2022-05-17 21:04:25 +0000

    adding support for multiple attribute ranges

--- ui/src/criteria/attribute.tsx
+++ ui/src/criteria/attribute.tsx
@@ -1,10 +1,16 @@
-import { Checkbox, FormControlLabel, ListItem, Slider } from "@mui/material";
+import {
+  Button,
+  Checkbox,
+  FormControlLabel,
+  ListItem,
+  Slider,
+} from "@mui/material";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import Input from "@mui/material/Input";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
-import { CriteriaPlugin, registerCriteriaPlugin } from "cohort";
+import { CriteriaPlugin, generateId, registerCriteriaPlugin } from "cohort";
 import { useUnderlay } from "hooks";
 import produce from "immer";
 import React, { useState } from "react";
@@ -17,6 +23,12 @@ type Selection = {
   name: string;
 };
 
+type DataRange = {
+  id: string;
+  min: number | undefined;
+  max: number | undefined;
+};
+
 interface Config extends CriteriaConfig {
   attribute: string;
   name: string;
@@ -27,8 +39,7 @@ interface Data extends Config {
   selected: Selection[];
 
   // The min/max are valid for integer attributes.
-  min: number | undefined;
-  max: number | undefined;
+  dataRanges: DataRange[];
 }
 
 type AttributeEditProps = {
@@ -46,11 +57,21 @@ type AttributeEditProps = {
       ?.attributes?.find((attribute) => attribute.name === data.attribute)
       ?.attributeFilterHint?.integerBoundsHint;
 
+    const initialDataRange: DataRange = {
+      id: generateId(),
+      min: undefined,
+      max: undefined,
+    };
+
+    if (integerBoundsHint?.min && integerBoundsHint?.max) {
+      initialDataRange.min = integerBoundsHint.min;
+      initialDataRange.max = integerBoundsHint.max;
+    }
+
     return {
       ...data,
       selected: !integerBoundsHint ? [] : undefined,
-      min: integerBoundsHint?.min,
-      max: integerBoundsHint?.max,
+      dataRanges: [initialDataRange],
     };
   }
 )
@@ -71,38 +92,50 @@ class _ implements CriteriaPlugin<Data> {
   }
 
   generateFilter(entityVar: string) {
-    if (isValid(this.data.min) && isValid(this.data.max)) {
-      return {
-        arrayFilter: {
-          operands: [
-            {
-              binaryFilter: {
-                attributeVariable: {
-                  variable: entityVar,
-                  name: this.data.attribute,
+    const dataRangeFilter: tanagra.Filter = {
+      arrayFilter: {
+        operands: this.data.dataRanges
+          .filter((range) => isValid(range.min) && isValid(range.max))
+          .map((range) => ({
+            arrayFilter: {
+              operands: [
+                {
+                  binaryFilter: {
+                    attributeVariable: {
+                      variable: entityVar,
+                      name: this.data.attribute,
+                    },
+                    operator: tanagra.BinaryFilterOperator.GreaterThan,
+                    attributeValue: {
+                      int64Val: range.min,
+                    },
+                  },
                 },
-                operator: tanagra.BinaryFilterOperator.LessThan,
-                attributeValue: {
-                  int64Val: this.data.max,
+                {
+                  binaryFilter: {
+                    attributeVariable: {
+                      variable: entityVar,
+                      name: this.data.attribute,
+                    },
+                    operator: tanagra.BinaryFilterOperator.GreaterThan,
+                    attributeValue: {
+                      int64Val: range.min,
+                    },
+                  },
                 },
-              },
+              ],
+              operator: tanagra.ArrayFilterOperator.And,
             },
-            {
-              binaryFilter: {
-                attributeVariable: {
-                  variable: entityVar,
-                  name: this.data.attribute,
-                },
-                operator: tanagra.BinaryFilterOperator.GreaterThan,
-                attributeValue: {
-                  int64Val: this.data.min,
-                },
-              },
-            },
-          ],
-          operator: tanagra.ArrayFilterOperator.And,
-        },
-      };
+          })),
+        operator: tanagra.ArrayFilterOperator.Or,
+      },
+    };
+
+    if (
+      dataRangeFilter.arrayFilter?.operands &&
+      dataRangeFilter.arrayFilter?.operands.length > 0
+    ) {
+      return dataRangeFilter;
     } else if (this.data.selected.length >= 0) {
       return {
         arrayFilter: {
@@ -131,6 +164,125 @@ class _ implements CriteriaPlugin<Data> {
   }
 }
 
+type SliderProps = {
+  minBound: number;
+  maxBound: number;
+  range: DataRange;
+  data: Data;
+  dispatchFn: (data: Data) => void;
+  index: number;
+};
+
+function AttributeSlider(props: SliderProps) {
+  const { minBound, maxBound, range, data, dispatchFn, index } = props;
+
+  // Two sets of values are needed due to the input box and slider is isolated.
+  const [minInputValue, setMinInputValue] = useState(
+    String(range.min || minBound)
+  );
+  const [maxInputValue, setMaxInputValue] = useState(
+    String(range.max || maxBound)
+  );
+  const [minValue, setMinValue] = useState(range.min || minBound);
+  const [maxValue, setMaxValue] = useState(range.max || maxBound);
+
+  const updateValue = (newMin: number, newMax: number) => {
+    setMinValue(newMin);
+    setMaxValue(newMax);
+    dispatchFn(
+      produce(data, (oldData) => {
+        oldData.dataRanges[index].min = newMin;
+        oldData.dataRanges[index].max = newMax;
+      })
+    );
+  };
+
+  const handleChange = (event: Event, newValue: number | number[]) => {
+    const [newMin, newMax] = newValue as number[];
+    setMinInputValue(String(newMin));
+    setMaxInputValue(String(newMax));
+    updateValue(newMin, newMax);
+  };
+
+  // Make sure empty input won't get changed.
+
+  const handleMinInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setMinInputValue(event.target.value);
+    const newMin = event.target.value === "" ? 0 : Number(event.target.value);
+    updateValue(Math.min(maxValue, Math.max(minBound, newMin)), maxValue);
+  };
+  const handleMinInputBlur = () => {
+    setMinInputValue(String(minValue));
+  };
+
+  const handleMaxInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setMaxInputValue(event.target.value);
+    const newMax = event.target.value === "" ? 0 : Number(event.target.value);
+    updateValue(Math.max(minValue, Math.min(maxBound, newMax)), minValue);
+  };
+  const handleMaxInputBlur = () => {
+    setMaxInputValue(String(maxValue));
+  };
+
+  const handleDeleteRange = () => {
+    dispatchFn(
+      produce(data, (oldData) => {
+        oldData.dataRanges.splice(index, 1);
+      })
+    );
+  };
+
+  return (
+    <Box sx={{ width: "30%", minWidth: 500, margin: 5 }}>
+      <Grid container spacing={2} direction="row">
+        <Grid item>
+          <Input
+            value={minInputValue}
+            size="medium"
+            onChange={handleMinInputChange}
+            onBlur={handleMinInputBlur}
+            inputProps={{
+              step: Math.ceil((maxBound - minBound) / 20),
+              min: minBound,
+              max: maxBound,
+              type: "number",
+              "aria-labelledby": "input-slider",
+            }}
+          />
+        </Grid>
+        <Grid item xs>
+          <Slider
+            value={[minValue, maxValue]}
+            onChange={handleChange}
+            valueLabelDisplay="auto"
+            getAriaValueText={(value) => value.toString()}
+            min={minBound}
+            max={maxBound}
+            disableSwap
+          />
+        </Grid>
+        <Grid item>
+          <Input
+            value={maxInputValue}
+            onChange={handleMaxInputChange}
+            onBlur={handleMaxInputBlur}
+            inputProps={{
+              step: Math.ceil((maxBound - minBound) / 20),
+              min: minBound,
+              max: maxBound,
+              type: "number",
+              "aria-labelledby": "input-slider",
+            }}
+          />
+        </Grid>
+        <Button variant="outlined" sx={{ ml: 5 }} onClick={handleDeleteRange}>
+          Delete
+        </Button>
+      </Grid>
+    </Box>
+  );
+}
+
 function AttributeEdit(props: AttributeEditProps) {
   const underlay = useUnderlay();
   const hintDisplayName = (hint: tanagra.EnumHintValue) =>
@@ -147,107 +299,48 @@ function AttributeEdit(props: AttributeEditProps) {
   if (isValid(integerBoundsHint?.min) && isValid(integerBoundsHint?.max)) {
     // TODO: The comments can be removed once isValid is fixed.
 
-    // This is to ensure the compiler won't complain the object be undefined.
-    // Although we already know that min and max is valid.
-    const minBound = integerBoundsHint?.min || 0;
-    const maxBound = integerBoundsHint?.max || 0;
-
-    // Two sets of values are needed due to the input box and slider is isolated.
-    const [minInputValue, setMinInputValue] = useState(
-      String(props.data.min || minBound)
-    );
-    const [maxInputValue, setMaxInputValue] = useState(
-      String(props.data.max || maxBound)
-    );
-    const [minValue, setMinValue] = useState(props.data.min || minBound);
-    const [maxValue, setMaxValue] = useState(props.data.max || maxBound);
-
-    const updateValue = (newMin: number, newMax: number) => {
-      setMinValue(newMin);
-      setMaxValue(newMax);
+    const handleAddRange = () => {
       props.dispatchFn(
         produce(props.data, (data) => {
-          data.min = newMin;
-          data.max = newMax;
+          // TODO: Ask about a more efficient way to handle non-null values
+          //       Without the guard, there is a type mismatch (number | null | undefined vs number | undefined)
+          //       Changing type to be number | null | undefined creates problems in other places in the code
+          if (integerBoundsHint?.min && integerBoundsHint?.max) {
+            data.dataRanges.push({
+              id: generateId(),
+              min: integerBoundsHint.min,
+              max: integerBoundsHint.max,
+            });
+          }
         })
       );
     };
 
-    const handleChange = (event: Event, newValue: number | number[]) => {
-      const [newMin, newMax] = newValue as number[];
-      setMinInputValue(String(newMin));
-      setMaxInputValue(String(newMax));
-      updateValue(newMin, newMax);
-    };
-
-    // Make sure empty input won't get changed.
-
-    const handleMinInputChange = (
-      event: React.ChangeEvent<HTMLInputElement>
-    ) => {
-      setMinInputValue(event.target.value);
-      const newMin = event.target.value === "" ? 0 : Number(event.target.value);
-      updateValue(Math.min(maxValue, Math.max(minBound, newMin)), maxValue);
-    };
-    const handleMinInputBlur = () => {
-      setMinInputValue(String(minValue));
-    };
-
-    const handleMaxInputChange = (
-      event: React.ChangeEvent<HTMLInputElement>
-    ) => {
-      setMaxInputValue(event.target.value);
-      const newMax = event.target.value === "" ? 0 : Number(event.target.value);
-      updateValue(Math.max(minValue, Math.min(maxBound, newMax)), minValue);
-    };
-    const handleMaxInputBlur = () => {
-      setMaxInputValue(String(maxValue));
-    };
-
     return (
-      <Box sx={{ width: "30%", minWidth: 300, margin: 5 }}>
-        <Grid container spacing={2}>
-          <Grid item>
-            <Input
-              value={minInputValue}
-              size="medium"
-              onChange={handleMinInputChange}
-              onBlur={handleMinInputBlur}
-              inputProps={{
-                step: Math.ceil((maxBound - minBound) / 20),
-                min: minBound,
-                max: maxBound,
-                type: "number",
-                "aria-labelledby": "input-slider",
-              }}
-            />
-          </Grid>
-          <Grid item xs>
-            <Slider
-              value={[minValue, maxValue]}
-              onChange={handleChange}
-              valueLabelDisplay="auto"
-              getAriaValueText={(value) => value.toString()}
-              min={minBound}
-              max={maxBound}
-              disableSwap
-            />
-          </Grid>
-          <Grid item>
-            <Input
-              value={maxInputValue}
-              onChange={handleMaxInputChange}
-              onBlur={handleMaxInputBlur}
-              inputProps={{
-                step: Math.ceil((maxBound - minBound) / 20),
-                min: minBound,
-                max: maxBound,
-                type: "number",
-                "aria-labelledby": "input-slider",
-              }}
-            />
-          </Grid>
+      <Box>
+        <Grid container spacing={2} direction="column">
+          {props.data.dataRanges.map((range, index) => {
+            return (
+              <AttributeSlider
+                key={range.id}
+                index={index}
+                minBound={integerBoundsHint?.min || 0}
+                maxBound={integerBoundsHint?.max || 0}
+                range={range}
+                data={props.data}
+                dispatchFn={props.dispatchFn}
+              />
+            );
+          })}
         </Grid>
+        <Button
+          variant="contained"
+          size="large"
+          sx={{ mt: 5 }}
+          onClick={handleAddRange}
+        >
+          Add Range
+        </Button>
       </Box>
     );
   }
@@ -314,13 +407,16 @@ function AttributeDetails(props: AttributeDetailsProps) {
         ))}
       </>
     );
-  } else if (isValid(props.data.min) && isValid(props.data.max)) {
+  } else if (
+    isValid(props.data.dataRanges[0].min) &&
+    isValid(props.data.dataRanges[0].max)
+  ) {
     return (
       <>
         <Stack direction="row" alignItems="baseline">
           <Typography variant="body1">
-            Current {props.data.name} in Range {props.data.min} to{" "}
-            {props.data.max}
+            Current {props.data.name} in Range {props.data.dataRanges[0].min} to{" "}
+            {props.data.dataRanges[0].max}
           </Typography>
         </Stack>
       </>
