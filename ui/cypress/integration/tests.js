6543b4366476cb111b3114af68c82f46fe974ce8
Neel Ismail <neelismail@google.com>
2022-06-07 19:41:26 +0000

    including excluded attributes into integration test

--- ui/cypress/integration/tests.js
+++ ui/cypress/integration/tests.js
@@ -20,7 +20,17 @@ describe("Basic tests", () => {
 
     cy.get("button:Contains(Add Criteria)").first().click();
     cy.get("li:Contains(Year at Birth)").click();
-    cy.get("input").first().type("30");
+    cy.get("input").first().clear().type("30");
+    cy.get("a[aria-label=back]").click();
+
+    cy.get("button:Contains(Add Criteria)").last().click();
+    cy.get("li:Contains(Observations)").click();
+    cy.get("button[role=checkbox]").click();
+    cy.get("a[aria-label=back]").click();
+
+    cy.get("button:Contains(Add Criteria)").last().click();
+    cy.get("li:Contains(Year at Birth)").click();
+    cy.get("input").first().clear().type("50");
     cy.get("a[aria-label=back]").click();
     cy.get("a[aria-label=back]").click();
 
