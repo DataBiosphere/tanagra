678d3c211ea827ee256cfb23f693d2269530ad15
Neel Ismail <neelismail@google.com>
2022-06-30 18:09:34 +0000

    adding navigation functionality to undo and redo actions

--- ui/src/containers/UndoRedo.tsx
+++ ui/src/containers/UndoRedo.tsx
@@ -1,46 +1,60 @@
-import React from "react";
-import { ActionCreators as UndoActionCreators } from 'redux-undo'
-import { connect, ConnectedProps } from 'react-redux'
-import { Box, Button } from "@mui/material";
-import UndoIcon from "@mui/icons-material/Undo";
 import RedoIcon from "@mui/icons-material/Redo";
+import UndoIcon from "@mui/icons-material/Undo";
+import { Box, Button } from "@mui/material";
+import { useUrl } from "hooks";
+import { connect, ConnectedProps } from "react-redux";
+import { Link as RouterLink } from "react-router-dom";
+import { ActionCreators as UndoActionCreators } from "redux-undo";
 import { RootState } from "rootReducer";
 import { AppDispatch } from "store";
 
 const mapStateToProps = (state: RootState) => {
-    return {
-        canUndo: state.past.length > 0,
-        canRedo: state.future.length > 0,
-    }
-}
+  return {
+    canUndo: state.past.length > 1,
+    canRedo: state.future.length > 0,
+  };
+};
 
 const mapDispatchToProps = (dispatch: AppDispatch) => {
-    return {
-        onUndo: () => dispatch(UndoActionCreators.undo()),
-        onRedo: () => dispatch(UndoActionCreators.redo())
-    }
-}
+  return {
+    onUndo: () => dispatch(UndoActionCreators.undo()),
+    onRedo: () => dispatch(UndoActionCreators.redo()),
+  };
+};
 
-const connector = connect(mapStateToProps, mapDispatchToProps)
+const connector = connect(mapStateToProps, mapDispatchToProps);
 
-type propsFromRedux = ConnectedProps<typeof connector>
+type propsFromRedux = ConnectedProps<typeof connector>;
 
-function UndoRedo({
-    canUndo,
-    canRedo,
-    onUndo,
-    onRedo,
-}: propsFromRedux) {
-    return (
-        <Box>
-            <Button onClick={onUndo}  disabled={!canUndo}>
-                <UndoIcon fontSize="medium" sx={{ color: canUndo ? "white" : "gray" }} />
-            </Button>
-            <Button onClick={onRedo}  disabled={!canRedo}>
-                <RedoIcon fontSize="medium" sx={{ color: canRedo ? "white" : "gray" }} />
-            </Button>
-        </Box>
-    )
+function UndoRedo({ canUndo, canRedo, onUndo, onRedo }: propsFromRedux) {
+  const url = useUrl();
+
+  return (
+    <Box>
+      <Button
+        onClick={onUndo}
+        disabled={!canUndo}
+        component={RouterLink}
+        to={url}
+      >
+        <UndoIcon
+          fontSize="medium"
+          sx={{ color: canUndo ? "white" : "gray" }}
+        />
+      </Button>
+      <Button
+        onClick={onRedo}
+        disabled={!canRedo}
+        component={RouterLink}
+        to={url}
+      >
+        <RedoIcon
+          fontSize="medium"
+          sx={{ color: canRedo ? "white" : "gray" }}
+        />
+      </Button>
+    </Box>
+  );
 }
 
 export default connector(UndoRedo);
