6e0e61ec00a428a94c82f672d0be991eccc7480b
Neel Ismail <neelismail@google.com>
2022-06-30 18:24:18 +0000

    changing base url to prevent need to concatenate

--- ui/src/urlsSlice.ts
+++ ui/src/urlsSlice.ts
@@ -1,8 +1,8 @@
 import { AnyAction, createSlice } from "@reduxjs/toolkit";
 
 const createUrlParams = (): string => {
-  const baseUrl = "http://localhost:3000/#/"; // TODO: find solution for URL
-  return "/".concat(window.location.href.slice(baseUrl.length));
+  const baseUrl = "http://localhost:3000/#"; // TODO: find solution for URL
+  return window.location.href.slice(baseUrl.length);
 };
 
 function isUndoableAction(action: AnyAction): boolean {
