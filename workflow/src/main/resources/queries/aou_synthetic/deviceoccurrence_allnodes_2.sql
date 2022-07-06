537bb9cbe6cbc4a1f76a3cc208f1bbf6216b8d93
Mariko Medlock <mariko.medlock@gmail.com>
2022-07-06 18:44:37 -0400

    Generate rollup count tables for device, observation, visit.

---
+++ workflow/src/main/resources/queries/aou_synthetic/deviceoccurrence_allnodes_2.sql
@@ -0,0 +1,6 @@
+-- PrecomputeCounts all-auxiliary-nodes input query for devices. This must match the entityMapping for the device occurrence entity in the aou_synthetic underlay.
+SELECT
+  de.device_concept_id AS node,
+  de.person_id AS secondary
+FROM  `broad-tanagra-dev.aou_synthetic_SR2019q4r4.device_exposure` de
+;
---
+++ workflow/src/main/resources/queries/aou_synthetic/observationoccurrence_allnodes_2.sql
@@ -0,0 +1,6 @@
+-- PrecomputeCounts all-auxiliary-nodes input query for observations. This must match the entityMapping for the observation occurrence entity in the aou_synthetic underlay.
+SELECT
+  o.observation_concept_id AS node,
+  o.person_id AS secondary
+FROM  `broad-tanagra-dev.aou_synthetic_SR2019q4r4.observation` o
+;
