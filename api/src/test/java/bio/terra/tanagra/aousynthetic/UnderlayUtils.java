aca32d1123b57942d70f02397f2971c9d2e0273b
Mariko Medlock <mariko.medlock@gmail.com>
2022-07-07 16:31:33 -0400

    Update all condition entities query test to order by person_count DESC.

--- api/src/test/java/bio/terra/tanagra/aousynthetic/UnderlayUtils.java
+++ api/src/test/java/bio/terra/tanagra/aousynthetic/UnderlayUtils.java
@@ -32,7 +32,7 @@ public final class UnderlayUtils {
           "sex_at_birth");
 
   public static final String CONDITION_ENTITY = "condition";
-  public static final String CONDITION_ENTITY_NAME_ATTRIBUTE = "concept_name";
+  public static final String CONDITION_ENTITY_PERSON_COUNT_ATTRIBUTE = "person_count";
   public static final List<String> ALL_CONDITION_ATTRIBUTES =
       ImmutableList.of(
           "concept_id",
