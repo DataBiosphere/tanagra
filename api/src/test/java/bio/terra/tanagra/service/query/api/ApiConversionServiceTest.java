c45d9b5c5cdd0c5eb04805f34e430f6e5921523c
Neel Ismail <neelismail@google.com>
2022-07-15 20:22:38 +0000

    fixing merge conflict in api conversion service test file

--- api/src/test/java/bio/terra/tanagra/service/query/api/ApiConversionServiceTest.java
+++ api/src/test/java/bio/terra/tanagra/service/query/api/ApiConversionServiceTest.java
@@ -40,189 +40,189 @@ public class ApiConversionServiceTest extends BaseSpringUnitTest {
   @Test
   void entityFilter() {
     ApiEntityFilter apiEntityFilter =
-        new ApiEntityFilter()
-            .entityVariable("s")
-            .filter(
-                new ApiFilter()
-                    .binaryFilter(
-                        new ApiBinaryFilter()
-                            .attributeVariable(
-                                new ApiAttributeVariable().variable("s").name("rating"))
-                            .operator(ApiBinaryFilterOperator.EQUALS)
-                            .attributeValue(new ApiAttributeValue().int64Val(42L))));
+            new ApiEntityFilter()
+                    .entityVariable("s")
+                    .filter(
+                            new ApiFilter()
+                                    .binaryFilter(
+                                            new ApiBinaryFilter()
+                                                    .attributeVariable(
+                                                            new ApiAttributeVariable().variable("s").name("rating"))
+                                                    .operator(ApiBinaryFilterOperator.EQUALS)
+                                                    .attributeValue(new ApiAttributeValue().int64Val(42L))));
 
     Variable sVar = Variable.create("s");
     assertEquals(
-        EntityFilter.builder()
-            .primaryEntity(EntityVariable.create(SAILOR, Variable.create("s")))
-            .filter(
-                Filter.BinaryFunction.create(
-                    Expression.AttributeExpression.create(
-                        AttributeVariable.create(SAILOR_RATING, sVar)),
-                    Filter.BinaryFunction.Operator.EQUALS,
-                    Expression.Literal.create(DataType.INT64, "42")))
-            .build(),
-        apiConversionService.convertEntityFilter(
-            NAUTICAL_UNDERLAY_NAME, "sailors", apiEntityFilter));
+            EntityFilter.builder()
+                    .primaryEntity(EntityVariable.create(SAILOR, Variable.create("s")))
+                    .filter(
+                            Filter.BinaryFunction.create(
+                                    Expression.AttributeExpression.create(
+                                            AttributeVariable.create(SAILOR_RATING, sVar)),
+                                    Filter.BinaryFunction.Operator.EQUALS,
+                                    Expression.Literal.create(DataType.INT64, "42")))
+                    .build(),
+            apiConversionService.convertEntityFilter(
+                    NAUTICAL_UNDERLAY_NAME, "sailors", apiEntityFilter));
   }
 
   @Test
   void entityFilterUnknownNamesThrow() {
     NotFoundException bogusUnderlay =
-        assertThrows(
-            NotFoundException.class,
-            () ->
-                apiConversionService.convertEntityFilter(
-                    "bogus_underlay", "sailors", new ApiEntityFilter()));
+            assertThrows(
+                    NotFoundException.class,
+                    () ->
+                            apiConversionService.convertEntityFilter(
+                                    "bogus_underlay", "sailors", new ApiEntityFilter()));
     assertThat(bogusUnderlay.getMessage(), Matchers.containsString("No known underlay with name"));
     NotFoundException bogusEntity =
-        assertThrows(
-            NotFoundException.class,
-            () ->
-                apiConversionService.convertEntityFilter(
-                    NAUTICAL_UNDERLAY_NAME, "bogus_entity", new ApiEntityFilter()));
+            assertThrows(
+                    NotFoundException.class,
+                    () ->
+                            apiConversionService.convertEntityFilter(
+                                    NAUTICAL_UNDERLAY_NAME, "bogus_entity", new ApiEntityFilter()));
     assertThat(bogusEntity.getMessage(), Matchers.containsString("No known entity with name"));
   }
 
   @Test
   void entityDataset() {
     ApiEntityDataset apiEntityDataset =
-        new ApiEntityDataset()
-            .entityVariable("s")
-            .selectedAttributes(ImmutableList.of("name", "rating"))
-            .filter(
-                new ApiFilter()
-                    .binaryFilter(
-                        new ApiBinaryFilter()
-                            .attributeVariable(
-                                new ApiAttributeVariable().variable("s").name("rating"))
-                            .operator(ApiBinaryFilterOperator.EQUALS)
-                            .attributeValue(new ApiAttributeValue().int64Val(42L))));
+            new ApiEntityDataset()
+                    .entityVariable("s")
+                    .selectedAttributes(ImmutableList.of("name", "rating"))
+                    .filter(
+                            new ApiFilter()
+                                    .binaryFilter(
+                                            new ApiBinaryFilter()
+                                                    .attributeVariable(
+                                                            new ApiAttributeVariable().variable("s").name("rating"))
+                                                    .operator(ApiBinaryFilterOperator.EQUALS)
+                                                    .attributeValue(new ApiAttributeValue().int64Val(42L))));
 
     Variable sVar = Variable.create("s");
     assertEquals(
-        EntityDataset.builder()
-            .primaryEntity(EntityVariable.create(SAILOR, Variable.create("s")))
-            .selectedAttributes(ImmutableList.of(SAILOR_NAME, SAILOR_RATING))
-            .filter(
-                Filter.BinaryFunction.create(
-                    Expression.AttributeExpression.create(
-                        AttributeVariable.create(SAILOR_RATING, sVar)),
-                    Filter.BinaryFunction.Operator.EQUALS,
-                    Expression.Literal.create(DataType.INT64, "42")))
-            .build(),
-        apiConversionService.convertEntityDataset(
-            NAUTICAL_UNDERLAY_NAME, "sailors", apiEntityDataset));
+            EntityDataset.builder()
+                    .primaryEntity(EntityVariable.create(SAILOR, Variable.create("s")))
+                    .selectedAttributes(ImmutableList.of(SAILOR_NAME, SAILOR_RATING))
+                    .filter(
+                            Filter.BinaryFunction.create(
+                                    Expression.AttributeExpression.create(
+                                            AttributeVariable.create(SAILOR_RATING, sVar)),
+                                    Filter.BinaryFunction.Operator.EQUALS,
+                                    Expression.Literal.create(DataType.INT64, "42")))
+                    .build(),
+            apiConversionService.convertEntityDataset(
+                    NAUTICAL_UNDERLAY_NAME, "sailors", apiEntityDataset));
   }
 
   @Test
   void entityDatasetWithOrderBy() {
     ApiEntityDataset apiEntityDataset =
-        new ApiEntityDataset()
-            .entityVariable("s")
-            .selectedAttributes(ImmutableList.of("name", "rating"))
-            .orderByAttribute("name")
-            .orderByDirection(ApiOrderByDirection.DESC)
-            .filter(
-                new ApiFilter()
-                    .binaryFilter(
-                        new ApiBinaryFilter()
-                            .attributeVariable(
-                                new ApiAttributeVariable().variable("s").name("rating"))
-                            .operator(ApiBinaryFilterOperator.EQUALS)
-                            .attributeValue(new ApiAttributeValue().int64Val(42L))));
+            new ApiEntityDataset()
+                    .entityVariable("s")
+                    .selectedAttributes(ImmutableList.of("name", "rating"))
+                    .orderByAttribute("name")
+                    .orderByDirection(ApiOrderByDirection.DESC)
+                    .filter(
+                            new ApiFilter()
+                                    .binaryFilter(
+                                            new ApiBinaryFilter()
+                                                    .attributeVariable(
+                                                            new ApiAttributeVariable().variable("s").name("rating"))
+                                                    .operator(ApiBinaryFilterOperator.EQUALS)
+                                                    .attributeValue(new ApiAttributeValue().int64Val(42L))));
 
     Variable sVar = Variable.create("s");
     assertEquals(
-        EntityDataset.builder()
-            .primaryEntity(EntityVariable.create(SAILOR, Variable.create("s")))
-            .selectedAttributes(ImmutableList.of(SAILOR_NAME, SAILOR_RATING))
-            .orderByAttribute(SAILOR_NAME)
-            .orderByDirection(OrderByDirection.DESC)
-            .filter(
-                Filter.BinaryFunction.create(
-                    Expression.AttributeExpression.create(
-                        AttributeVariable.create(SAILOR_RATING, sVar)),
-                    Filter.BinaryFunction.Operator.EQUALS,
-                    Expression.Literal.create(DataType.INT64, "42")))
-            .build(),
-        apiConversionService.convertEntityDataset(
-            NAUTICAL_UNDERLAY_NAME, "sailors", apiEntityDataset));
+            EntityDataset.builder()
+                    .primaryEntity(EntityVariable.create(SAILOR, Variable.create("s")))
+                    .selectedAttributes(ImmutableList.of(SAILOR_NAME, SAILOR_RATING))
+                    .orderByAttribute(SAILOR_NAME)
+                    .orderByDirection(OrderByDirection.DESC)
+                    .filter(
+                            Filter.BinaryFunction.create(
+                                    Expression.AttributeExpression.create(
+                                            AttributeVariable.create(SAILOR_RATING, sVar)),
+                                    Filter.BinaryFunction.Operator.EQUALS,
+                                    Expression.Literal.create(DataType.INT64, "42")))
+                    .build(),
+            apiConversionService.convertEntityDataset(
+                    NAUTICAL_UNDERLAY_NAME, "sailors", apiEntityDataset));
   }
 
   @Test
   void entityDatasetUnknownNamesThrow() {
     NotFoundException bogusUnderlay =
-        assertThrows(
-            NotFoundException.class,
-            () ->
-                apiConversionService.convertEntityDataset(
-                    "bogus_underlay", "sailors", new ApiEntityDataset()));
+            assertThrows(
+                    NotFoundException.class,
+                    () ->
+                            apiConversionService.convertEntityDataset(
+                                    "bogus_underlay", "sailors", new ApiEntityDataset()));
     assertThat(bogusUnderlay.getMessage(), Matchers.containsString("No known underlay with name"));
     NotFoundException bogusEntity =
-        assertThrows(
-            NotFoundException.class,
-            () ->
-                apiConversionService.convertEntityDataset(
-                    NAUTICAL_UNDERLAY_NAME, "bogus_entity", new ApiEntityDataset()));
+            assertThrows(
+                    NotFoundException.class,
+                    () ->
+                            apiConversionService.convertEntityDataset(
+                                    NAUTICAL_UNDERLAY_NAME, "bogus_entity", new ApiEntityDataset()));
     assertThat(bogusEntity.getMessage(), Matchers.containsString("No known entity with name"));
   }
 
   @Test
   void entityDatasetNegativeLimitThrows() {
     ApiEntityDataset apiEntityDataset =
-        new ApiEntityDataset()
-            .entityVariable("s")
-            .selectedAttributes(ImmutableList.of("name", "rating"))
-            .limit(-1)
-            .filter(
-                new ApiFilter()
-                    .binaryFilter(
-                        new ApiBinaryFilter()
-                            .attributeVariable(
-                                new ApiAttributeVariable().variable("s").name("rating"))
-                            .operator(ApiBinaryFilterOperator.EQUALS)
-                            .attributeValue(new ApiAttributeValue().int64Val(42L))));
+            new ApiEntityDataset()
+                    .entityVariable("s")
+                    .selectedAttributes(ImmutableList.of("name", "rating"))
+                    .limit(-1)
+                    .filter(
+                            new ApiFilter()
+                                    .binaryFilter(
+                                            new ApiBinaryFilter()
+                                                    .attributeVariable(
+                                                            new ApiAttributeVariable().variable("s").name("rating"))
+                                                    .operator(ApiBinaryFilterOperator.EQUALS)
+                                                    .attributeValue(new ApiAttributeValue().int64Val(42L))));
 
     IllegalArgumentException invalidLimitException =
-        assertThrows(
-            IllegalArgumentException.class,
-            () ->
-                apiConversionService.convertEntityDataset(
-                    NAUTICAL_UNDERLAY_NAME, "sailors", apiEntityDataset));
+            assertThrows(
+                    IllegalArgumentException.class,
+                    () ->
+                            apiConversionService.convertEntityDataset(
+                                    NAUTICAL_UNDERLAY_NAME, "sailors", apiEntityDataset));
     assertThat(invalidLimitException.getMessage(), Matchers.containsString("The provided limit"));
   }
 
   @Test
   void entityDatasetWithLimit() {
     ApiEntityDataset apiEntityDataset =
-        new ApiEntityDataset()
-            .entityVariable("s")
-            .selectedAttributes(ImmutableList.of("name", "rating"))
-            .limit(1)
-            .filter(
-                new ApiFilter()
-                    .binaryFilter(
-                        new ApiBinaryFilter()
-                            .attributeVariable(
-                                new ApiAttributeVariable().variable("s").name("rating"))
-                            .operator(ApiBinaryFilterOperator.EQUALS)
-                            .attributeValue(new ApiAttributeValue().int64Val(42L))));
+            new ApiEntityDataset()
+                    .entityVariable("s")
+                    .selectedAttributes(ImmutableList.of("name", "rating"))
+                    .limit(1)
+                    .filter(
+                            new ApiFilter()
+                                    .binaryFilter(
+                                            new ApiBinaryFilter()
+                                                    .attributeVariable(
+                                                            new ApiAttributeVariable().variable("s").name("rating"))
+                                                    .operator(ApiBinaryFilterOperator.EQUALS)
+                                                    .attributeValue(new ApiAttributeValue().int64Val(42L))));
 
     Variable sVar = Variable.create("s");
     assertEquals(
-        EntityDataset.builder()
-            .primaryEntity(EntityVariable.create(SAILOR, Variable.create("s")))
-            .selectedAttributes(ImmutableList.of(SAILOR_NAME, SAILOR_RATING))
-            .limit(1)
-            .filter(
-                Filter.BinaryFunction.create(
-                    Expression.AttributeExpression.create(
-                        AttributeVariable.create(SAILOR_RATING, sVar)),
-                    Filter.BinaryFunction.Operator.EQUALS,
-                    Expression.Literal.create(DataType.INT64, "42")))
-            .build(),
-        apiConversionService.convertEntityDataset(
-            NAUTICAL_UNDERLAY_NAME, "sailors", apiEntityDataset));
+            EntityDataset.builder()
+                    .primaryEntity(EntityVariable.create(SAILOR, Variable.create("s")))
+                    .selectedAttributes(ImmutableList.of(SAILOR_NAME, SAILOR_RATING))
+                    .limit(1)
+                    .filter(
+                            Filter.BinaryFunction.create(
+                                    Expression.AttributeExpression.create(
+                                            AttributeVariable.create(SAILOR_RATING, sVar)),
+                                    Filter.BinaryFunction.Operator.EQUALS,
+                                    Expression.Literal.create(DataType.INT64, "42")))
+                    .build(),
+            apiConversionService.convertEntityDataset(
+                    NAUTICAL_UNDERLAY_NAME, "sailors", apiEntityDataset));
   }
 }
