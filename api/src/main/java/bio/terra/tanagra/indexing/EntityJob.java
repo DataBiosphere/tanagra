53e38d332198de43ab08f7f9a718c315052deb36
Mariko Medlock <mariko.medlock@gmail.com>
2022-09-26 14:30:24 -0400

    Move common code into EntityJob base class.

--- api/src/main/java/bio/terra/tanagra/indexing/EntityJob.java
+++ api/src/main/java/bio/terra/tanagra/indexing/EntityJob.java
@@ -1,6 +1,14 @@
 package bio.terra.tanagra.indexing;
 
+import bio.terra.tanagra.exception.InvalidConfigException;
+import bio.terra.tanagra.underlay.DataPointer;
 import bio.terra.tanagra.underlay.Entity;
+import bio.terra.tanagra.underlay.TablePointer;
+import bio.terra.tanagra.underlay.datapointer.BigQueryDataset;
+import bio.terra.tanagra.utils.GoogleBigQuery;
+import com.google.cloud.bigquery.Table;
+import com.google.cloud.bigquery.TableId;
+import java.util.Optional;
 
 public abstract class EntityJob implements IndexingJob {
   private final Entity entity;
@@ -17,5 +25,43 @@ public abstract class EntityJob implements IndexingJob {
 
   public Entity getEntity() {
     return entity;
+  }
+
+  protected BigQueryDataset getOutputDataPointer() {
+    TablePointer outputTable = getEntity().getIndexDataMapping().getTablePointer();
+    DataPointer outputDataPointer = outputTable.getDataPointer();
+    if (!(outputDataPointer instanceof BigQueryDataset)) {
+      throw new InvalidConfigException("Entity indexing job only supports BigQuery");
+    }
+    return (BigQueryDataset) outputDataPointer;
+  }
+
+  protected JobStatus checkTableExistenceForJobStatus(TablePointer outputTable) {
+    // Check if the table already exists. We don't expect this to be a long-running operation, so
+    // there is no IN_PROGRESS state for this job.
+    BigQueryDataset outputBQDataset = getOutputDataPointer();
+    GoogleBigQuery googleBigQuery = outputBQDataset.getBigQueryService();
+    Optional<Table> tableOpt =
+        googleBigQuery.getTable(
+            outputBQDataset.getProjectId(),
+            outputBQDataset.getDatasetId(),
+            outputTable.getTableName());
+    return tableOpt.isPresent() ? JobStatus.COMPLETE : JobStatus.NOT_STARTED;
+  }
+
+  protected void createTableFromSql(TablePointer outputTable, String sql, boolean isDryRun) {
+    BigQueryDataset outputBQDataset = getOutputDataPointer();
+    LOGGER.info(
+        "output BQ table: project={}, dataset={}, table={}",
+        outputBQDataset.getProjectId(),
+        outputBQDataset.getDatasetId(),
+        outputTable.getTableName());
+
+    TableId destinationTable =
+        TableId.of(
+            outputBQDataset.getProjectId(),
+            outputBQDataset.getDatasetId(),
+            outputTable.getTableName());
+    outputBQDataset.getBigQueryService().createTableFromQuery(destinationTable, sql, isDryRun);
   }
 }
--- api/src/main/java/bio/terra/tanagra/indexing/job/WriteParentChildIdPairs.java
+++ api/src/main/java/bio/terra/tanagra/indexing/job/WriteParentChildIdPairs.java
@@ -1,16 +1,9 @@
 package bio.terra.tanagra.indexing.job;
 
-import bio.terra.tanagra.exception.InvalidConfigException;
 import bio.terra.tanagra.indexing.EntityJob;
 import bio.terra.tanagra.query.SQLExpression;
-import bio.terra.tanagra.underlay.DataPointer;
 import bio.terra.tanagra.underlay.Entity;
 import bio.terra.tanagra.underlay.TablePointer;
-import bio.terra.tanagra.underlay.datapointer.BigQueryDataset;
-import bio.terra.tanagra.utils.GoogleBigQuery;
-import com.google.cloud.bigquery.Table;
-import com.google.cloud.bigquery.TableId;
-import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -48,54 +41,22 @@ public class WriteParentChildIdPairs extends EntityJob {
     String sql = selectChildParentIdPairs.renderSQL();
     LOGGER.info("select all child-parent id pairs SQL: {}", sql);
 
-    BigQueryDataset outputBQDataset = getOutputDataPointer();
     TablePointer outputTable =
         getEntity()
             .getIndexDataMapping()
             .getHierarchyMapping(hierarchyName)
             .getChildParent()
             .getTablePointer();
-    LOGGER.info(
-        "output BQ table: project={}, dataset={}, table={}",
-        outputBQDataset.getProjectId(),
-        outputBQDataset.getDatasetId(),
-        outputTable.getTableName());
-
-    TableId destinationTable =
-        TableId.of(
-            outputBQDataset.getProjectId(),
-            outputBQDataset.getDatasetId(),
-            outputTable.getTableName());
-    outputBQDataset.getBigQueryService().createTableFromQuery(destinationTable, sql, isDryRun);
+    createTableFromSql(outputTable, sql, isDryRun);
   }
 
   @Override
   public JobStatus checkStatus() {
-    // Check if the table already exists. We don't expect this to be a long-running operation, so
-    // there is no IN_PROGRESS state for this job.
-    BigQueryDataset outputBQDataset = getOutputDataPointer();
-    TablePointer outputTable =
+    return checkTableExistenceForJobStatus(
         getEntity()
             .getIndexDataMapping()
             .getHierarchyMapping(hierarchyName)
             .getChildParent()
-            .getTablePointer();
-    GoogleBigQuery googleBigQuery = outputBQDataset.getBigQueryService();
-    Optional<Table> tableOpt =
-        googleBigQuery.getTable(
-            outputBQDataset.getProjectId(),
-            outputBQDataset.getDatasetId(),
-            outputTable.getTableName());
-    return tableOpt.isPresent() ? JobStatus.COMPLETE : JobStatus.NOT_STARTED;
-  }
-
-  private BigQueryDataset getOutputDataPointer() {
-    TablePointer outputTable = getEntity().getIndexDataMapping().getTablePointer();
-    DataPointer outputDataPointer = outputTable.getDataPointer();
-    if (!(outputDataPointer instanceof BigQueryDataset)) {
-      throw new InvalidConfigException(
-          "WriteParentChildIdPairs indexing job only supports BigQuery");
-    }
-    return (BigQueryDataset) outputDataPointer;
+            .getTablePointer());
   }
 }
