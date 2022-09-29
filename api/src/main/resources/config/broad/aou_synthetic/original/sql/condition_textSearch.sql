a92f2f5c7964765afc9b85fa0fcfcf98a23b7ed7
Mariko Medlock <mariko.medlock@gmail.com>
2022-09-29 18:21:55 -0400

    Config for aou_synthetic person, condition, procedure entities.

---
+++ api/src/main/resources/config/broad/aou_synthetic/original/sql/condition_textSearch.sql
@@ -0,0 +1,30 @@
+SELECT textsearch.id, textsearch.text FROM (
+
+    SELECT
+      c.concept_id AS id, c.concept_name AS text
+    FROM  `broad-tanagra-dev.aou_synthetic_SR2019q4r4.concept` c
+
+    UNION ALL
+
+    SELECT
+      c.concept_id AS id, CAST(c.concept_id AS STRING) AS text
+    FROM  `broad-tanagra-dev.aou_synthetic_SR2019q4r4.concept` c
+
+    UNION ALL
+
+    SELECT
+      c.concept_id AS id, c.concept_code AS text
+    FROM  `broad-tanagra-dev.aou_synthetic_SR2019q4r4.concept` c
+
+    UNION ALL
+
+    SELECT
+      cs.concept_id AS id, cs.concept_synonym_name AS text
+    FROM  `broad-tanagra-dev.aou_synthetic_SR2019q4r4.concept_synonym` cs
+) AS textsearch
+
+JOIN `broad-tanagra-dev.aou_synthetic_SR2019q4r4.concept` c
+ON c.concept_id = textsearch.id
+
+WHERE c.domain_id = 'Condition'
+AND c.valid_end_date > DATE('2022-01-01')
