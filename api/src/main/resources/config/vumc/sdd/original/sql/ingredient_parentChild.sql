1007b1862785b727b5e8880d23bf4bb430b5c8b5
Mariko Medlock <mariko.medlock@gmail.com>
2022-10-02 18:18:50 -0400

    Remove sex_at_birth attribute from person entity.

--- api/src/main/resources/config/vumc/sdd/original/sql/ingredient_parentChild.sql
+++ api/src/main/resources/config/vumc/sdd/original/sql/ingredient_parentChild.sql
@@ -6,12 +6,27 @@ JOIN `victr-tanagra-test.sd_static.concept` c1  ON c1.concept_id = cr.concept_id
 JOIN `victr-tanagra-test.sd_static.concept` c2  ON c2.concept_id = cr.concept_id_2
 WHERE
   cr.relationship_id IN ('Has form', 'RxNorm - ATC', 'RxNorm - ATC name', 'Mapped from', 'Subsumes')
-  AND c1.domain_id = c2.domain_id
-  AND c2.domain_id = 'Drug'
   AND c1.concept_id != c2.concept_id
 
-  AND c1.vocabulary_id IN ('RxNorm', 'RxNorm Extension', 'ATC')
-  AND c2.vocabulary_id IN ('RxNorm', 'RxNorm Extension', 'ATC')
+  AND c1.domain_id = 'Drug'
+  AND ((c1.vocabulary_id IN ('RxNorm', 'RxNorm Extension')
+        AND c1.concept_class_id = 'Ingredient'
+        AND c1.standard_concept = 'S')
+    OR (c1.vocabulary_id = 'RxNorm'
+        AND c1.concept_class_id = 'Precise Ingredient')
+    OR (c1.vocabulary_id = 'ATC'
+        AND c1.concept_class_id IN ('ATC 1st', 'ATC 2nd', 'ATC 3rd', 'ATC 4th', 'ATC 5th')
+        AND c1.standard_concept = 'C'))
+
+  AND c2.domain_id = 'Drug'
+  AND ((c2.vocabulary_id IN ('RxNorm', 'RxNorm Extension')
+        AND c2.concept_class_id = 'Ingredient'
+        AND c2.standard_concept = 'S')
+    OR (c2.vocabulary_id = 'RxNorm'
+        AND c2.concept_class_id = 'Precise Ingredient')
+    OR (c2.vocabulary_id = 'ATC'
+        AND c2.concept_class_id IN ('ATC 1st', 'ATC 2nd', 'ATC 3rd', 'ATC 4th', 'ATC 5th')
+        AND c2.standard_concept = 'C'))
 
 UNION ALL
 
@@ -24,9 +39,23 @@ JOIN `victr-tanagra-test.sd_static.concept` c2  ON c2.concept_id = ca.descendant
 WHERE
   ca.min_levels_of_separation = 1
   AND ca.max_levels_of_separation = 1
-  AND c1.domain_id = c2.domain_id
-  AND c2.domain_id = 'Drug'
 
-  AND c1.vocabulary_id IN ('ATC')
-  AND c1.concept_class_id = 'ATC 4th'
-  AND c2.vocabulary_id IN ('RxNorm', 'RxNorm Extension')
+  AND c1.domain_id = 'Drug'
+  AND ((c1.vocabulary_id IN ('RxNorm', 'RxNorm Extension')
+        AND c1.concept_class_id = 'Ingredient'
+        AND c1.standard_concept = 'S')
+    OR (c1.vocabulary_id = 'RxNorm'
+        AND c1.concept_class_id = 'Precise Ingredient')
+    OR (c1.vocabulary_id = 'ATC'
+        AND c1.concept_class_id IN ('ATC 1st', 'ATC 2nd', 'ATC 3rd', 'ATC 4th', 'ATC 5th')
+        AND c1.standard_concept = 'C'))
+
+  AND c2.domain_id = 'Drug'
+  AND ((c2.vocabulary_id IN ('RxNorm', 'RxNorm Extension')
+        AND c2.concept_class_id = 'Ingredient'
+        AND c2.standard_concept = 'S')
+    OR (c2.vocabulary_id = 'RxNorm'
+        AND c2.concept_class_id = 'Precise Ingredient')
+    OR (c2.vocabulary_id = 'ATC'
+        AND c2.concept_class_id IN ('ATC 1st', 'ATC 2nd', 'ATC 3rd', 'ATC 4th', 'ATC 5th')
+        AND c2.standard_concept = 'C'))
