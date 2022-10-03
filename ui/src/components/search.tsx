1c6b0ef6f5a89a60bf3031d56dce16fb52a09dfd
Timothy Jennison <tjennison@google.com>
2022-10-03 13:03:54 -0400

    Add clear button to search boxes

--- ui/src/components/search.tsx
+++ ui/src/components/search.tsx
@@ -1,6 +1,8 @@
+import ClearIcon from "@mui/icons-material/Clear";
 import SearchIcon from "@mui/icons-material/Search";
 import Box from "@mui/material/Box";
 import IconButton from "@mui/material/IconButton";
+import InputAdornment from "@mui/material/InputAdornment";
 import Stack from "@mui/material/Stack";
 import { TextField } from "mui-rff";
 import { Form } from "react-final-form";
@@ -14,9 +16,8 @@ export function Search(props: SearchProps) {
   return (
     <Box m={1}>
       <Form
-        initialValues={{ query: "" }}
         onSubmit={({ query }) => props.onSearch(query)}
-        render={({ handleSubmit }) => (
+        render={({ handleSubmit, form }) => (
           <form onSubmit={handleSubmit}>
             <Stack direction="row" justifyContent="center" alignItems="center">
               <TextField
@@ -25,6 +26,22 @@ export function Search(props: SearchProps) {
                 name="query"
                 placeholder={props.placeholder}
                 autoComplete="off"
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        onClick={() => {
+                          form.reset();
+                          props.onSearch("");
+                        }}
+                        onMouseDown={(event) => event.preventDefault()}
+                        edge="end"
+                      >
+                        <ClearIcon />
+                      </IconButton>
+                    </InputAdornment>
+                  ),
+                }}
               />
               <IconButton type="submit">
                 <SearchIcon />
