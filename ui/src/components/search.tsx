353f318bc545d0c0ea2cd62fe0c931434df0f95e
Timothy Jennison <tjennison@google.com>
2022-10-05 09:48:44 -0400

    Proper "back" behavior in classification criteria
    * The in-app back button returns to the list view from the hierarchy
      view in classification criteria.
    * The browser bar/forward buttons work when searching and across both
      views.

--- ui/src/components/search.tsx
+++ ui/src/components/search.tsx
@@ -6,17 +6,35 @@ import InputAdornment from "@mui/material/InputAdornment";
 import Stack from "@mui/material/Stack";
 import { TextField } from "mui-rff";
 import { Form } from "react-final-form";
+import { useSearchParams } from "react-router-dom";
 
 export type SearchProps = {
   placeholder?: string;
-  onSearch: (query: string) => void;
+  onSearch?: (query: string) => void;
+  initialValue?: string;
+  searchKey?: string;
 };
 
 export function Search(props: SearchProps) {
+  const [, setSearchParams] = useSearchParams();
+
+  const onSearch = (query: string) => {
+    if (props.onSearch) {
+      props.onSearch(query);
+    } else {
+      const sk = props.searchKey ?? "search";
+      setSearchParams((params) => {
+        params.set(sk, query);
+        return params;
+      });
+    }
+  };
+
   return (
     <Box m={1}>
       <Form
-        onSubmit={({ query }) => props.onSearch(query)}
+        initialValues={{ query: props.initialValue }}
+        onSubmit={({ query }) => onSearch(query)}
         render={({ handleSubmit, form }) => (
           <form onSubmit={handleSubmit}>
             <Stack direction="row" justifyContent="center" alignItems="center">
@@ -32,7 +50,7 @@ export function Search(props: SearchProps) {
                       <IconButton
                         onClick={() => {
                           form.reset();
-                          props.onSearch("");
+                          onSearch("");
                         }}
                         onMouseDown={(event) => event.preventDefault()}
                         edge="end"
