954c346715d9cd2760da8abf5d5f88238257165d
Erik J. Olson <erik.j.olson@vumc.org>
2022-10-31 08:12:26 -0500

    Create Admin UI blocks in api spec

--- api/src/main/java/bio/terra/tanagra/app/controller/AdminController.java
+++ api/src/main/java/bio/terra/tanagra/app/controller/AdminController.java
@@ -1,12 +1,15 @@
 package bio.terra.tanagra.app.controller;
 
+import bio.terra.tanagra.generated.controller.AdminApi;
+import bio.terra.tanagra.generated.model.ApiListWorkspacesResponse;
 import bio.terra.tanagra.plugin.PluginService;
 import bio.terra.tanagra.plugin.accesscontrol.IAccessControlPlugin;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
 @Controller
-public class AdminController {
+public class AdminController implements AdminApi {
   private final PluginService pluginService;
 
   @Autowired
@@ -16,5 +19,11 @@ public class AdminController {
 
   public void stubEndpoint() {
     this.pluginService.getPlugin(IAccessControlPlugin.class).grantAccess(null, null);
+  }
+
+  @Override
+  public ResponseEntity<ApiListWorkspacesResponse> listWorkspaces(
+      Integer pageSize, String pageToken) {
+    return AdminApi.super.listWorkspaces(pageSize, pageToken);
   }
 }
