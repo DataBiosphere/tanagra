ed2d1e2070d39b4fa09ae4bf41b0697e5f8f256b
Erik J. Olson <erik.j.olson@vumc.org>
2022-11-29 17:28:43 -0600

    Add underlay pluggable configuration

---
+++ underlay/src/main/java/bio/terra/tanagra/plugin/PluginConfig.java
@@ -0,0 +1,37 @@
+package bio.terra.tanagra.plugin;
+
+import java.util.Map;
+
+import bio.terra.tanagra.exception.InvalidConfigException;
+import bio.terra.tanagra.serialization.UFPluginConfig;
+import com.google.common.base.Strings;
+
+public class PluginConfig {
+  private final String type;
+  private final Map<String, String> parameters;
+
+  public PluginConfig(String type, Map<String, String> parameters) {
+    this.type = type;
+    this.parameters = parameters;
+  }
+
+  public String getType() {
+    return this.type;
+  }
+
+  public Map<String, String> getParameters() {
+    return this.parameters;
+  }
+
+  public String getValue(String key) {
+    return this.parameters.get(key);
+  }
+
+  public static PluginConfig fromSerialized(UFPluginConfig serialized) {
+    if (Strings.isNullOrEmpty(serialized.getType())) {
+      throw new InvalidConfigException("Plugin type is undefined");
+    }
+
+    return new PluginConfig(serialized.getType(), serialized.getParameters());
+  }
+}
