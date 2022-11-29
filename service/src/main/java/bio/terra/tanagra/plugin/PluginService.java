cc6d359d7d307a2c5db74c0c972d54390156f1a5
Erik J. Olson <erik.j.olson@vumc.org>
2022-11-29 14:46:28 -0600

    Use enum for plugin defaults

--- service/src/main/java/bio/terra/tanagra/plugin/PluginService.java
+++ service/src/main/java/bio/terra/tanagra/plugin/PluginService.java
@@ -1,9 +1,8 @@
 package bio.terra.tanagra.plugin;
 
-import static org.springframework.util.StringUtils.capitalize;
-
 import bio.terra.tanagra.service.jdbc.DataSourceFactory;
 import bio.terra.tanagra.service.jdbc.DataSourceId;
+import java.util.HashMap;
 import java.util.Map;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,39 +14,45 @@ public class PluginService {
 
   private final PluggableConfiguration configuration;
   private final DataSourceFactory dataSourceFactory;
-  private final Map<String, Plugin> availablePlugins;
+  private final Map<Class<?>, Plugin> availablePlugins = new HashMap<>();
 
   @Autowired
   public PluginService(PluggableConfiguration configuration, DataSourceFactory dataSourceFactory)
       throws PluginException {
     this.configuration = configuration;
     this.dataSourceFactory = dataSourceFactory;
-    this.availablePlugins = PluginDefaults.getDefaultPlugins();
 
+    loadDefaultPlugins();
     loadPlugins();
   }
 
   public <T extends Plugin> T getPlugin(Class<T> c) {
-    return c.cast(availablePlugins.get(c.getSimpleName()));
+    return c.cast(availablePlugins.get(c));
+  }
+
+  private void loadDefaultPlugins() throws PluginException {
+    for (PluginType pluginType : PluginType.values()) {
+      availablePlugins.put(pluginType.getType(), loadPlugin(pluginType.getDefaultType()));
+    }
   }
 
   private void loadPlugins() throws PluginException {
     try {
       if (configuration.isConfigured()) {
         for (Map.Entry<String, PluginConfig> p : configuration.getPlugins().entrySet()) {
-          String key = capitalize(p.getKey());
+          PluginType pluginType = PluginType.valueOf(p.getKey());
           PluginConfig config = p.getValue();
           DataSource dataSource = getDataSource(config.getValue(PLUGIN_DATASOURCE_PARAMETER));
 
           Class<?> pluginClass = Class.forName(p.getValue().getType());
-          Plugin plugin = (Plugin) pluginClass.getConstructor().newInstance();
+          Plugin plugin = loadPlugin(pluginClass);
 
           plugin.init(config, dataSource);
 
-          availablePlugins.put(key, plugin);
+          availablePlugins.put(pluginType.getType(), plugin);
         }
       }
-    } catch (Exception e) {
+    } catch (ClassNotFoundException e) {
       throw new PluginException(e);
     }
   }
@@ -61,5 +66,13 @@ public class PluginService {
     }
 
     return dataSource;
+  }
+
+  private Plugin loadPlugin(Class<?> pluginClass) throws PluginException {
+    try {
+      return (Plugin) pluginClass.getConstructor().newInstance();
+    } catch (Exception e) {
+      throw new PluginException(e);
+    }
   }
 }
---
+++ service/src/main/java/bio/terra/tanagra/plugin/PluginType.java
@@ -0,0 +1,27 @@
+package bio.terra.tanagra.plugin;
+
+import bio.terra.tanagra.service.accesscontrol.AccessControlPlugin;
+import bio.terra.tanagra.service.accesscontrol.OpenAccessControlPlugin;
+import bio.terra.tanagra.service.identity.IdentityPlugin;
+import bio.terra.tanagra.service.identity.SingleUserIdentityPlugin;
+
+public enum PluginType {
+  ACCESS_CONTROL(AccessControlPlugin.class, OpenAccessControlPlugin.class),
+  IDENTITY(IdentityPlugin.class, SingleUserIdentityPlugin.class);
+
+  private final Class<?> type;
+  private final Class<?> defaultType;
+
+  <TT extends Plugin, TD extends TT> PluginType(Class<TT> type, Class<TD> defaultType) {
+    this.type = type;
+    this.defaultType = defaultType;
+  }
+
+  public Class<?> getType() {
+    return type;
+  }
+
+  public Class<?> getDefaultType() {
+    return defaultType;
+  }
+}
