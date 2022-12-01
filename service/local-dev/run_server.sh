bafe32ffb2fc3f210bf0d240bcfa9767020df6a5
Mariko Medlock <mariko.medlock@gmail.com>
2022-12-01 13:33:11 -0500

    Log db migration flags. Remove verily underlays from default spring properties.

--- service/local-dev/run_server.sh
+++ service/local-dev/run_server.sh
@@ -5,6 +5,7 @@ export TANAGRA_DB_INITIALIZE_ON_START=true
 export TANAGRA_DB_USERNAME=dbuser
 export TANAGRA_DB_PASSWORD=dbpwd
 
+export TANAGRA_UNDERLAY_FILES=broad/aou_synthetic/expanded/aou_synthetic.json,broad/cms_synpuf/expanded/cms_synpuf.json
 export TANAGRA_FEATURE_ARTIFACT_STORAGE_ENABLED=true
 
 ./gradlew bootRun
