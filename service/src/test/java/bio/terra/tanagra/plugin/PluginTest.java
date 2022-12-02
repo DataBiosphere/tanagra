40ddc8525c2e1f0fd31e17d5ebd414830f052898
Erik J. Olson <erik.j.olson@vumc.org>
2022-12-02 11:37:04 -0600

    Add test for default plugins

--- service/src/test/java/bio/terra/tanagra/plugin/PluginTest.java
+++ service/src/test/java/bio/terra/tanagra/plugin/PluginTest.java
@@ -3,6 +3,8 @@ package bio.terra.tanagra.plugin;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import bio.terra.tanagra.service.accesscontrol.AccessControlPlugin;
+import bio.terra.tanagra.service.accesscontrol.OpenAccessControlPlugin;
 import bio.terra.tanagra.testing.BaseSpringUnitTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,13 +17,20 @@ public class PluginTest extends BaseSpringUnitTest {
   @Autowired private PluginService pluginService;
 
   @Test
-  void pluginLoading() {
+  void defaultPluginLoading() {
+    AccessControlPlugin accessControlPlugin =
+        pluginService.getPlugin(TEST_UNDERLAY, AccessControlPlugin.class);
+    assertTrue(accessControlPlugin instanceof OpenAccessControlPlugin);
+  }
+
+  @Test
+  void configuredPluginLoading() {
     TestPlugin testPlugin = pluginService.getPlugin(TEST_UNDERLAY, TestPlugin.class);
     assertTrue(testPlugin instanceof TestPluginExternalImplementation);
   }
 
   @Test
-  void pluginParameters() {
+  void parameters() {
     TestPlugin test = pluginService.getPlugin(TEST_UNDERLAY, TestPlugin.class);
     assertEquals("configured value", test.getParameter("test"));
   }
