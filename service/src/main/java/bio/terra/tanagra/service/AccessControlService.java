c4151754e779684d685ee6d7085d03d97b00928c
Erik J. Olson <erik.j.olson@vumc.org>
2022-12-02 18:19:30 -0600

    Remove plugins from AccessControlService

--- service/src/main/java/bio/terra/tanagra/service/AccessControlService.java
+++ service/src/main/java/bio/terra/tanagra/service/AccessControlService.java
@@ -2,28 +2,16 @@ package bio.terra.tanagra.service;
 
 import bio.terra.common.exception.BadRequestException;
 import bio.terra.common.exception.UnauthorizedException;
-import bio.terra.tanagra.plugin.PluginService;
-import bio.terra.tanagra.service.accesscontrol.AccessControlPlugin;
 import bio.terra.tanagra.service.accesscontrol.Action;
 import bio.terra.tanagra.service.accesscontrol.ResourceId;
 import bio.terra.tanagra.service.accesscontrol.ResourceIdCollection;
 import bio.terra.tanagra.service.accesscontrol.ResourceType;
 import bio.terra.tanagra.service.accesscontrol.UserId;
-import bio.terra.tanagra.service.identity.IdentityPlugin;
 import javax.annotation.Nullable;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AccessControlService {
-
-  private final PluginService pluginService;
-
-  @Autowired
-  public AccessControlService(PluginService pluginService) {
-    this.pluginService = pluginService;
-  }
-
   public void throwIfUnauthorized(Object credential, Action action, ResourceType resourceType) {
     throwIfUnauthorized(credential, action, resourceType, null);
   }
@@ -33,11 +21,7 @@ public class AccessControlService {
       Action action,
       ResourceType resourceType,
       @Nullable ResourceId resourceId) {
-    // TODO: Update signature to take underlayName parameter once all routes have been configured to
-    // require an underlay.
-    String underlayName = "";
-
-    if (!isAuthorized(underlayName, credential, action, resourceType, resourceId)) {
+    if (!isAuthorized(credential, action, resourceType, resourceId)) {
       throw new UnauthorizedException(
           "User is unauthorized to "
               + action
@@ -49,12 +33,11 @@ public class AccessControlService {
   }
 
   public boolean isAuthorized(
-      String underlayName,
       Object credential,
       Action action,
       ResourceType resourceType,
       @Nullable ResourceId resourceId) {
-    UserId userId = getUserId(underlayName, credential);
+    UserId userId = getUserId(credential);
     if (userId == null) {
       throw new BadRequestException("Invalid user id");
     }
@@ -63,13 +46,13 @@ public class AccessControlService {
           "Action not available for resource type: " + action + ", " + resourceType);
     }
 
-    return pluginService
-        .getPlugin(underlayName, AccessControlPlugin.class)
-        .isAuthorized(userId, action, resourceType, resourceId);
+    // TODO: check authorization via AccessControlPlugin
+    return true;
   }
 
-  public UserId getUserId(String underlayName, Object credential) {
-    return pluginService.getPlugin(underlayName, IdentityPlugin.class).getUserId(credential);
+  public UserId getUserId(Object credential) {
+    // TODO: get identity via IdentityPlugin
+    return new UserId("single_user");
   }
 
   public ResourceIdCollection listResourceIds(ResourceType type) {
@@ -77,9 +60,7 @@ public class AccessControlService {
   }
 
   public ResourceIdCollection listResourceIds(ResourceType type, int offset, int limit) {
-    // TODO: specify underlay
-    return pluginService
-        .getPlugin("", AccessControlPlugin.class)
-        .listResourceIds(type, offset, limit);
+    // TODO: check authorization via AccessControlPlugin
+    return ResourceIdCollection.allResourceIds();
   }
 }
