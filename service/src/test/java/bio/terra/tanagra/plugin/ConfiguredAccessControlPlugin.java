36bf1cdd4462124aaa2473af52f7024d55bb6806
Erik J. Olson <erik.j.olson@vumc.org>
2022-12-06 09:53:41 -0600

    Update plugin tests

---
+++ service/src/test/java/bio/terra/tanagra/plugin/ConfiguredAccessControlPlugin.java
@@ -0,0 +1,32 @@
+package bio.terra.tanagra.plugin;
+
+import bio.terra.tanagra.service.accesscontrol.AccessControlPlugin;
+import bio.terra.tanagra.service.accesscontrol.Action;
+import bio.terra.tanagra.service.accesscontrol.ResourceId;
+import bio.terra.tanagra.service.accesscontrol.ResourceIdCollection;
+import bio.terra.tanagra.service.accesscontrol.ResourceType;
+import bio.terra.tanagra.service.accesscontrol.UserId;
+import javax.annotation.Nullable;
+
+public class ConfiguredAccessControlPlugin implements AccessControlPlugin {
+  @Override
+  public void init(PluginConfig config) {
+    // do nothing
+  }
+
+  @Override
+  public String getDescription() {
+    return null;
+  }
+
+  @Override
+  public boolean isAuthorized(
+      UserId userId, Action action, ResourceType resourceType, @Nullable ResourceId resourceId) {
+    return false;
+  }
+
+  @Override
+  public ResourceIdCollection listResourceIds(ResourceType type, int offset, int limit) {
+    return null;
+  }
+}
--- service/src/test/java/bio/terra/tanagra/plugin/PluginTest.java
+++ service/src/test/java/bio/terra/tanagra/plugin/PluginTest.java
@@ -11,14 +11,22 @@ import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles("test")
 public class PluginTest extends BaseSpringUnitTest {
-  private static final String TEST_UNDERLAY = "test";
+  private static final String TEST_UNDERLAY_DEFAULT = "test-no-plugins";
+  private static final String TEST_UNDERLAY_CONFIGURED = "test-plugins-configured";
 
   @Autowired private PluginService pluginService;
 
   @Test
   void defaultPluginLoading() {
     AccessControlPlugin accessControlPlugin =
-        pluginService.getPlugin(TEST_UNDERLAY, AccessControlPlugin.class);
+        pluginService.getPlugin(TEST_UNDERLAY_DEFAULT, AccessControlPlugin.class);
     assertTrue(accessControlPlugin instanceof OpenAccessControlPlugin);
+  }
+
+  @Test
+  void configuredPluginLoading() {
+    AccessControlPlugin accessControlPlugin =
+        pluginService.getPlugin(TEST_UNDERLAY_CONFIGURED, AccessControlPlugin.class);
+    assertTrue(accessControlPlugin instanceof ConfiguredAccessControlPlugin);
   }
 }
