774e71305ef1d81f233a4b5a0a3476dbb1cf9037
melissachang <10929390+melissachang@users.noreply.github.com>
2023-01-24 09:02:49 -0800

    Add snp entity, snp_person entity group (#329)
    * snp
    
    Main.run.xml
    
    fix null year_of_birth
    
    initial
    
    change snp entity to use group by
    
    fix
    
    * vumc
    
    * update goldens

--- service/src/test/resources/sql/sdd/note-noFilter.sql
+++ service/src/test/resources/sql/sdd/note-noFilter.sql
@@ -1,1 +1,1 @@
-SELECT n.concept_code AS concept_code, n.id AS id, n.name AS name, n.vocabulary AS vocabulary FROM `verily-tanagra-dev.sdstatic_index_011823`.note AS n LIMIT 30
+SELECT n.concept_code AS concept_code, n.id AS id, n.name AS name, n.vocabulary AS vocabulary FROM `verily-tanagra-dev.sdstatic_index_011923`.note AS n LIMIT 30
