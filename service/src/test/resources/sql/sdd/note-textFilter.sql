7ee554c17a40f00ba3b78d582bde80c054ff9e76
melissachang <10929390+melissachang@users.noreply.github.com>
2023-02-06 13:13:43 -0800

    [SDD] Add BioVU attributes to person entity (#336)
    * For SDD, add BioVU columns to person table
    
    * update goldens
    
    * typo
    
    * get rid of duplicate rows in x_biovu_sample_status
    
    * comment

--- service/src/test/resources/sql/sdd/note-textFilter.sql
+++ service/src/test/resources/sql/sdd/note-textFilter.sql
@@ -1,1 +1,1 @@
-SELECT n.concept_code AS concept_code, n.id AS id, n.name AS name, n.vocabulary AS vocabulary FROM `verily-tanagra-dev.sdstatic_index_011923`.note AS n WHERE CONTAINS_SUBSTR(n.text, 'admis') LIMIT 30
+SELECT n.concept_code AS concept_code, n.id AS id, n.name AS name, n.vocabulary AS vocabulary FROM `verily-tanagra-dev.sdstatic_index_020123`.note AS n WHERE CONTAINS_SUBSTR(n.text, 'admis') LIMIT 30
