ee37087f64c3f5e35b67f15a1b61b85b65f0142d
melissachang <10929390+melissachang@users.noreply.github.com>
2023-02-13 10:30:38 -0800

    GHA for keeping configs in sync

--- .github/tools/verify_config_in_sync.sh
+++ .github/tools/verify_config_in_sync.sh
@@ -6,9 +6,9 @@
 
 
 configs_to_compare_list="
-broad/cms_synpuf verily/cms_synpuf\n
-broad/aou_synthetic verily/aou_synthetic\n
-vumc/sdd verily/sdd
+cms_synpuf broad/cms_synpuf verily/cms_synpuf\n
+aou_synthetic broad/aou_synthetic verily/aou_synthetic\n
+sdd vumc/sdd verily/sdd
 "
 
 # Needed for for loop to split only on newline, not on space
@@ -18,19 +18,31 @@ IFS='
 exit_code=0
 for configs_to_compare in $(echo -e ${configs_to_compare_list})
 do
-  underlay_1=$(echo ${configs_to_compare} | awk '{print $1}')
-  underlay_2=$(echo ${configs_to_compare} | awk '{print $2}')
-  echo "Comparing ${underlay_1} to ${underlay_2}"
+  underlay_name=$(echo ${configs_to_compare} | awk '{print $1}')
+  underlay_1=$(echo ${configs_to_compare} | awk '{print $2}')
+  underlay_2=$(echo ${configs_to_compare} | awk '{print $3}')
+  printf "\nComparing ${underlay_1} to ${underlay_2}\n"
 
-  underlay_dir_1=$(echo service/src/main/resources/config/${underlay_1})
-  underlay_dir_2=$(echo service/src/main/resources/config/${underlay_2})
-  diff_output=$(diff -rq ${underlay_dir_1} ${underlay_dir_2})
+  underlay_dir_1=$(echo service/src/main/resources/config/${underlay_1}/original)
+  underlay_dir_2=$(echo service/src/main/resources/config/${underlay_2}/original)
+  diff_output=$(diff -rq --exclude ${underlay_name}.json --exclude sql ${underlay_dir_1} ${underlay_dir_2})
 
   if [[ $(echo ${diff_output} | wc -c) -gt 1 ]]
   then
-    echo "${underlay_1} and ${underlay_2} are different:"
-    echo "${diff_output}"
-    printf "Please update and add files to your PR.\n\n"
+    printf "Differences found:\n${diff_output}\n"
+    printf "To see differences, run:\ndiff -r --exclude ${underlay_name}.json --exclude sql ${underlay_dir_1} ${underlay_dir_2}\n"
+    printf "Please update files and add to your PR.\n"
+    exit_code=1
+  fi
+
+  # Ignore legitimate differences in SQL files
+  sql_diff_output=$(diff -rq --ignore-matching-lines '[FROM|JOIN|dataflowServiceAccountEmail]' ${underlay_dir_1}/sql ${underlay_dir_2}/sql)
+
+  if [[ $(echo ${sql_diff_output} | wc -c) -gt 1 ]]
+  then
+    printf "Differences found:\n${sql_diff_output}\n"
+    printf "To see differences, run:\ndiff -r --ignore-matching-lines '[FROM|JOIN|dataflowServiceAccountEmail]' ${underlay_dir_1}/sql ${underlay_dir_2}/sql)\n"
+    printf "Please update files and add to your PR.\n"
     exit_code=1
   fi
 done
