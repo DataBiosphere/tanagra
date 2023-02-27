c339b7b7ff56f0ccb4d51134d8ebd7a0a41fa98d
melissachang <10929390+melissachang@users.noreply.github.com>
2023-02-27 11:53:54 -0800

    new index

--- service/src/test/resources/sql/sdd/snp-textFilter.sql
+++ service/src/test/resources/sql/sdd/snp-textFilter.sql
@@ -1,1 +1,1 @@
-SELECT s.id AS id FROM `verily-tanagra-dev.sdstatic_index_020123`.snp AS s WHERE CONTAINS_SUBSTR(s.id, 'RS1292') LIMIT 30
+SELECT s.id AS id FROM `verily-tanagra-dev.sdstatic_index_022723`.snp AS s WHERE CONTAINS_SUBSTR(s.id, 'RS1292') LIMIT 30
