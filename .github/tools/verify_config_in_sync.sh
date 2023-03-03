86af13d4fb1ac79b6af3454d1a2c278d7d3a89ad
melissachang <10929390+melissachang@users.noreply.github.com>
2023-03-03 11:00:26 -0800

    vumc/sdd

--- .github/tools/verify_config_in_sync.sh
+++ .github/tools/verify_config_in_sync.sh
@@ -25,7 +25,7 @@ do
 
   underlay_dir_1=$(echo service/src/main/resources/config/${underlay_1}/original)
   underlay_dir_2=$(echo service/src/main/resources/config/${underlay_2}/original)
-  diff_output=$(diff -rq --exclude ${underlay_name}.json --exclude sql ${underlay_dir_1} ${underlay_dir_2})
+  diff_output=$(diff -rq --ignore-all-space --exclude ${underlay_name}.json --exclude sql ${underlay_dir_1} ${underlay_dir_2})
 
   if [[ $(echo ${diff_output} | wc -c) -gt 1 ]]
   then
@@ -36,7 +36,7 @@ do
   fi
 
   # Ignore legitimate differences in SQL files
-  sql_diff_output=$(diff -rq --ignore-matching-lines '[FROM|JOIN|dataflowServiceAccountEmail]' ${underlay_dir_1}/sql ${underlay_dir_2}/sql)
+  sql_diff_output=$(diff -rq --ignore-all-space --ignore-matching-lines '[FROM|JOIN|dataflowServiceAccountEmail]' ${underlay_dir_1}/sql ${underlay_dir_2}/sql)
 
   if [[ $(echo ${sql_diff_output} | wc -c) -gt 1 ]]
   then
