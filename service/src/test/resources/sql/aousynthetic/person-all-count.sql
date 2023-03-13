3bae40d090e4ad3e01cca372ff7d648198ff5102
melissachang <10929390+melissachang@users.noreply.github.com>
2023-03-13 13:27:44 -0700

    Fix broad aou_synthetic ingredient concept sets not working (#355)
    * Use new index for broad aou_synthetic
    
    * comment
    
    * fix service:test

--- service/src/test/resources/sql/aousynthetic/person-all-count.sql
+++ service/src/test/resources/sql/aousynthetic/person-all-count.sql
@@ -1,1 +1,1 @@
-SELECT COUNT(p.id) AS t_count FROM `broad-tanagra-dev.aousynthetic_index_011523`.person AS p
+SELECT COUNT(p.id) AS t_count FROM `broad-tanagra-dev.aousynthetic_index_031323`.person AS p
--- service/src/test/resources/sql/aousynthetic/visit-noFilter.sql
+++ service/src/test/resources/sql/aousynthetic/visit-noFilter.sql
@@ -1,1 +1,1 @@
-SELECT v.id AS id, v.name AS name FROM `broad-tanagra-dev.aousynthetic_index_011523`.visit AS v LIMIT 30
+SELECT v.id AS id, v.name AS name FROM `broad-tanagra-dev.aousynthetic_index_031323`.visit AS v LIMIT 30
