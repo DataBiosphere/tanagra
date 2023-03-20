c3bdf3e6c3fdf9b9a599382ac6723c04abb8ed18
melissachang <10929390+melissachang@users.noreply.github.com>
2023-03-20 13:29:58 -0700

    wip

--- service/src/main/resources/config/verily/sdd/original/sql/ingredient_all.sql
+++ service/src/main/resources/config/verily/sdd/original/sql/ingredient_all.sql
@@ -6,7 +6,8 @@ AND ((c.vocabulary_id IN ('RxNorm', 'RxNorm Extension')
         AND c.concept_class_id = 'Ingredient'
         AND c.standard_concept = 'S')
     OR (c.vocabulary_id = 'RxNorm'
-        AND c.concept_class_id = 'Precise Ingredient')
+        AND (c.concept_class_id = 'Precise Ingredient'
+            OR c.concept_class_id = 'Branded Drug'))
     OR (c.vocabulary_id = 'ATC'
         AND c.concept_class_id IN ('ATC 1st', 'ATC 2nd', 'ATC 3rd', 'ATC 4th', 'ATC 5th')
         AND c.standard_concept = 'C'))
