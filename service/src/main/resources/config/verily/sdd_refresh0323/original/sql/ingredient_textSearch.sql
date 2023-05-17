6152e6d30453ab9caa861f9cc1fad169b146342b
Mariko Medlock <marikomedlock@google.com>
2023-05-17 09:27:55 -0400

    Original and expanded verily/sdd_refresh0323 underlay.

---
+++ service/src/main/resources/config/verily/sdd_refresh0323/original/sql/ingredient_textSearch.sql
@@ -0,0 +1,31 @@
+SELECT textsearch.id, textsearch.text FROM (
+
+    SELECT
+      c.concept_id AS id, c.concept_name AS text
+    FROM  `victr-tanagra-test.sd_20230328.concept` c
+
+    UNION ALL
+
+    SELECT
+      c.concept_id AS id, CAST(c.concept_id AS STRING) AS text
+    FROM  `victr-tanagra-test.sd_20230328.concept` c
+
+    UNION ALL
+
+    SELECT
+      c.concept_id AS id, c.concept_code AS text
+    FROM  `victr-tanagra-test.sd_20230328.concept` c
+) AS textsearch
+
+JOIN `victr-tanagra-test.sd_20230328.concept` c
+ON c.concept_id = textsearch.id
+
+WHERE c.domain_id = 'Drug'
+AND ((c.vocabulary_id IN ('RxNorm', 'RxNorm Extension')
+        AND c.concept_class_id = 'Ingredient'
+        AND c.standard_concept = 'S')
+    OR (c.vocabulary_id = 'RxNorm'
+        AND c.concept_class_id = 'Precise Ingredient')
+    OR (c.vocabulary_id = 'ATC'
+        AND c.concept_class_id IN ('ATC 1st', 'ATC 2nd', 'ATC 3rd', 'ATC 4th', 'ATC 5th')
+        AND c.standard_concept = 'C'))
---
+++ service/src/main/resources/config/verily/sdd_refresh0323/original/sql/x_snp.sql
@@ -0,0 +1,2 @@
+SELECT DISTINCT snp_id FROM `victr-tanagra-test.sd_20230328.x_snp`
+ORDER BY snp_id
