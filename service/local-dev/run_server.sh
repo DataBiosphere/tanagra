6d12c40db86f04d65c632cddd7de5f1e096e2865
Chenchal Subraveti <c.subraveti@vumc.org>
2023-06-02 14:00:10 -0500

    set export TANAGRA_DB_INITIALIZE_ON_START=true

--- service/local-dev/run_server.sh
+++ service/local-dev/run_server.sh
@@ -21,7 +21,7 @@ while getopts ":av" arg; do
 done
 
 export TANAGRA_DATABASE_NAME=tanagra_db
-export TANAGRA_DB_INITIALIZE_ON_START=false
+export TANAGRA_DB_INITIALIZE_ON_START=true
 export TANAGRA_DB_USERNAME=dbuser
 export TANAGRA_DB_PASSWORD=dbpwd
 
