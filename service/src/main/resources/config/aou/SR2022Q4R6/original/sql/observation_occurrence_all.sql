3e28433942e986af97e090cc2348650c54fdfdc0
Chenchal Subraveti <c.subraveti@vumc.org>
2023-06-12 11:28:58 -0500

    Added new starter underlay for aor synthetic SR2022Q4R6 dataset

---
+++ service/src/main/resources/config/aou/SR2022Q4R6/original/sql/observation_occurrence_all.sql
@@ -0,0 +1,14 @@
+SELECT
+  o.observation_id AS id, o.person_id, o.observation_concept_id,
+  o.observation_date, o.value_as_string, o.value_as_concept_id, o.unit_concept_id,
+  o.observation_source_value AS source_value, o.observation_source_concept_id AS source_criteria_id,
+  CAST(FLOOR(TIMESTAMP_DIFF(o.observation_datetime, p.birth_datetime, DAY) / 365.25) AS INT64) AS age_at_occurrence,
+  o.visit_occurrence_id, vo.visit_concept_id
+
+FROM `all-of-us-ehr-dev.SR2022Q4R6.observation` AS o
+
+JOIN `all-of-us-ehr-dev.SR2022Q4R6.person` AS p
+ON p.person_id = o.person_id
+
+LEFT JOIN `all-of-us-ehr-dev.SR2022Q4R6.visit_occurrence` AS vo
+ON vo.visit_occurrence_id = o.visit_occurrence_id
