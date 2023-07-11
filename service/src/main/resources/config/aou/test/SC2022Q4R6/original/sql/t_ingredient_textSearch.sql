bbd6c0fd76b1e063313e293fbf02012c8d842a45
Chenchal Subraveti <c.subraveti@vumc.org>
2023-07-10 19:50:17 -0500

    updated underlays for SR2022Q4R6 and SC2022Q4R6

---
+++ service/src/main/resources/config/aou/test/SC2022Q4R6/original/sql/t_ingredient_textSearch.sql
@@ -0,0 +1,31 @@
+SELECT textsearch.id, textsearch.text FROM (
+
+    SELECT
+      c.concept_id AS id, c.concept_name AS text
+    FROM  `all-of-us-ehr-dev.SC2022Q4R6.concept` c
+
+    UNION ALL
+
+    SELECT
+      c.concept_id AS id, CAST(c.concept_id AS STRING) AS text
+    FROM  `all-of-us-ehr-dev.SC2022Q4R6.concept` c
+
+    UNION ALL
+
+    SELECT
+      c.concept_id AS id, c.concept_code AS text
+    FROM  `all-of-us-ehr-dev.SC2022Q4R6.concept` c
+) AS textsearch
+
+JOIN `all-of-us-ehr-dev.SC2022Q4R6.concept` c
+ON c.concept_id = textsearch.id
+
+WHERE c.domain_id = 'Drug'
+AND ((c.vocabulary_id IN ('RxNorm', 'RxNorm Extension')
+        AND c.concept_class_id = 'Ingredient'
+        AND c.standard_concept = 'S')
+    OR (c.vocabulary_id = 'RxNorm'
+        AND c.concept_class_id = 'Precise Ingredient')
+    OR (c.vocabulary_id = 'ATC'
+        AND c.concept_class_id IN ('ATC 1st', 'ATC 2nd', 'ATC 3rd', 'ATC 4th', 'ATC 5th')
+        AND c.standard_concept = 'C'))
