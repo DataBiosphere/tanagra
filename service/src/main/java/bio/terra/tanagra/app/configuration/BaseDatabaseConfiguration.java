e59db0b17fe671cdc76c75f1782b7466c44e5d78
Mariko Medlock <marikomedlock@google.com>
2023-07-11 09:43:01 -0400

    Try hardcoding changes in base db config class.

--- service/src/main/java/bio/terra/tanagra/app/configuration/BaseDatabaseConfiguration.java
+++ service/src/main/java/bio/terra/tanagra/app/configuration/BaseDatabaseConfiguration.java
@@ -2,6 +2,11 @@ package bio.terra.tanagra.app.configuration;
 
 import bio.terra.common.db.BaseDatabaseProperties;
 import bio.terra.common.db.DataSourceInitializer;
+import org.apache.commons.dbcp2.*;
+import org.apache.commons.pool2.ObjectPool;
+import org.apache.commons.pool2.impl.GenericObjectPool;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
+
 import javax.sql.DataSource;
 import java.util.Properties;
 
@@ -9,28 +14,35 @@ import java.util.Properties;
 public class BaseDatabaseConfiguration {
   private final DataSource dataSource;
 
-  public BaseDatabaseConfiguration(BaseDatabaseProperties databaseProperties) {
-    dataSource = DataSourceInitializer.initializeDataSource(databaseProperties);
+  public BaseDatabaseConfiguration(BaseDatabaseProperties baseDatabaseProperties) {
+//    dataSource = DataSourceInitializer.initializeDataSource(databaseProperties);
+    Properties props = new Properties();
+    props.setProperty("user", baseDatabaseProperties.getUsername());
+    props.setProperty("password", baseDatabaseProperties.getPassword());
+    props.setProperty("socketFactory", "com.google.cloud.sql.mysql.SocketFactory");
+    props.setProperty("cloudSqlInstance", "all-of-us-workbench-test:us-central1:workbenchmaindb");
+
+//    ConnectionFactory connectionFactory =
+//            new DriverManagerConnectionFactory(baseDatabaseProperties.getUri(), props);
+    ConnectionFactory connectionFactory = new DriverManagerConnectionFactory("jdbc:mysql:///tanagra_db", props);
+
+    PoolableConnectionFactory poolableConnectionFactory =
+            new PoolableConnectionFactory(connectionFactory, null);
+
+    GenericObjectPoolConfig<PoolableConnection> config = new GenericObjectPoolConfig<>();
+    config.setJmxEnabled(baseDatabaseProperties.isJmxEnabled());
+    config.setMaxTotal(baseDatabaseProperties.getPoolMaxTotal());
+    config.setMaxIdle(baseDatabaseProperties.getPoolMaxIdle());
+    ObjectPool<PoolableConnection> connectionPool =
+            new GenericObjectPool<>(poolableConnectionFactory, config);
+
+    poolableConnectionFactory.setPool(connectionPool);
+
+    dataSource = new PoolingDataSource<>(connectionPool);
   }
 
   // The DataSource is nested inside this configuration, not directly injectable.
   public DataSource getDataSource() {
-    if (dataSource == null) {
-      Properties props = new Properties();
-      props.setProperty("user", getUsername());
-      props.setProperty("password", getPassword());
-      props.setProperty("socketFactory", "com.google.cloud.sql.mysql.SocketFactory");
-      props.setProperty("cloudSqlInstance", "all-of-us-workbench-test:us-central1:workbenchmaindb");
-
-      ConnectionFactory connectionFactory = new DriverManagerConnectionFactory("jdbc:mysql:///tanagra_db", props);
-
-      PoolableConnectionFactory poolableConnectionFactory =
-              new PoolableConnectionFactory(connectionFactory, null);
-      ObjectPool<PoolableConnection> connectionPool =
-              new GenericObjectPool<>(poolableConnectionFactory);
-      poolableConnectionFactory.setPool(connectionPool);
-      dataSource = new PoolingDataSource<>(connectionPool);
-    }
     return dataSource;
   }
 }
