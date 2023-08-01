50a576f864e5c44d87289b68f30e642970d45d55
Timothy Jennison <tjennison@google.com>
2023-08-01 15:45:28 +0000

    Add context display support to document searches

--- ui/src/cohortReview/plugins/textSearch.tsx
+++ ui/src/cohortReview/plugins/textSearch.tsx
@@ -7,15 +7,16 @@ import {
   CohortReviewPlugin,
   registerCohortReviewPlugin,
 } from "cohortReview/pluginRegistry";
+import Checkbox from "components/checkbox";
 import { HintDataSelect, Selection } from "components/hintDataSelect";
 import { Search } from "components/search";
 import { SortDirection, SortOrder } from "data/configuration";
 import { useSource } from "data/sourceContext";
 import { DataValue } from "data/types";
+import { findAll } from "highlight-words-core";
 import { GridBox } from "layout/gridBox";
 import GridLayout from "layout/gridLayout";
-import React, { PropsWithChildren, useMemo } from "react";
-import Highlighter from "react-highlight-words";
+import React, { useMemo } from "react";
 import useSWRImmutable from "swr/immutable";
 import { CohortReviewPageConfig } from "underlaysSlice";
 
@@ -48,17 +49,10 @@ class _ implements CohortReviewPlugin {
   }
 }
 
-const fillerText = [
-  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ex leo, placerat sit amet purus sit amet, malesuada sagittis magna. Donec pretium justo non justo fringilla, non efficitur magna consequat. Nunc efficitur velit at neque interdum rutrum. Morbi viverra at erat a mollis. Nulla et erat finibus neque posuere volutpat id et enim. Ut pretium eget ante id suscipit. Fusce gravida eleifend orci, sed convallis turpis lobortis sed. Morbi in ex consequat tortor porta egestas. Proin maximus augue id nisi iaculis faucibus. Morbi quis arcu id felis consequat pretium in in velit. Sed euismod augue interdum, volutpat ante eget, pellentesque lacus. Donec elementum vestibulum pulvinar. Morbi at porta turpis. Donec eget malesuada massa, vitae ultrices neque. Donec pharetra, ante nec efficitur semper, turpis turpis dignissim tortor, vel hendrerit ante erat ut diam. Aliquam quis augue id felis sollicitudin vestibulum.",
-  "Vestibulum cursus velit leo, at congue odio aliquam a. Aenean leo dui, finibus ac mollis sit amet, pretium vitae ex. Aliquam scelerisque rhoncus justo, sed fringilla justo pellentesque at. Cras sed erat congue, molestie tellus in, dictum diam. Integer pellentesque a mauris euismod ullamcorper. Fusce non tempus quam, nec convallis risus. Sed est orci, tincidunt at dolor eget, luctus rutrum nisl.",
-  "Etiam semper magna enim, nec cursus nisl imperdiet in. Pellentesque dapibus arcu nisl, hendrerit condimentum justo mollis vitae. Nunc feugiat tempus erat, nec convallis erat efficitur non. Aliquam ornare eros in nisi malesuada, non hendrerit odio facilisis. Donec bibendum enim eu turpis scelerisque efficitur. Phasellus id feugiat orci. Nunc scelerisque ligula vitae neque porta, porttitor pretium ex dictum. Proin accumsan, orci ullamcorper vulputate sagittis, nibh lectus fringilla dui, eu placerat metus eros in ante. Quisque non sem vitae tortor feugiat sagittis. Vestibulum feugiat lacus arcu. Aliquam erat volutpat. Sed sit amet mauris vitae quam pharetra euismod ut eu ligula. Mauris nisl elit, gravida vel libero at, efficitur tristique orci. Sed nec sapien eget libero porta auctor id ac enim. Etiam molestie libero non molestie gravida. Vestibulum sit amet lorem vitae nunc maximus ultrices eget eget quam.",
-  "Phasellus ac porttitor nibh. Proin est eros, faucibus in arcu gravida, tempus ultrices purus. Proin et magna accumsan, pharetra lacus in, porta dui. Integer iaculis lorem vel erat imperdiet, vel fermentum felis tincidunt. Integer vestibulum dictum turpis, nec aliquet nisl fringilla non. Curabitur quam quam, posuere ac sollicitudin sed, rhoncus ut nisl. Fusce a luctus odio. Nunc non lacus sit amet leo tempus finibus. Donec sed tincidunt arcu, dignissim pellentesque dui. Aliquam quam sapien, sollicitudin vel sem at, feugiat euismod lacus. Integer laoreet velit non lorem pretium condimentum. Mauris pulvinar et sapien sit amet sollicitudin. Fusce at tincidunt turpis, vitae tempus nisi. Etiam non malesuada leo, nec vestibulum ipsum. Mauris nec risus a sapien consequat fringilla.",
-  "Aliquam ligula felis, porttitor id pulvinar non, condimentum et justo. Sed efficitur mauris metus, id dictum urna sagittis nec. Aliquam nec placerat eros, quis suscipit mauris. Phasellus eget nisl in arcu posuere hendrerit non vel enim. Pellentesque blandit nec dui sed pellentesque. Proin fermentum, elit placerat tempus viverra, eros risus consequat ante, in placerat augue nunc ut elit. Integer eget malesuada erat, at facilisis tellus. Mauris eleifend ante id massa placerat auctor. Donec et felis ornare sem lacinia viverra vitae a tellus. Phasellus eu porttitor elit. Donec eget tristique dolor. Ut cursus velit a nibh faucibus, quis feugiat erat imperdiet. Duis nec facilisis orci. Etiam porttitor magna a mauris accumsan, et iaculis mi accumsan. Curabitur mollis placerat augue, vitae egestas elit sodales sed.",
-];
-
 type SearchData = {
   query: string;
   categories?: Selection[];
+  context?: boolean;
 };
 
 function TextSearch({ id, config }: { id: string; config: Config }) {
@@ -74,16 +68,14 @@ function TextSearch({ id, config }: { id: string; config: Config }) {
     () =>
       context.occurrences[config.occurrence].map((o) => ({
         ...o,
-        [config.text]: formatValue(
-          o[config.text] ??
-            fillerText[Math.floor(Math.random() * fillerText.length)]
-        ),
+        [config.text]: formatValue(o[config.text]),
       })),
     [context.occurrences]
   );
 
   const searchData = context.searchData<SearchData>(id);
-  const regExp = new RegExp(searchData?.query ?? "", "i");
+  const query = searchData?.query ?? "";
+  const regExp = new RegExp(query, "i");
 
   const occurrences = useMemo(
     () =>
@@ -149,30 +141,47 @@ function TextSearch({ id, config }: { id: string; config: Config }) {
       data.categories = sel;
     });
 
+  const onChangeContext = () =>
+    context.updateSearchData(id, (data: SearchData) => {
+      data.context = !data.context;
+    });
+
   return (
     <GridLayout rows>
       <GridBox
         sx={{
+          p: 2,
           backgroundColor: (theme) => theme.palette.background.paper,
           borderBottomStyle: "solid",
           borderColor: (theme) => theme.palette.divider,
           borderWidth: "1px",
         }}
       >
-        <GridLayout cols="1fr 1fr" rowAlign="middle">
-          <Search
-            initialValue={searchData?.query}
-            delayMs={0}
-            onSearch={onSearch}
-          />
-          {config.categoryAttribute ? (
-            <HintDataSelect
-              hintData={hintDataState.data?.hintData}
-              maxChips={2}
-              selected={searchData?.categories ?? []}
-              onSelect={onSelect}
+        <GridLayout rows spacing={1} height="auto">
+          <GridLayout cols="1fr 1fr" spacing={2} rowAlign="middle">
+            <Search
+              initialValue={searchData?.query}
+              delayMs={0}
+              onSearch={onSearch}
             />
-          ) : null}
+            {config.categoryAttribute ? (
+              <HintDataSelect
+                hintData={hintDataState.data?.hintData}
+                maxChips={2}
+                selected={searchData?.categories ?? []}
+                onSelect={onSelect}
+              />
+            ) : null}
+          </GridLayout>
+          <GridLayout cols rowAlign="middle">
+            <Checkbox
+              size="small"
+              fontSize="small"
+              checked={!!searchData?.context}
+              onChange={onChangeContext}
+            />
+            <Typography variant="body2">Show keywords in context</Typography>
+          </GridLayout>
         </GridLayout>
       </GridBox>
       <GridBox
@@ -202,11 +211,10 @@ function TextSearch({ id, config }: { id: string; config: Config }) {
                 ))}
               </Stack>
               <Box sx={{ p: 1 }}>
-                <Highlighter
-                  searchWords={[regExp]}
-                  textToHighlight={String(o[config.text])}
-                  unhighlightTag={Unhighlighted}
-                  highlightTag={Highlighted}
+                <TextBlock
+                  query={query}
+                  text={String(o[config.text])}
+                  context={searchData.context}
                 />
               </Box>
             </Paper>
@@ -221,17 +229,55 @@ function formatValue(value: DataValue) {
   return value instanceof Date ? value.toDateString() : String(value);
 }
 
-function Unhighlighted(props: PropsWithChildren<object>) {
-  return <Typography component="span">{props.children}</Typography>;
-}
+type TextBlockProps = {
+  text: string;
+  query: string;
+  context?: boolean;
+};
 
-function Highlighted(props: PropsWithChildren<{ highlightIndex: number }>) {
+const CONTEXT_LENGTH = 40;
+
+function TextBlock(props: TextBlockProps) {
+  const chunks = useMemo(() => {
+    return findAll({
+      searchWords: [props.query],
+      textToHighlight: props.text,
+    });
+  }, [props.text, props.query]);
+
   return (
-    <Typography
-      component="span"
-      sx={{ backgroundColor: (theme) => theme.highlightColor }}
-    >
-      {props.children}
-    </Typography>
+    <>
+      {chunks.map(({ start, end, highlight }, i) => {
+        const trim = !!props.context && !highlight && chunks.length > 1;
+
+        let text = "";
+        if (trim && i === 0) {
+          text = "…" + props.text.substring(end - CONTEXT_LENGTH, end);
+        } else if (trim && i === chunks.length - 1) {
+          text = props.text.substr(start, CONTEXT_LENGTH) + "…";
+        } else if (trim && end - start > CONTEXT_LENGTH * 2 + 1) {
+          text =
+            props.text.substr(start, CONTEXT_LENGTH) +
+            "…\n…" +
+            props.text.substr(end - CONTEXT_LENGTH, CONTEXT_LENGTH);
+        } else {
+          text = props.text.substring(start, end);
+        }
+
+        return (
+          <Typography
+            key={i}
+            component="span"
+            sx={{
+              backgroundColor: (theme) =>
+                highlight ? theme.highlightColor : undefined,
+              whiteSpace: "pre-wrap",
+            }}
+          >
+            {text}
+          </Typography>
+        );
+      })}
+    </>
   );
 }
