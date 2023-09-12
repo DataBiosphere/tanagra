6020f3323e7e0d1e8326cc413f17c51f42e14f0c
Chenchal Subraveti <c.subraveti@vumc.org>
2023-09-12 09:09:00 -0500

    Changed t_person to have attribute has_fitbit_activity_summary for person_ids who have activity summary datqa

--- service/src/main/resources/config/aou/test/SR2023Q3R1/sql/t_person.sql
+++ service/src/main/resources/config/aou/test/SR2023Q3R1/sql/t_person.sql
@@ -1,4 +1,7 @@
 SELECT
-    p.person_id, p.year_of_birth, p.birth_datetime, p.gender_concept_id, p.race_concept_id, p.ethnicity_concept_id
+    p.person_id, p.year_of_birth, p.birth_datetime, p.gender_concept_id,
+    p.race_concept_id, p.ethnicity_concept_id, p.sex_at_birth_concept_id,
+    EXISTS
+        (SELECT 1 FROM `all-of-us-ehr-dev.SR2023Q3R1.activity_summary` fas WHERE p.person_id = fas.person_id)
+        AS has_fitbit_activity_summary
 FROM `all-of-us-ehr-dev.SR2023Q3R1.person` p
-
