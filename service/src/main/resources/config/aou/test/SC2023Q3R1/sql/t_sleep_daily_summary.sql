5be0d761795814c21a77c768ea13099394d3e308
Chenchal Subraveti <c.subraveti@vumc.org>
2023-09-14 14:40:57 -0500

    updated indexing for CT dataset to accommodate fitbit data type for criteria nd prepackaged concept sets

---
+++ service/src/main/resources/config/aou/test/SC2023Q3R1/sql/t_sleep_daily_summary.sql
@@ -0,0 +1,15 @@
+SELECT
+    ROW_NUMBER() OVER() AS row_id,
+    person_id,
+    sleep_date,
+    is_main_sleep,
+    minute_in_bed,
+    minute_asleep,
+    minute_after_wakeup,
+    minute_awake,
+    minute_restless,
+    minute_deep,
+    minute_light,
+    minute_rem,
+    minute_wake
+FROM `all-of-us-ehr-dev.SC2023Q3R1.sleep_daily_summary`
