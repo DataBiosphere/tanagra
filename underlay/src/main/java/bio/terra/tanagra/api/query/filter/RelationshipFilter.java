872555041725d974d1c0d742a861fd6585e17b79
Mariko Medlock <marikomedlock@google.com>
2023-09-14 14:16:11 -0400

    Move conditional blocks into helper methods.

--- underlay/src/main/java/bio/terra/tanagra/api/query/filter/RelationshipFilter.java
+++ underlay/src/main/java/bio/terra/tanagra/api/query/filter/RelationshipFilter.java
@@ -43,8 +43,6 @@ public class RelationshipFilter extends EntityFilter {
   @Override
   public FilterVariable getFilterVariable(
       TableVariable entityTableVar, List<TableVariable> tableVars) {
-    FieldPointer entityIdFieldPointer =
-        selectEntity.getIdAttribute().getMapping(Underlay.MappingType.INDEX).getValue();
     RelationshipMapping indexMapping = relationship.getMapping(Underlay.MappingType.INDEX);
 
     // Build sub-filter variable.
@@ -63,110 +61,160 @@ public class RelationshipFilter extends EntityFilter {
 
     if (fkOnSelectTable) {
       LOGGER.info("Foreign key on select entity table: {}", selectEntity.getName());
-      // SELECT id from filterEntity WHERE [subfilter]
-      FieldVariable filterEntityIdFieldVar =
-          filterEntity
-              .getIdAttribute()
-              .getMapping(Underlay.MappingType.INDEX)
-              .getValue()
-              .buildVariable(filterEntityTableVar, filterEntityTableVars);
-      Query filterEntitySubQuery =
-          new Query.Builder()
-              .select(List.of(filterEntityIdFieldVar))
-              .tables(filterEntityTableVars)
-              .where(filterEntitySubFilterVar)
-              .build();
-
-      // WHERE selectEntity.foreignKey IN
-      //    (SELECT id FROM filterEntity WHERE [subfilter]) --> from above
-      FieldVariable selectEntityFKFieldVar =
-          selectEntity
-              .getAttribute(indexMapping.getForeignKeyAttribute())
-              .getMapping(Underlay.MappingType.INDEX)
-              .getValue()
-              .buildVariable(entityTableVar, tableVars);
-      return new SubQueryFilterVariable(
-          selectEntityFKFieldVar, SubQueryFilterVariable.Operator.IN, filterEntitySubQuery);
+      return getFilterVariableForFKOnSelectEntity(
+          indexMapping,
+          filterEntityTableVar,
+          filterEntityTableVars,
+          filterEntitySubFilterVar,
+          entityTableVar,
+          tableVars);
     } else if (fkOnFilterTable) {
       LOGGER.info("Foreign key on filter entity table: {}", filterEntity.getName());
-      // SELECT foreignKey from filterEntity WHERE [subfilter]
-      FieldVariable filterEntityFKFieldVar =
-          filterEntity
-              .getAttribute(indexMapping.getForeignKeyAttribute())
-              .getMapping(Underlay.MappingType.INDEX)
-              .getValue()
-              .buildVariable(filterEntityTableVar, tableVars);
-      Query filterEntitySubQuery =
-          new Query.Builder()
-              .select(List.of(filterEntityFKFieldVar))
-              .tables(filterEntityTableVars)
-              .where(filterEntitySubFilterVar)
-              .build();
-
-      // WHERE selectEntity.id IN
-      //    (SELECT foreignKey FROM filterEntity WHERE [subfilter]) --> from above
-      FieldVariable selectEntityIdFieldVar =
-          selectEntity
-              .getIdAttribute()
-              .getMapping(Underlay.MappingType.INDEX)
-              .getValue()
-              .buildVariable(entityTableVar, tableVars);
-      return new SubQueryFilterVariable(
-          selectEntityIdFieldVar, SubQueryFilterVariable.Operator.IN, filterEntitySubQuery);
+      return getFilterVariableForFKOnFilterEntity(
+          indexMapping,
+          filterEntityTableVar,
+          filterEntityTableVars,
+          filterEntitySubFilterVar,
+          entityTableVar,
+          tableVars);
     } else {
       LOGGER.info(
           "Intermediate table connects select entity ({}) and filter entity ({}) tables",
           selectEntity.getName(),
           filterEntity.getName());
-      // SELECT id from filterEntity WHERE [subfilter]
-      FieldVariable filterEntityIdFieldVar =
-          filterEntity
-              .getIdAttribute()
-              .getMapping(Underlay.MappingType.INDEX)
-              .getValue()
-              .buildVariable(filterEntityTableVar, filterEntityTableVars);
-      Query filterEntitySubQuery =
-          new Query.Builder()
-              .select(List.of(filterEntityIdFieldVar))
-              .tables(filterEntityTableVars)
-              .where(filterEntitySubFilterVar)
-              .build();
-
-      //    SELECT selectEntityId FROM intermediateTable WHERE filterEntityId IN
-      //        (SELECT id FROM filterEntity WHERE [subfilter]) --> from above
-      TableVariable intermediateTableVar = TableVariable.forPrimary(indexMapping.getIdPairsTable());
-      List<TableVariable> intermediateTableVars = Lists.newArrayList(intermediateTableVar);
-      FieldVariable intermediateSelectEntityIdFieldVar =
-          indexMapping
-              .getIdPairsId(selectEntity)
-              .buildVariable(intermediateTableVar, intermediateTableVars);
-      FieldVariable intermediateFilterEntityIdFieldVar =
-          indexMapping
-              .getIdPairsId(filterEntity)
-              .buildVariable(intermediateTableVar, intermediateTableVars);
-      Query intermediateSubQuery =
-          new Query.Builder()
-              .select(List.of(intermediateSelectEntityIdFieldVar))
-              .tables(intermediateTableVars)
-              .where(
-                  new SubQueryFilterVariable(
-                      intermediateFilterEntityIdFieldVar,
-                      SubQueryFilterVariable.Operator.IN,
-                      filterEntitySubQuery))
-              .build();
-
-      // WHERE selectEntity.id IN (
-      //    SELECT selectEntityId FROM intermediateTable WHERE filterEntityId IN --> from above
-      //        (SELECT id FROM filterEntity WHERE [subfilter]) --> from above
-      //    )
-      FieldVariable selectEntityIdFieldVar =
-          selectEntity
-              .getIdAttribute()
-              .getMapping(Underlay.MappingType.INDEX)
-              .getValue()
-              .buildVariable(entityTableVar, tableVars);
-      return new SubQueryFilterVariable(
-          selectEntityIdFieldVar, SubQueryFilterVariable.Operator.IN, intermediateSubQuery);
+      return getFilterVariableForIntermediateTable(
+          indexMapping,
+          filterEntityTableVar,
+          filterEntityTableVars,
+          filterEntitySubFilterVar,
+          entityTableVar,
+          tableVars);
     }
+
+    // Handle group by count.
+  }
+
+  private SubQueryFilterVariable getFilterVariableForFKOnSelectEntity(
+      RelationshipMapping indexMapping,
+      TableVariable filterEntityTableVar,
+      List<TableVariable> filterEntityTableVars,
+      FilterVariable filterEntitySubFilterVar,
+      TableVariable entityTableVar,
+      List<TableVariable> tableVars) {
+    // SELECT id from filterEntity WHERE [subfilter]
+    FieldVariable filterEntityIdFieldVar =
+        filterEntity
+            .getIdAttribute()
+            .getMapping(Underlay.MappingType.INDEX)
+            .getValue()
+            .buildVariable(filterEntityTableVar, filterEntityTableVars);
+    Query filterEntitySubQuery =
+        new Query.Builder()
+            .select(List.of(filterEntityIdFieldVar))
+            .tables(filterEntityTableVars)
+            .where(filterEntitySubFilterVar)
+            .build();
+
+    // WHERE selectEntity.foreignKey IN
+    //    (SELECT id FROM filterEntity WHERE [subfilter]) --> from above
+    FieldVariable selectEntityFKFieldVar =
+        selectEntity
+            .getAttribute(indexMapping.getForeignKeyAttribute())
+            .getMapping(Underlay.MappingType.INDEX)
+            .getValue()
+            .buildVariable(entityTableVar, tableVars);
+    return new SubQueryFilterVariable(
+        selectEntityFKFieldVar, SubQueryFilterVariable.Operator.IN, filterEntitySubQuery);
+  }
+
+  private FilterVariable getFilterVariableForFKOnFilterEntity(
+      RelationshipMapping indexMapping,
+      TableVariable filterEntityTableVar,
+      List<TableVariable> filterEntityTableVars,
+      FilterVariable filterEntitySubFilterVar,
+      TableVariable entityTableVar,
+      List<TableVariable> tableVars) {
+    // SELECT foreignKey from filterEntity WHERE [subfilter]
+    FieldVariable filterEntityFKFieldVar =
+        filterEntity
+            .getAttribute(indexMapping.getForeignKeyAttribute())
+            .getMapping(Underlay.MappingType.INDEX)
+            .getValue()
+            .buildVariable(filterEntityTableVar, tableVars);
+    Query filterEntitySubQuery =
+        new Query.Builder()
+            .select(List.of(filterEntityFKFieldVar))
+            .tables(filterEntityTableVars)
+            .where(filterEntitySubFilterVar)
+            .build();
+
+    // WHERE selectEntity.id IN
+    //    (SELECT foreignKey FROM filterEntity WHERE [subfilter]) --> from above
+    FieldVariable selectEntityIdFieldVar =
+        selectEntity
+            .getIdAttribute()
+            .getMapping(Underlay.MappingType.INDEX)
+            .getValue()
+            .buildVariable(entityTableVar, tableVars);
+    return new SubQueryFilterVariable(
+        selectEntityIdFieldVar, SubQueryFilterVariable.Operator.IN, filterEntitySubQuery);
+  }
+
+  private FilterVariable getFilterVariableForIntermediateTable(
+      RelationshipMapping indexMapping,
+      TableVariable filterEntityTableVar,
+      List<TableVariable> filterEntityTableVars,
+      FilterVariable filterEntitySubFilterVar,
+      TableVariable entityTableVar,
+      List<TableVariable> tableVars) {
+    // SELECT id from filterEntity WHERE [subfilter]
+    FieldVariable filterEntityIdFieldVar =
+        filterEntity
+            .getIdAttribute()
+            .getMapping(Underlay.MappingType.INDEX)
+            .getValue()
+            .buildVariable(filterEntityTableVar, filterEntityTableVars);
+    Query filterEntitySubQuery =
+        new Query.Builder()
+            .select(List.of(filterEntityIdFieldVar))
+            .tables(filterEntityTableVars)
+            .where(filterEntitySubFilterVar)
+            .build();
+
+    //    SELECT selectEntityId FROM intermediateTable WHERE filterEntityId IN
+    //        (SELECT id FROM filterEntity WHERE [subfilter]) --> from above
+    TableVariable intermediateTableVar = TableVariable.forPrimary(indexMapping.getIdPairsTable());
+    List<TableVariable> intermediateTableVars = Lists.newArrayList(intermediateTableVar);
+    FieldVariable intermediateSelectEntityIdFieldVar =
+        indexMapping
+            .getIdPairsId(selectEntity)
+            .buildVariable(intermediateTableVar, intermediateTableVars);
+    FieldVariable intermediateFilterEntityIdFieldVar =
+        indexMapping
+            .getIdPairsId(filterEntity)
+            .buildVariable(intermediateTableVar, intermediateTableVars);
+    Query intermediateSubQuery =
+        new Query.Builder()
+            .select(List.of(intermediateSelectEntityIdFieldVar))
+            .tables(intermediateTableVars)
+            .where(
+                new SubQueryFilterVariable(
+                    intermediateFilterEntityIdFieldVar,
+                    SubQueryFilterVariable.Operator.IN,
+                    filterEntitySubQuery))
+            .build();
+
+    // WHERE selectEntity.id IN (
+    //    SELECT selectEntityId FROM intermediateTable WHERE filterEntityId IN --> from above
+    //        (SELECT id FROM filterEntity WHERE [subfilter]) --> from above
+    //    )
+    FieldVariable selectEntityIdFieldVar =
+        selectEntity
+            .getIdAttribute()
+            .getMapping(Underlay.MappingType.INDEX)
+            .getValue()
+            .buildVariable(entityTableVar, tableVars);
+    return new SubQueryFilterVariable(
+        selectEntityIdFieldVar, SubQueryFilterVariable.Operator.IN, intermediateSubQuery);
   }
 }
