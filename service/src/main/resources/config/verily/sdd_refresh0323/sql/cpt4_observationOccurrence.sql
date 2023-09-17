f01424c299a0f52bd09afc1a087bb6fa056188bd
Mariko Medlock <marikomedlock@google.com>
2023-09-17 10:39:55 -0400

    Point to verily aou_static_prep tables for verily/sdd config sql.

--- service/src/main/resources/config/verily/sdd_refresh0323/sql/cpt4_observationOccurrence.sql
+++ service/src/main/resources/config/verily/sdd_refresh0323/sql/cpt4_observationOccurrence.sql
@@ -1,7 +1,7 @@
 SELECT oo.observation_id, pc.id AS cpt4_id
 FROM `victr-tanagra-test.sd_20230328.observation` AS oo
 
-JOIN `victr-tanagra-test.aou_static_prep.prep_cpt` AS pc
+JOIN `verily-tanagra-dev.aou_static_prep_useast1.prep_cpt` AS pc
 ON pc.concept_id = oo.observation_source_concept_id
 
 WHERE pc.type = 'CPT4'
