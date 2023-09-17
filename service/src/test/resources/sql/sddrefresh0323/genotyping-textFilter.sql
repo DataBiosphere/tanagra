93e1814d3d2c35ac4c927f6840682f6949e25c32
Mariko Medlock <marikomedlock@google.com>
2023-09-17 10:41:41 -0400

    Update verily/sdd dataset name in sql golden files.

--- service/src/test/resources/sql/sddrefresh0323/genotyping-textFilter.sql
+++ service/src/test/resources/sql/sddrefresh0323/genotyping-textFilter.sql
@@ -1,1 +1,1 @@
-SELECT g.id AS id, g.name AS name FROM `verily-tanagra-dev.sd20230328_index_083023`.genotyping AS g WHERE CONTAINS_SUBSTR(g.id, 'Illumina') LIMIT 30
+SELECT g.id AS id, g.name AS name FROM `verily-tanagra-dev.sd20230328_index_091523`.genotyping AS g WHERE CONTAINS_SUBSTR(g.id, 'Illumina') LIMIT 30
--- service/src/test/resources/sql/sddrefresh0323/note-noFilter.sql
+++ service/src/test/resources/sql/sddrefresh0323/note-noFilter.sql
@@ -1,1 +1,1 @@
-SELECT n.concept_code AS concept_code, n.id AS id, n.name AS name, n.vocabulary AS vocabulary FROM `verily-tanagra-dev.sd20230328_index_083023`.note AS n LIMIT 30
+SELECT n.concept_code AS concept_code, n.id AS id, n.name AS name, n.vocabulary AS vocabulary FROM `verily-tanagra-dev.sd20230328_index_091523`.note AS n LIMIT 30
