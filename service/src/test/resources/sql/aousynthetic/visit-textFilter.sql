b98931f6df7610c5b3f14bf0685e35d636ef6d29
Mariko Medlock <marikomedlock@google.com>
2023-09-18 10:01:46 -0400

    Update sql golden files for text search function swap.

--- service/src/test/resources/sql/aousynthetic/visit-textFilter.sql
+++ service/src/test/resources/sql/aousynthetic/visit-textFilter.sql
@@ -1,1 +1,1 @@
-SELECT v.id AS id, v.name AS name FROM `broad-tanagra-dev.aousynthetic_index_082523`.visit AS v WHERE CONTAINS_SUBSTR(v.text, 'ambul') LIMIT 30
+SELECT v.id AS id, v.name AS name FROM `broad-tanagra-dev.aousynthetic_index_082523`.visit AS v WHERE REGEXP_CONTAINS(UPPER(v.text), UPPER('ambul')) LIMIT 30
--- service/src/test/resources/sql/sddrefresh0323/snp-textFilter.sql
+++ service/src/test/resources/sql/sddrefresh0323/snp-textFilter.sql
@@ -1,1 +1,1 @@
-SELECT s.id AS id, s.name AS name FROM `verily-tanagra-dev.sd20230328_index_091523`.snp AS s WHERE CONTAINS_SUBSTR(s.id, 'RS1292') LIMIT 30
+SELECT s.id AS id, s.name AS name FROM `verily-tanagra-dev.sd20230328_index_091523`.snp AS s WHERE REGEXP_CONTAINS(UPPER(s.id), UPPER('RS1292')) LIMIT 30
