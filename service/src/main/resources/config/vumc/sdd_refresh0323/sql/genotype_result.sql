44e5e347327ae1b426485c8901d9554f762c1873
Mariko Medlock <marikomedlock@google.com>
2023-09-21 17:47:16 -0400

    Make same changes to vumc/sdd as in verily/sdd.

--- service/src/main/resources/config/vumc/sdd_refresh0323/sql/genotype_result.sql
+++ service/src/main/resources/config/vumc/sdd_refresh0323/sql/genotype_result.sql
@@ -2,5 +2,5 @@
  platform_id column has type NUMERIC. This causes index column to be FLOAT.
  Currently indexing doesn't handle floats properly, so change column to INTEGER.
 */
-SELECT genotype_result_id AS genotype_result_id, person_id, CAST(platform_id as INT64) as platform_id
+SELECT genotype_result_id AS genotype_result_id, person_id, CAST(PARSE_NUMERIC(platform_id) as INT64) as platform_id
 FROM `sd-vumc-tanagra-test.sd_20230331.x_genotype_result`
--- service/src/main/resources/config/vumc/sdd_refresh0323/sql/genotyping_all.sql
+++ service/src/main/resources/config/vumc/sdd_refresh0323/sql/genotyping_all.sql
@@ -2,7 +2,7 @@
  platform_id column has type NUMERIC. This causes index column to be FLOAT.
  Currently indexing doesn't handle floats properly, so change column to INTEGER.
 */
-SELECT CAST(platform_id as INT64) AS platform_id, assay_name as assay_name
+SELECT CAST(PARSE_NUMERIC(platform_id) as INT64) AS platform_id, assay_name as assay_name
 FROM `sd-vumc-tanagra-test.sd_20230331.platform`
 UNION ALL
 /*
