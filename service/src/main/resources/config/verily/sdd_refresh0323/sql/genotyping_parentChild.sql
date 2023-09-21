9829eefbf3366c85d02a3a502fdb387596a77cad
Mariko Medlock <marikomedlock@google.com>
2023-09-21 17:14:22 -0400

    platform_id datatype numeric -> string.

--- service/src/main/resources/config/verily/sdd_refresh0323/sql/genotyping_parentChild.sql
+++ service/src/main/resources/config/verily/sdd_refresh0323/sql/genotyping_parentChild.sql
@@ -1,6 +1,6 @@
 SELECT
 /* Use parent ids defined in platform.sql */
     parent_seq + 100 AS parent,
-    CAST(platform_id as INT64) AS child
+    CAST(PARSE_NUMERIC(platform_id) as INT64) AS child
 FROM `sd-vumc-tanagra-test.sd_20230331.genotype_criteria` g, `sd-vumc-tanagra-test.sd_20230331.platform` p
 WHERE g.name = p.assay_name
