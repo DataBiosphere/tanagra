34bebcafcadae2db91aa2939ecd105d8aeae4ef1
Mariko Medlock <marikomedlock@google.com>
2023-10-02 10:36:00 -0400

    Force measurement_concept_id null > 0.

--- service/src/main/resources/config/verily/sdd_refresh0323/sql/measurement_occurrence_all.sql
+++ service/src/main/resources/config/verily/sdd_refresh0323/sql/measurement_occurrence_all.sql
@@ -1,5 +1,7 @@
 SELECT
-  mo.measurement_id AS id, mo.person_id, mo.measurement_concept_id, mo.measurement_date,
+  mo.measurement_id AS id, mo.person_id,
+  (CASE WHEN mo.measurement_concept_id IS NULL THEN 0 ELSE mo.measurement_concept_id END) AS measurement_concept_id,
+  mo.measurement_date,
   mo.value_as_number AS value_numeric, mo.value_as_concept_id, mo.unit_concept_id,
   mo.measurement_source_value AS source_value, mo.measurement_source_concept_id AS source_criteria_id,
   CAST(FLOOR(TIMESTAMP_DIFF(mo.measurement_datetime, p.birth_datetime, DAY) / 365.25) AS INT64) AS age_at_occurrence,
@@ -12,5 +14,3 @@ ON p.person_id = mo.person_id
 
 LEFT JOIN `sd-vumc-tanagra-test.sd_20230331.visit_occurrence` AS vo
 ON vo.visit_occurrence_id = mo.visit_occurrence_id
-
-WHERE mo.measurement_concept_id IS NOT NULL
