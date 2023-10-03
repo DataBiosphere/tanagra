1a24a8eeceb291bb6b58d69710ad650dd6c8e4c7
Mariko Medlock <marikomedlock@google.com>
2023-10-03 18:09:17 -0400

    Make same changes to vumc/sdd underlay.

--- service/src/main/resources/config/vumc/sdd_refresh0323/sql/genotype_result.sql
+++ service/src/main/resources/config/vumc/sdd_refresh0323/sql/genotype_result.sql
@@ -1,6 +1,2 @@
-/*
- platform_id column has type NUMERIC. This causes index column to be FLOAT.
- Currently indexing doesn't handle floats properly, so change column to INTEGER.
-*/
-SELECT genotype_result_id AS genotype_result_id, person_id, CAST(platform_id as INT64) as platform_id
-FROM `sd-vumc-tanagra-test.sd_20230331.x_genotype_result`
+SELECT CAST(genotype_result_id AS INT64) AS genotype_result_id, person_id, CAST(platform_id AS INT64) AS platform_id
+FROM `sd-vumc-tanagra-test.sd_20230331`.x_genotype_result
--- service/src/main/resources/config/vumc/sdd_refresh0323/sql/genotyping_all.sql
+++ service/src/main/resources/config/vumc/sdd_refresh0323/sql/genotyping_all.sql
@@ -1,16 +1,13 @@
-/*
- platform_id column has type NUMERIC. This causes index column to be FLOAT.
- Currently indexing doesn't handle floats properly, so change column to INTEGER.
-*/
-SELECT CAST(platform_id as INT64) AS platform_id, assay_name as assay_name
-FROM `sd-vumc-tanagra-test.sd_20230331.x_platform`
+/* Hierarchy leaf nodes all live in genotype_criteria and x_platform. */
+SELECT CAST(xp.platform_id AS INT64) platform_id, xp.assay_name
+FROM `sd-vumc-tanagra-test.sd_20230331`.x_platform AS xp
+
+JOIN `sd-vumc-tanagra-test.sd_20230331`.genotype_criteria AS gc
+    ON gc.name = xp.assay_name AND gc.type = 'DNA'
+
 UNION ALL
-/*
- Add some rows to get hierarchy to work. Parent ids are defined in
- genotype_criteria, criteria_meta_seq column. criteria_meta_seq is not in the
- same "ID space" as platform_id. Tanagra requres parent id to be in same "ID
- space" as platform_id. So create artificial platform_ids for parents.
- */
-(SELECT 101 AS platform_id, 'GWAS Platforms' AS assay_name UNION ALL
- SELECT 102, 'Non-GWAS and Targeted Genotyping Platforms' UNION ALL
- SELECT 103, 'Sequencing Platforms')
+
+/* Hierarchy root nodes only live in genotype_criteria. */
+SELECT (100 + gc.criteria_meta_seq) AS platform_id, gc.name AS assay_name
+FROM `sd-vumc-tanagra-test.sd_20230331`.genotype_criteria AS gc
+WHERE gc.type = 'DNA' AND gc.parent_seq = 0
