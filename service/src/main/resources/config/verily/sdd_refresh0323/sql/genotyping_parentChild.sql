1894872cf0742e950f8b0d00e807e764c72c555e
Mariko Medlock <marikomedlock@google.com>
2023-10-03 17:42:57 -0400

    Update genotyping hierarchy.

--- service/src/main/resources/config/verily/sdd_refresh0323/sql/genotyping_parentChild.sql
+++ service/src/main/resources/config/verily/sdd_refresh0323/sql/genotyping_parentChild.sql
@@ -1,6 +1,6 @@
-SELECT
-/* Use parent ids defined in platform.sql */
-    parent_seq + 100 AS parent,
-    CAST(platform_id as INT64) AS child
-FROM `sd-vumc-tanagra-test.sd_20230331.genotype_criteria` g, `sd-vumc-tanagra-test.sd_20230331.x_platform` p
-WHERE g.name = p.assay_name
+SELECT (100 + gc.parent_seq) AS parent, xp.platform_id AS child
+
+FROM `sd-vumc-tanagra-test.sd_20230331`.x_platform AS xp
+
+JOIN `sd-vumc-tanagra-test.sd_20230331`.genotype_criteria AS gc
+  ON gc.name = xp.assay_name AND gc.type = 'DNA'
