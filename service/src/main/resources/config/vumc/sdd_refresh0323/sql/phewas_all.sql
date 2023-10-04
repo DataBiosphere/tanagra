2b1f73374839ee3d6c0ccb7c17d14d793b5449d4
marikomedlock <marikomedlock@users.noreply.github.com>
2023-10-04 13:05:09 -0400

    Add a numeric code for PheWAS for numeric ordering. (#569)
    * Add a numeric code for PheWAS for numeric ordering.
    
    * Make same changes to vumc/sdd underlay.

--- service/src/main/resources/config/vumc/sdd_refresh0323/sql/phewas_all.sql
+++ service/src/main/resources/config/vumc/sdd_refresh0323/sql/phewas_all.sql
@@ -3,4 +3,5 @@ SELECT
     REGEXP_EXTRACT(label, r'^PHEWAS_([0-9.]+)-') AS code,
     REGEXP_EXTRACT(label, r'^PHEWAS_[0-9.]+-(.*)') AS display_name,
     REGEXP_EXTRACT(label, r'^PHEWAS_(.*)') AS label,
+    PARSE_NUMERIC(REGEXP_EXTRACT(label, r'^PHEWAS_([0-9.]+)-')) AS numeric_code
 FROM `sd-vumc-tanagra-test.sd_20230331.phewas_criteria`
