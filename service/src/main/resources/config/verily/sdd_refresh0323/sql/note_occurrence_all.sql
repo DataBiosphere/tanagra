87e6e82fad3f054b9f28199cfe924fd70d858490
Mariko Medlock <marikomedlock@google.com>
2023-10-04 10:45:01 -0400

    Rename attribute text > note_text.

--- service/src/main/resources/config/verily/sdd_refresh0323/sql/note_occurrence_all.sql
+++ service/src/main/resources/config/verily/sdd_refresh0323/sql/note_occurrence_all.sql
@@ -1,6 +1,6 @@
 SELECT
   n.note_id AS id, n.person_id, n.note_type_concept_id,
-  n.note_date, n.note_title AS title, n.note_text AS text,
+  n.note_date, n.note_title AS title, n.note_text,
   n.note_source_value AS source_value,
   CAST(FLOOR(TIMESTAMP_DIFF(n.note_datetime, p.birth_datetime, DAY) / 365.25) AS INT64) AS age_at_occurrence,
   n.visit_occurrence_id, vo.visit_concept_id
