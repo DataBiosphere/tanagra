2e28ddc2b897eefb2e3a38b2901df30b8992739d
Mariko Medlock <marikomedlock@google.com>
2023-10-06 09:52:23 -0400

    Join genotype_criteria to x_platform on assay_name case insensitively.

--- service/src/main/resources/config/verily/sdd_refresh0323/sql/genotyping_all.sql
+++ service/src/main/resources/config/verily/sdd_refresh0323/sql/genotyping_all.sql
@@ -3,7 +3,7 @@ SELECT CAST(xp.platform_id AS INT64) platform_id, xp.assay_name
 FROM `sd-vumc-tanagra-test.sd_20230331`.x_platform AS xp
 
 JOIN `sd-vumc-tanagra-test.sd_20230331`.genotype_criteria AS gc
-    ON gc.name = xp.assay_name AND gc.type = 'DNA'
+    ON UPPER(gc.name) = UPPER(xp.assay_name) AND gc.type = 'DNA'
 
 UNION ALL
 
