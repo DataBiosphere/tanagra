7529d844bf0524430604cb786f24a7aaa1f72811
marikomedlock <marikomedlock@users.noreply.github.com>
2023-10-06 11:14:03 -0400

    Join genotype_criteria to x_platform on assay_name case insensitively. (#578)
    * Join genotype_criteria to x_platform on assay_name case insensitively.
    
    * Make same change to vumc/sdd underlay.

--- service/src/main/resources/config/verily/sdd_refresh0323/sql/genotyping_parentChild.sql
+++ service/src/main/resources/config/verily/sdd_refresh0323/sql/genotyping_parentChild.sql
@@ -3,4 +3,4 @@ SELECT (100 + gc.parent_seq) AS parent, CAST(xp.platform_id AS INT64) AS child
 FROM `sd-vumc-tanagra-test.sd_20230331`.x_platform AS xp
 
 JOIN `sd-vumc-tanagra-test.sd_20230331`.genotype_criteria AS gc
-  ON gc.name = xp.assay_name AND gc.type = 'DNA'
+  ON UPPER(gc.name) = UPPER(xp.assay_name) AND gc.type = 'DNA'
