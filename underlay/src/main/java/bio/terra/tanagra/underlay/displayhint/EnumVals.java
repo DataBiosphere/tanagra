e755e68bf876a4dd92310b998c1f4f910326b0da
Mariko Medlock <marikomedlock@google.com>
2023-10-11 14:05:59 -0400

    Allow enum value/display pairs that are columns in the same table.

--- underlay/src/main/java/bio/terra/tanagra/underlay/displayhint/EnumVals.java
+++ underlay/src/main/java/bio/terra/tanagra/underlay/displayhint/EnumVals.java
@@ -12,7 +12,6 @@ import bio.terra.tanagra.query.Query;
 import bio.terra.tanagra.query.QueryRequest;
 import bio.terra.tanagra.query.QueryResult;
 import bio.terra.tanagra.query.RowResult;
-import bio.terra.tanagra.query.TablePointer;
 import bio.terra.tanagra.query.TableVariable;
 import bio.terra.tanagra.serialization.UFDisplayHint;
 import bio.terra.tanagra.serialization.displayhint.UFEnumVals;
@@ -21,6 +20,7 @@ import bio.terra.tanagra.underlay.DisplayHint;
 import bio.terra.tanagra.underlay.ValueDisplay;
 import java.util.*;
 import java.util.stream.Collectors;
+import javax.annotation.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -89,35 +89,7 @@ public final class EnumVals extends DisplayHint {
    * <p>LIMIT 101
    */
   public static EnumVals computeForField(Literal.DataType dataType, FieldPointer value) {
-    Query query = queryPossibleEnumVals(value);
-    List<ColumnSchema> columnSchemas =
-        List.of(
-            new ColumnSchema(
-                ENUM_VALUE_COLUMN_ALIAS, CellValue.SQLDataType.fromUnderlayDataType(dataType)),
-            new ColumnSchema(ENUM_COUNT_COLUMN_ALIAS, CellValue.SQLDataType.INT64));
-
-    DataPointer dataPointer = value.getTablePointer().getDataPointer();
-    QueryRequest queryRequest =
-        new QueryRequest(query.renderSQL(), new ColumnHeaderSchema(columnSchemas));
-    QueryResult queryResult = dataPointer.getQueryExecutor().execute(queryRequest);
-
-    List<EnumVal> enumVals = new ArrayList<>();
-    Iterator<RowResult> rowResultIter = queryResult.getRowResults().iterator();
-    while (rowResultIter.hasNext()) {
-      RowResult rowResult = rowResultIter.next();
-      String val = rowResult.get(ENUM_VALUE_COLUMN_ALIAS).getString().orElse(null);
-      long count = rowResult.get(ENUM_COUNT_COLUMN_ALIAS).getLong().getAsLong();
-      enumVals.add(new EnumVal(new ValueDisplay(val), count));
-      if (enumVals.size() > MAX_ENUM_VALS_FOR_DISPLAY_HINT) {
-        // if there are more than the max number of values, then skip the display hint
-        LOGGER.info(
-            "Skipping enum values display hint because there are >{} possible values: {}",
-            MAX_ENUM_VALS_FOR_DISPLAY_HINT,
-            value.getColumnName());
-        return null;
-      }
-    }
-    return new EnumVals(enumVals);
+    return new EnumVals(queryPossibleEnumVals(dataType, value, null));
   }
 
   /**
@@ -141,67 +113,73 @@ public final class EnumVals extends DisplayHint {
    */
   public static EnumVals computeForField(
       Literal.DataType dataType, FieldPointer value, FieldPointer display) {
-    if (!display.isForeignKey() && !display.hasSqlFunctionWrapper()) {
-      throw new UnsupportedOperationException(
-          "Display fields that are not foreign key fields are not yet supported: "
-              + value.getColumnName()
-              + ", "
-              + display.getColumnName());
+    List<EnumVal> enumVals = queryPossibleEnumVals(dataType, value, display);
+
+    // Check that there is exactly one display per value.
+    Map<Literal, String> valDisplay = new HashMap<>();
+    enumVals.stream()
+        .forEach(
+            enumVal -> {
+              if (valDisplay.containsKey(enumVal.getValueDisplay().getValue())) {
+                throw new InvalidConfigException(
+                    "Found >1 possible display for the enum value "
+                        + enumVal.getValueDisplay().getValue());
+              } else {
+                valDisplay.put(
+                    enumVal.getValueDisplay().getValue(), enumVal.getValueDisplay().getDisplay());
+              }
+            });
+    return new EnumVals(enumVals);
+  }
+
+  private static List<EnumVal> queryPossibleEnumVals(
+      Literal.DataType dataType, FieldPointer value, @Nullable FieldPointer display) {
+    List<TableVariable> nestedQueryTables = new ArrayList<>();
+    TableVariable nestedPrimaryTable = TableVariable.forPrimary(value.getTablePointer());
+    nestedQueryTables.add(nestedPrimaryTable);
+
+    FieldVariable nestedValueFieldVar =
+        value.buildVariable(nestedPrimaryTable, nestedQueryTables, ENUM_VALUE_COLUMN_ALIAS);
+    FieldVariable nestedDisplayFieldVar = null;
+    if (display != null) {
+      if (!value.getTablePointer().equals(display.getTablePointer())) {
+        throw new UnsupportedOperationException(
+            "Value and display must be in the same table to compute enum values display hint.");
+      }
+      nestedDisplayFieldVar =
+          display.buildVariable(nestedPrimaryTable, nestedQueryTables, ENUM_DISPLAY_COLUMN_ALIAS);
     }
+    FieldPointer countFieldPointer = value.toBuilder().sqlFunctionWrapper("COUNT").build();
+    FieldVariable nestedCountFieldVar =
+        countFieldPointer.buildVariable(
+            nestedPrimaryTable, nestedQueryTables, ENUM_COUNT_COLUMN_ALIAS);
 
-    Query possibleValuesQuery = queryPossibleEnumVals(value);
-    DataPointer dataPointer = value.getTablePointer().getDataPointer();
-    TablePointer possibleValsTable =
-        TablePointer.fromRawSql(possibleValuesQuery.renderSQL(), dataPointer);
-    FieldPointer possibleValField =
-        new FieldPointer.Builder()
-            .tablePointer(possibleValsTable)
-            .columnName(ENUM_VALUE_COLUMN_ALIAS)
-            .build();
-    FieldPointer possibleCountField =
-        new FieldPointer.Builder()
-            .tablePointer(possibleValsTable)
-            .columnName(ENUM_COUNT_COLUMN_ALIAS)
-            .build();
-    FieldPointer possibleDisplayField =
-        new FieldPointer.Builder()
-            .tablePointer(possibleValsTable)
-            .columnName(ENUM_VALUE_COLUMN_ALIAS)
-            .foreignTablePointer(display.getForeignTablePointer())
-            .foreignKeyColumnName(display.getForeignKeyColumnName())
-            .foreignColumnName(display.getForeignColumnName())
-            .sqlFunctionWrapper(display.getSqlFunctionWrapper())
-            .build();
+    List<FieldVariable> selectFields =
+        display == null
+            ? List.of(nestedValueFieldVar, nestedCountFieldVar)
+            : List.of(nestedValueFieldVar, nestedCountFieldVar, nestedDisplayFieldVar);
+    List<FieldVariable> groupByFields =
+        display == null
+            ? List.of(nestedValueFieldVar)
+            : List.of(nestedValueFieldVar, nestedDisplayFieldVar);
 
-    // build the outer query for the list of (possible value, display) pairs
-    List<TableVariable> tables = new ArrayList<>();
-    TableVariable primaryTable = TableVariable.forPrimary(possibleValsTable);
-    tables.add(primaryTable);
-
-    FieldVariable valueFieldVar =
-        possibleValField.buildVariable(primaryTable, tables, ENUM_VALUE_COLUMN_ALIAS);
-    FieldVariable countFieldVar =
-        possibleCountField.buildVariable(primaryTable, tables, ENUM_COUNT_COLUMN_ALIAS);
-    FieldVariable displayFieldVar =
-        possibleDisplayField.buildVariable(primaryTable, tables, ENUM_DISPLAY_COLUMN_ALIAS);
     Query query =
         new Query.Builder()
-            .select(List.of(valueFieldVar, countFieldVar, displayFieldVar))
-            .tables(tables)
-            .orderBy(List.of(new OrderByVariable(displayFieldVar)))
-            .limit(MAX_ENUM_VALS_FOR_DISPLAY_HINT + 1)
+            .select(selectFields)
+            .tables(nestedQueryTables)
+            .orderBy(List.of(new OrderByVariable(nestedValueFieldVar)))
+            .groupBy(groupByFields)
             .build();
+    List<ColumnSchema> columnSchemas = new ArrayList<>();
+    columnSchemas.add(
+        new ColumnSchema(
+            ENUM_VALUE_COLUMN_ALIAS, CellValue.SQLDataType.fromUnderlayDataType(dataType)));
+    columnSchemas.add(new ColumnSchema(ENUM_COUNT_COLUMN_ALIAS, CellValue.SQLDataType.INT64));
+    if (display != null) {
+      columnSchemas.add(new ColumnSchema(ENUM_DISPLAY_COLUMN_ALIAS, CellValue.SQLDataType.STRING));
+    }
 
-    LOGGER.info(
-        "SQL data type of value is: {}", CellValue.SQLDataType.fromUnderlayDataType(dataType));
-    List<ColumnSchema> columnSchemas =
-        List.of(
-            new ColumnSchema(
-                ENUM_VALUE_COLUMN_ALIAS, CellValue.SQLDataType.fromUnderlayDataType(dataType)),
-            new ColumnSchema(ENUM_COUNT_COLUMN_ALIAS, CellValue.SQLDataType.INT64),
-            new ColumnSchema(ENUM_DISPLAY_COLUMN_ALIAS, CellValue.SQLDataType.STRING));
-
-    // run the query
+    DataPointer dataPointer = value.getTablePointer().getDataPointer();
     QueryRequest queryRequest =
         new QueryRequest(query.renderSQL(), new ColumnHeaderSchema(columnSchemas));
     QueryResult queryResult = dataPointer.getQueryExecutor().execute(queryRequest);
@@ -212,49 +190,24 @@ public final class EnumVals extends DisplayHint {
     while (rowResultIter.hasNext()) {
       RowResult rowResult = rowResultIter.next();
       CellValue cellValue = rowResult.get(ENUM_VALUE_COLUMN_ALIAS);
-      enumVals.add(
-          new EnumVal(
-              new ValueDisplay(
-                  // TODO: Make a static NULL Literal instance, instead of overloading the String
-                  // value.
+      ValueDisplay valueDisplay =
+          (display == null)
+              ? new ValueDisplay(cellValue.getLiteral().orElse(new Literal((String) null)))
+              : new ValueDisplay(
                   cellValue.getLiteral().orElse(new Literal((String) null)),
-                  rowResult.get(ENUM_DISPLAY_COLUMN_ALIAS).getString().orElse(null)),
-              rowResult.get(ENUM_COUNT_COLUMN_ALIAS).getLong().getAsLong()));
+                  rowResult.get(ENUM_DISPLAY_COLUMN_ALIAS).getString().orElse(null));
+      enumVals.add(
+          new EnumVal(valueDisplay, rowResult.get(ENUM_COUNT_COLUMN_ALIAS).getLong().getAsLong()));
       if (enumVals.size() > MAX_ENUM_VALS_FOR_DISPLAY_HINT) {
         // if there are more than the max number of values, then skip the display hint
         LOGGER.info(
             "Skipping enum values display hint because there are >{} possible values: {}",
             MAX_ENUM_VALS_FOR_DISPLAY_HINT,
-            valueFieldVar.getAlias());
+            value.getColumnName());
         return null;
       }
     }
-
-    // Sort the enum values, so the expanded config has a consistent ordering.
-    enumVals.sort(Comparator.comparing(ev -> String.valueOf(ev.getValueDisplay().getDisplay())));
-
-    return new EnumVals(enumVals);
-  }
-
-  private static Query queryPossibleEnumVals(FieldPointer value) {
-    List<TableVariable> nestedQueryTables = new ArrayList<>();
-    TableVariable nestedPrimaryTable = TableVariable.forPrimary(value.getTablePointer());
-    nestedQueryTables.add(nestedPrimaryTable);
-
-    FieldVariable nestedValueFieldVar =
-        value.buildVariable(nestedPrimaryTable, nestedQueryTables, ENUM_VALUE_COLUMN_ALIAS);
-
-    FieldPointer countFieldPointer = value.toBuilder().sqlFunctionWrapper("COUNT").build();
-    FieldVariable nestedCountFieldVar =
-        countFieldPointer.buildVariable(
-            nestedPrimaryTable, nestedQueryTables, ENUM_COUNT_COLUMN_ALIAS);
-
-    return new Query.Builder()
-        .select(List.of(nestedValueFieldVar, nestedCountFieldVar))
-        .tables(nestedQueryTables)
-        .orderBy(List.of(new OrderByVariable(nestedValueFieldVar)))
-        .groupBy(List.of(nestedValueFieldVar))
-        .build();
+    return enumVals;
   }
 
   @Override
