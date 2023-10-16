d7b5a4e2533348963b7b0087d4c26ebc2876ba86
Chenchal Subraveti <c.subraveti@vumc.org>
2023-10-16 12:42:30 +0530

    Adding cpt4 to CT Note we have cpt4_device_occurrence but not added to top level json, since devices domain s in a different PR.

---
+++ service/src/main/resources/config/aou/test/SC2023Q3R1/sql/t_cpt4_device_occurrence.sql
@@ -0,0 +1,7 @@
+SELECT de.device_exposure_id, pc.id AS cpt4_id
+FROM `all-of-us-ehr-dev.SC2023Q3R1.device_exposure` AS de
+
+JOIN `all-of-us-ehr-dev.SC2023Q3R1.prep_cpt` AS pc
+ON pc.concept_id = de.device_concept_id
+
+WHERE pc.type = 'CPT4'
---
+++ service/src/main/resources/config/aou/test/SC2023Q3R1/sql/t_cpt4_measurement_occurrence.sql
@@ -0,0 +1,7 @@
+SELECT mo.measurement_id, pc.id AS cpt4_id
+FROM `all-of-us-ehr-dev.SC2023Q3R1.measurement` AS mo
+
+JOIN `all-of-us-ehr-dev.SC2023Q3R1.prep_cpt` AS pc
+ON pc.concept_id = mo.measurement_source_concept_id
+
+WHERE pc.type = 'CPT4'
