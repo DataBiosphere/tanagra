554f7a937493fedcfb040d59ec9e3a044ee46062
Mariko Medlock <marikomedlock@google.com>
2023-10-16 12:14:00 -0400

    Update test generated sql.

--- service/src/test/resources/sql/sddrefresh0323/genotyping-noFilter.sql
+++ service/src/test/resources/sql/sddrefresh0323/genotyping-noFilter.sql
@@ -1,1 +1,1 @@
-SELECT g.id AS id, g.name AS name FROM `verily-tanagra-dev.sd20230331_index_100523`.genotyping AS g LIMIT 30
+SELECT g.id AS id, g.name AS name FROM `verily-tanagra-dev.sd20230331_index_101623`.genotyping AS g LIMIT 30
