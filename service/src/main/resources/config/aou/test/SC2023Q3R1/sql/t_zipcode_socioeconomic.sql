f4a6908788c5956a71e25f92e3e99ba39e710246
Chenchal Subraveti <chenchals@users.noreply.github.com>
2023-10-17 05:04:00 +0530

    [DT-294-524-525] tanagra aou rt/ct zipcode-genomics-visits-device underlay updates  (#582)
    * updated for zip code socioeconomic status data for CT
    rebasing branch 'chenchals/DT-525-tanagra-Aou-CT-zipcode' on '2a51f7a8'.
    
    * updated RT and CT with has_ehr_data
    
    * added add ehr
    
    * added add ehr
    
    * rebase and updated for visit
    
    * Updated for visit domain, genomics, prepackaged zipcode_socioeconomic for CT
    
    * Updated for visit domain, genomics, prepackaged zipcode_socioeconomic for CT
    
    * Updated for visit domain for RT, modified default sort for visit
    
    * Updated for visit domain for RT, modified default sort for visit
    
    * Updated for Device in both RT and CT. Fixed cross dataset reference in SQLs for RT :-)
    
    * Updated project ids to within vpc secure perimeter
    
    * Updated for device
    
    * Updated zipcode_socioeconomic tables
    
    * updated device removed groupToItems
    
    * removed groupToItems from type=CRITERIA_OCCURRENCE
    
    * fixed sql

---
+++ service/src/main/resources/config/aou/test/SC2023Q3R1/sql/t_zipcode_socioeconomic.sql
@@ -0,0 +1,22 @@
+SELECT
+    ROW_NUMBER() OVER() AS row_id,
+    observation.person_id,
+    timestamp(observation.observation_datetime) as observation_datetime_utc,
+    format_datetime('%Y-%m-%dT%H:%M:%S', observation.observation_datetime) as observation_datetime_str,
+    zip_code.zip3_as_string as zip_code,
+    zip_code.fraction_assisted_income as assisted_income,
+    zip_code.fraction_high_school_edu as high_school_education,
+    zip_code.median_income,
+    zip_code.fraction_no_health_ins as no_health_insurance,
+    zip_code.fraction_poverty as poverty,
+    zip_code.fraction_vacant_housing as vacant_housing,
+    zip_code.deprivation_index,
+    zip_code.acs as american_community_survey_year
+FROM
+    `all-of-us-ehr-dev.SC2023Q3R1.zip3_ses_map` zip_code
+JOIN
+    `all-of-us-ehr-dev.SC2023Q3R1.observation` observation
+        ON CAST(SUBSTR(observation.value_as_string,
+            0, STRPOS(observation.value_as_string,'*') - 1) AS INT64) = zip_code.zip3
+    AND observation_source_concept_id = 1585250
+    AND observation.value_as_string NOT LIKE 'Res%'
