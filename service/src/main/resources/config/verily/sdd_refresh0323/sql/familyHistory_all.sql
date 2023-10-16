66b0093b1ea1fe50d9d284345a74ca40b3674acd
Mariko Medlock <marikomedlock@google.com>
2023-10-16 14:27:20 -0400

    Add person_source_value attribute.

--- service/src/main/resources/config/verily/sdd_refresh0323/sql/familyHistory_all.sql
+++ service/src/main/resources/config/verily/sdd_refresh0323/sql/familyHistory_all.sql
@@ -1,6 +1,6 @@
 SELECT
   ROW_NUMBER() OVER (ORDER BY xfh.uniq) AS id, xfh.person_id,
-  xfh.entry_date, xfh.item AS note_text,
+  xfh.entry_date, xfh.item AS note_text, xfh.family_history_src_name, xfc.relation_name,
   CAST(FLOOR(TIMESTAMP_DIFF(xfh.entry_date, p.birth_datetime, DAY) / 365.25) AS INT64) AS age_at_occurrence,
   xfh.visit_occurrence_id, vo.visit_concept_id
 
