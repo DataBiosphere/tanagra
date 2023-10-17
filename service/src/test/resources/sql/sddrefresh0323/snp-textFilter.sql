766b0362fbf88c5c882d42392ca2efadefa69291
marikomedlock <marikomedlock@users.noreply.github.com>
2023-10-17 09:50:51 -0400

    Allow enum value/display pairs that are columns in the same table. (#581)
    * Allow enum value/display pairs that are columns in the same table.
    
    * Fix literal compareTo NPE.
    
    * Re-index verily/sdd.
    
    * Update test generated sql.
    
    * Handle null enum vals list.
    
    * Add family history entity + group. (#585)
    
    * Add familyHistory entity + group.
    
    * Add person_source_value attribute.
    
    * Make same changes to vumc/sdd underlay.
    
    * Add config files for vitals. (#586)
    
    * Blood pressure entity + group.
    
    * Fix status_code data type.
    
    * Add pulse entity + group.
    
    * Add sleep after poll for table existence in entity-level hints job.
    
    * Add respiratory rate entity + group.
    
    * Add bmi entity + group.
    
    * Add height, weight entities + groups.
    
    * Cast is_clean to boolean.
    
    * Make same changes to vumc/sdd underlay.
    
    * Fix typo in family history sql.

--- service/src/test/resources/sql/sddrefresh0323/snp-textFilter.sql
+++ service/src/test/resources/sql/sddrefresh0323/snp-textFilter.sql
@@ -1,1 +1,1 @@
-SELECT s.id AS id, s.name AS name FROM `verily-tanagra-dev.sd20230331_index_100523`.snp AS s WHERE REGEXP_CONTAINS(UPPER(s.id), UPPER('RS1292')) LIMIT 30
+SELECT s.id AS id, s.name AS name FROM `verily-tanagra-dev.sd20230331_index_101623`.snp AS s WHERE REGEXP_CONTAINS(UPPER(s.id), UPPER('RS1292')) LIMIT 30
