800e85f5455ad22d825b97e6ba03cc0e64bb3bec
Mariko Medlock <marikomedlock@google.com>
2023-11-08 09:39:37 -0500

    Fix aouSR2019q4r4 occurrence date/timestamp fields.

---
+++ underlay/src/main/resources/config/datamapping/aouSR2019q4r4/entity/conditionOccurrence/all.sql
@@ -0,0 +1,20 @@
+SELECT
+  co.condition_occurrence_id,
+  co.person_id,
+  co.condition_concept_id,
+  cc.concept_name AS condition_concept_name,
+  co.condition_start_datetime,
+  co.condition_end_datetime,
+  co.stop_reason,
+  co.condition_source_value,
+  co.condition_source_concept_id,
+  CAST(FLOOR(TIMESTAMP_DIFF(co.condition_start_datetime, p.birth_datetime, DAY) / 365.25) AS INT64) AS age_at_occurrence,
+  co.visit_occurrence_id
+
+FROM `${omopDataset}.condition_occurrence` AS co
+
+JOIN `${omopDataset}.person` AS p
+    ON p.person_id = co.person_id
+
+JOIN `${omopDataset}.concept` AS cc
+    ON cc.concept_id = co.condition_concept_id
