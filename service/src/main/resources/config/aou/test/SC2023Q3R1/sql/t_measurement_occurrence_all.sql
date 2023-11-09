c058d848fe9b8474c70461559bc16b546b3b2ae9
freemabd <brian.d.freeman@gmail.com>
2023-11-09 12:36:09 -0600

    DT-321 updating config

--- service/src/main/resources/config/aou/test/SC2023Q3R1/sql/t_measurement_occurrence_all.sql
+++ service/src/main/resources/config/aou/test/SC2023Q3R1/sql/t_measurement_occurrence_all.sql
@@ -2,7 +2,7 @@ SELECT
   mo.measurement_id,
   mo.person_id,
   CASE WHEN mo.measurement_concept_id IS NULL THEN 0 ELSE mo.measurement_concept_id END AS measurement_concept_id,
-  mo.measurement_datetime,
+  mo.measurement_datetime AS date,
   mo.value_as_number AS value_numeric,
   mo.value_as_concept_id,
   mo.unit_concept_id,
@@ -15,4 +15,5 @@ FROM `all-of-us-ehr-dev.SC2023Q3R1.measurement` AS mo
 JOIN `all-of-us-ehr-dev.SC2023Q3R1.person` AS p ON p.person_id = mo.person_id
 LEFT JOIN `all-of-us-ehr-dev.SC2023Q3R1.visit_occurrence` AS vo ON vo.visit_occurrence_id = mo.visit_occurrence_id
 LEFT JOIN `all-of-us-ehr-dev.SC2023Q3R1.concept` AS c ON vo.visit_concept_id = c.concept_id
+WHERE mo.measurement_concept_id != 0
 
