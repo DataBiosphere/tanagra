3741a5fdc8fb9f89020ff6e432d98c0fc858d957
Mariko Medlock <marikomedlock@google.com>
2023-11-13 21:11:07 -0500

    First pass aou underlay.

--- underlay/src/main/resources/config/datamapping/aou/entity/measurementOccurrence/all.sql
+++ underlay/src/main/resources/config/datamapping/aou/entity/measurementOccurrence/all.sql
@@ -3,7 +3,7 @@ SELECT
   mo.person_id,
   (CASE WHEN mo.measurement_concept_id IS NULL THEN 0 ELSE mo.measurement_concept_id END) AS measurement_concept_id,
   mc.concept_name AS measurement_concept_name,
-  mo.measurement_date,
+  mo.measurement_datetime,
   mo.value_as_number,
   mo.value_as_concept_id,
   evc.concept_name AS value_as_concept_name,
@@ -19,7 +19,7 @@ SELECT
 FROM `${omopDataset}.measurement` AS mo
 
 JOIN `${omopDataset}.person` AS p
-ON p.person_id = mo.person_id
+    ON p.person_id = mo.person_id
 
 LEFT JOIN `${omopDataset}.concept` AS mc
     ON mc.concept_id = mo.measurement_concept_id
@@ -31,9 +31,9 @@ LEFT JOIN `${omopDataset}.concept` AS uc
     ON uc.concept_id = mo.unit_concept_id
 
 LEFT JOIN `${omopDataset}.visit_occurrence` AS vo
-ON vo.visit_occurrence_id = mo.visit_occurrence_id
+    ON vo.visit_occurrence_id = mo.visit_occurrence_id
 
 LEFT JOIN `${omopDataset}.concept` AS vc
     ON vc.concept_id = vo.visit_concept_id
-    AND vc.domain_id = 'Visit'
-    AND vc.standard_concept = 'S'
+
+WHERE mo.measurement_concept_id != 0
--- underlay/src/main/resources/config/datamapping/omop/entity/measurementSnomed/rootNode.sql
+++
@@ -1,6 +0,0 @@
-SELECT
-    concept_id
-FROM `${omopDataset}.concept`
-WHERE
-    domain_id = 'Measurement'
-    AND vocabulary_id = 'SNOMED'
