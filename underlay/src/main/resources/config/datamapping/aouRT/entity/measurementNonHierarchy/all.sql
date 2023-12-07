42e26a5e906ef8a3832691e088d5b19350a7f09f
Chenchal Subraveti <c.subraveti@vumc.org>
2023-12-07 16:48:23 -0600

    Fix joins (DT-623) for measurement non-hierarchy, SNOMED, LOINC, HCPCS

--- underlay/src/main/resources/config/datamapping/aouRT/entity/measurementNonHierarchy/all.sql
+++ underlay/src/main/resources/config/datamapping/aouRT/entity/measurementNonHierarchy/all.sql
@@ -4,6 +4,8 @@ JOIN `${omopDataset}.concept` c ON m.measurement_concept_id = c.concept_id
 WHERE c.domain_id = 'Measurement'
   AND c.vocabulary_id = 'HCPCS'
   AND c.standard_concept = 'S'
+  AND m.measurement_concept_id IS NOT null
+  AND m.measurement_concept_id != 0
 UNION DISTINCT
 SELECT DISTINCT c.*
 FROM `${omopDataset}.measurement` m
@@ -12,3 +14,5 @@ WHERE c.domain_id = 'Measurement'
   AND c.vocabulary_id = 'LOINC'
   AND c.standard_concept = 'S'
   AND c.concept_class_id = 'Clinical Observation'
+  AND m.measurement_concept_id IS NOT null
+  AND m.measurement_concept_id != 0
