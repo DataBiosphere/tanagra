51b455a4348ce583827c22faa1c6b669f5d8889f
Chenchal Subraveti <c.subraveti@vumc.org>
2023-12-07 10:27:21 -0600

    Fix joins (DT-623) for procedure and missed-condition

--- underlay/src/main/resources/config/datamapping/aouRT/entitygroup/cpt4Person/occurrenceCriteria_procedure.sql
+++ underlay/src/main/resources/config/datamapping/aouRT/entitygroup/cpt4Person/occurrenceCriteria_procedure.sql
@@ -1,7 +1,7 @@
 SELECT po.procedure_occurrence_id, pc.id AS cpt4_id
 FROM `${omopDataset}.procedure_occurrence` AS po
-
 JOIN `${staticTablesDataset}.prep_cpt` AS pc
     ON pc.concept_id = po.procedure_source_concept_id
-
 WHERE pc.type = 'CPT4'
+  AND procedure_source_concept_id IS NOT null
+  AND procedure_source_concept_id != 0
