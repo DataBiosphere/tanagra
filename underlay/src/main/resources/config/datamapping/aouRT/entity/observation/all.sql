0c6a86dc9b16dc6627947a2ec99f06007c9a49a6
Chenchal Subraveti <c.subraveti@vumc.org>
2023-12-08 20:34:52 -0600

    Fix joins - observation

--- underlay/src/main/resources/config/datamapping/aouRT/entity/observation/all.sql
+++ underlay/src/main/resources/config/datamapping/aouRT/entity/observation/all.sql
@@ -4,9 +4,7 @@ SELECT
     vocabulary_id,
     concept_code,
     (CASE WHEN standard_concept IS NULL THEN 'Source' WHEN standard_concept = 'S' THEN 'Standard' ELSE 'Unknown' END) AS standard_concept
-
 FROM `${omopDataset}.concept`
-
 WHERE domain_id = 'Observation'
     AND standard_concept = 'S'
     AND vocabulary_id != 'PPI'
--- underlay/src/main/resources/config/datamapping/aouRT/entitygroup/observationPerson/primaryCriteria.sql
+++ underlay/src/main/resources/config/datamapping/aouRT/entitygroup/observationPerson/primaryCriteria.sql
@@ -1,5 +1,12 @@
 SELECT
   o.person_id,
   o.observation_concept_id
-
 FROM `${omopDataset}.observation` AS o
+WHERE o.measurement_concept_id
+  IN (SELECT concept_id
+      FROM `${omopDataset}.concept` oc
+      WHERE oc.domain_id = 'Observation'
+        AND oc.standard_concept = 'S'
+        AND oc.vocabulary_id != 'PPI'
+        AND oc.concept_class_id != 'Survey'
+      )
