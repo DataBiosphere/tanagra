2c31342423aa07a4f5e89650a6bcde9fc5daa93a
Chenchal Subraveti <c.subraveti@vumc.org>
2023-12-11 14:16:01 -0600

    Fix visit joins

--- underlay/src/main/resources/config/datamapping/aouRT/entity/visit/all.sql
+++ underlay/src/main/resources/config/datamapping/aouRT/entity/visit/all.sql
@@ -3,13 +3,10 @@ SELECT
     concept_name,
     vocabulary_id,
     concept_code,
-    (CASE WHEN standard_concept IS NULL THEN 'Source' WHEN standard_concept = 'S' THEN 'Standard' ELSE 'Unknown' END) AS standard_concept
-
+    'Standard'  AS standard_concept
 FROM `${omopDataset}.visit_occurrence` AS vo
-
-JOIN `${omopDataset}.concept` c
-    ON vo.visit_concept_id = c.concept_id
-
-WHERE c.domain_id = 'Visit'
-  AND c.standard_concept = 'S'
-  AND vo.visit_concept_id > 0
+JOIN `${omopDataset}.concept` c ON vo.visit_concept_id = c.concept_id
+    AND c.domain_id = 'Visit'
+    AND c.standard_concept = 'S'
+WHERE vo.visit_concept_id IS NOT null
+    AND vo.visit_concept_id != 0
