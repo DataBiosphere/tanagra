abcd3f25db2bec257f8041027d03a9dbfccf3989
Chenchal Subraveti <c.subraveti@vumc.org>
2024-01-03 16:42:41 -0600

    fix fine-tune for cpt4- added sql for device and sorted sub-sqls by domain name

--- underlay/src/main/resources/config/datamapping/aouRT/entity/cpt4/all.sql
+++ underlay/src/main/resources/config/datamapping/aouRT/entity/cpt4/all.sql
@@ -4,7 +4,7 @@ SELECT
     CASE WHEN c.concept_name IS NULL THEN pc.name ELSE c.concept_name END AS name,
     pc.type,
     (CASE WHEN pc.is_standard = 0 THEN 'Source' ELSE 'Standard' END) AS standard_concept,
-    pc.code as concept_code
+    pc.code as concept_code,
     CASE WHEN pc.code IS NULL THEN pc.name ELSE CONCAT(pc.code, ' ', pc.name) END AS label
 
 FROM `${staticTablesDataset}.prep_cpt` pc
