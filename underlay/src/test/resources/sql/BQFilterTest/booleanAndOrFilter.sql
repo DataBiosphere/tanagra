0fae8a334a4f01560eb951e43df538ccc79bbea6
Mariko Medlock <marikomedlock@google.com>
2024-01-04 11:30:18 -0500

    Fix golden file for boolean andor filter.

--- underlay/src/test/resources/sql/BQFilterTest/booleanAndOrFilter.sql
+++ underlay/src/test/resources/sql/BQFilterTest/booleanAndOrFilter.sql
@@ -4,5 +4,9 @@
     FROM
         ${ENT_condition}      
     WHERE
-        concept_code != @val          
-        AND REGEXP_CONTAINS(UPPER(T_TXT), UPPER(@val0))
+        (
+            concept_code != @val         
+        )          
+        AND (
+            REGEXP_CONTAINS(UPPER(T_TXT), UPPER(@val0))         
+        )
