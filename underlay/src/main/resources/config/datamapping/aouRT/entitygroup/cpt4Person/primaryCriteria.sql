afa917696e784cf72f80f1b503438685d026d7e7
Chenchal Subraveti <chenchals@users.noreply.github.com>
2024-01-04 11:03:42 -0600

    [DT-656-658] Fine-tune for ic10cm, icd10pcs, and cpt4 (#653)
    * fine-tune for ic10cm, icd10pcs, and cpt4
    
    * fix fine-tune for cpt4- added sql for device and sorted sub-sqls by domain name
    
    * fix fine-tune for cpt4- added sql for device and sorted sub-sqls by domain name
    
    * fix fine-tune for cpt4- added sql for device and sorted sub-sqls by domain name

--- underlay/src/main/resources/config/datamapping/aouRT/entitygroup/cpt4Person/primaryCriteria.sql
+++ underlay/src/main/resources/config/datamapping/aouRT/entitygroup/cpt4Person/primaryCriteria.sql
@@ -1,17 +1,24 @@
-SELECT po.person_id, pc.id AS cpt4_id
-FROM `${omopDataset}.procedure_occurrence` AS po
-JOIN `${staticTablesDataset}.prep_cpt` AS pc
-    ON pc.concept_id = po.procedure_source_concept_id
+SELECT io.person_id, pc.id AS cpt4_id
+FROM `${omopDataset}.device_exposure` AS io
+    JOIN `${staticTablesDataset}.prep_cpt` AS pc ON pc.concept_id = io.device_source_concept_id
 WHERE pc.type = 'CPT4'
-  AND po.procedure_source_concept_id IS NOT null
-  AND po.procedure_source_concept_id != 0
+  AND io.device_source_concept_id IS NOT null
+  AND io.device_source_concept_id != 0
+
+UNION ALL
+
+SELECT io.person_id, pc.id AS cpt4_id
+FROM `${omopDataset}.drug_exposure` AS io
+    JOIN `${staticTablesDataset}.prep_cpt` AS pc ON pc.concept_id = io.drug_source_concept_id
+WHERE pc.type = 'CPT4'
+  AND io.drug_source_concept_id IS NOT null
+  AND io.drug_source_concept_id != 0
 
 UNION ALL
 
 SELECT mo.person_id, pc.id AS cpt4_id
 FROM `${omopDataset}.measurement` AS mo
-JOIN `${staticTablesDataset}.prep_cpt` AS pc
-    ON pc.concept_id = mo.measurement_source_concept_id
+    JOIN `${staticTablesDataset}.prep_cpt` AS pc ON pc.concept_id = mo.measurement_source_concept_id
 WHERE pc.type = 'CPT4'
   AND mo.measurement_source_concept_id IS NOT null
   AND mo.measurement_source_concept_id != 0
@@ -20,18 +27,16 @@ UNION ALL
 
 SELECT oo.person_id, pc.id AS cpt4_id
 FROM `${omopDataset}.observation` AS oo
-JOIN `${staticTablesDataset}.prep_cpt` AS pc
-    ON pc.concept_id = oo.observation_source_concept_id
+    JOIN `${staticTablesDataset}.prep_cpt` AS pc ON pc.concept_id = oo.observation_source_concept_id
 WHERE pc.type = 'CPT4'
   AND oo.observation_source_concept_id IS NOT null
   AND oo.observation_source_concept_id != 0
 
 UNION ALL
 
-SELECT io.person_id, pc.id AS cpt4_id
-FROM `${omopDataset}.drug_exposure` AS io
-JOIN `${staticTablesDataset}.prep_cpt` AS pc
-    ON pc.concept_id = io.drug_source_concept_id
+SELECT po.person_id, pc.id AS cpt4_id
+FROM `${omopDataset}.procedure_occurrence` AS po
+    JOIN `${staticTablesDataset}.prep_cpt` AS pc ON pc.concept_id = po.procedure_source_concept_id
 WHERE pc.type = 'CPT4'
-  AND io.drug_source_concept_id IS NOT null
-  AND io.drug_source_concept_id != 0
+  AND po.procedure_source_concept_id IS NOT null
+  AND po.procedure_source_concept_id != 0
