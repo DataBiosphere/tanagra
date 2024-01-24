e6b21248f35555cab25d8edd0f309595a4794e65
Chenchal Subraveti <c.subraveti@vumc.org>
2024-01-24 20:05:11 +0530

    Updated for icd9cm and icd9proc non-hierarchy concepts

---
+++ underlay/src/main/resources/config/datamapping/aouRT/entitygroup/icd9procNonHierarchyPerson/primaryCriteria.sql
@@ -0,0 +1,14 @@
+SELECT co.person_id, c.concept_id
+FROM `${omopDataset}.procedure_occurrence` AS co
+JOIN `${omopDataset}.concept` AS c
+    ON c.concept_id = co.condition_source_concept_id
+WHERE c.vocabulary_id = 'ICD9Proc'
+  AND c.concept_id NOT IN(
+    SELECT concept_id FROM `${omopDataset}.concept`
+    WHERE vocabulary_id = 'ICD9Proc'
+      AND DATE_DIFF(CAST(valid_end_date AS DATE), CURRENT_DATE(), DAY) > 0
+    UNION ALL
+    SELECT concept_id FROM `${staticTablesDataset}.prep_concept`
+    WHERE vocabulary_id = 'ICD9Proc'
+      AND DATE_DIFF(CAST(valid_end_date AS DATE), CURRENT_DATE(), DAY) > 0
+)
