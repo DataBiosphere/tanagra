e9b074c9eb8f57e82c54bdfa515c4846af36bafd
Mariko Medlock <marikomedlock@google.com>
2024-01-29 09:22:23 -0500

    race_concept_id > race_source_concept_id in SQL file.

--- underlay/src/main/resources/config/datamapping/sd/entity/person/all.sql
+++ underlay/src/main/resources/config/datamapping/sd/entity/person/all.sql
@@ -1,7 +1,7 @@
 SELECT
     p.person_id, p.year_of_birth, p.birth_datetime, p.person_source_value,
     p.gender_concept_id, gc.concept_name AS gender_concept_name,
-    p.race_concept_id, rc.concept_name AS race_concept_name,
+    p.race_source_concept_id, rc.concept_name AS race_concept_name,
     p.ethnicity_concept_id, ec.concept_name AS ethnicity_concept_name,
 
     /* Add BioVU sample columns. The way x_biovu_sample_status is created, there should be at
@@ -21,7 +21,7 @@ LEFT JOIN `${omopDataset}.concept` gc
 ON gc.concept_id = p.gender_concept_id
 
 LEFT JOIN `${omopDataset}.concept` rc
-ON rc.concept_id = p.race_concept_id
+ON rc.concept_id = p.race_source_concept_id
 
 LEFT JOIN `${omopDataset}.concept` ec
 ON ec.concept_id = p.ethnicity_concept_id
