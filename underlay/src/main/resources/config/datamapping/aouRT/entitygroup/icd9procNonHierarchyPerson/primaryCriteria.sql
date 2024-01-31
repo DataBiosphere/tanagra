5be2089825b71c3431933296de542dae508413cc
Chenchal Subraveti <c.subraveti@vumc.org>
2024-01-31 16:39:55 -0600

    Update for icd9proc

--- underlay/src/main/resources/config/datamapping/aouRT/entitygroup/icd9procNonHierarchyPerson/primaryCriteria.sql
+++ underlay/src/main/resources/config/datamapping/aouRT/entitygroup/icd9procNonHierarchyPerson/primaryCriteria.sql
@@ -12,3 +12,20 @@ WHERE c.vocabulary_id = 'ICD9Proc'
     WHERE vocabulary_id = 'ICD9Proc'
       AND DATE_DIFF(CAST(valid_end_date AS DATE), CURRENT_DATE(), DAY) > 0
 )
+
+UNION ALL
+
+SELECT io.person_id, c.concept_id
+FROM `${omopDataset}.drug_exposure` AS io
+JOIN `${omopDataset}.concept` AS c
+    ON c.concept_id = io.drug_source_concept_id
+WHERE c.vocabulary_id = 'ICD9Proc'
+  AND c.concept_id NOT IN(
+    SELECT concept_id FROM `${omopDataset}.concept`
+    WHERE vocabulary_id = 'ICD9Proc'
+      AND DATE_DIFF(CAST(valid_end_date AS DATE), CURRENT_DATE(), DAY) > 0
+    UNION ALL
+    SELECT concept_id FROM `${staticTablesDataset}.prep_concept`
+    WHERE vocabulary_id = 'ICD9Proc'
+      AND DATE_DIFF(CAST(valid_end_date AS DATE), CURRENT_DATE(), DAY) > 0
+)
