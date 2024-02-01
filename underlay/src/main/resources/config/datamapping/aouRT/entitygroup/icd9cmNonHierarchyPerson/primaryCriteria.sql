cc78005435d7d05381ba3f75b0ed5d68e6a1148e
Chenchal Subraveti <c.subraveti@vumc.org>
2024-01-31 18:12:26 -0600

    Update for icd9cm non-hierarchy person counts

--- underlay/src/main/resources/config/datamapping/aouRT/entitygroup/icd9cmNonHierarchyPerson/primaryCriteria.sql
+++ underlay/src/main/resources/config/datamapping/aouRT/entitygroup/icd9cmNonHierarchyPerson/primaryCriteria.sql
@@ -12,3 +12,54 @@ WHERE c.vocabulary_id = 'ICD9CM'
     WHERE vocabulary_id = 'ICD9CM'
       AND DATE_DIFF(CAST(valid_end_date AS DATE), CURRENT_DATE(), DAY) > 0
 )
+
+UNION ALL
+
+SELECT mo.person_id, c.concept_id
+FROM `${omopDataset}.measurement` AS mo
+         JOIN `${omopDataset}.concept` AS c
+              ON c.concept_id = mo.measurement_source_concept_id
+WHERE c.vocabulary_id = 'ICD9CM'
+  AND c.concept_id NOT IN(
+    SELECT concept_id FROM `${omopDataset}.concept`
+    WHERE vocabulary_id = 'ICD9Proc'
+      AND DATE_DIFF(CAST(valid_end_date AS DATE), CURRENT_DATE(), DAY) > 0
+    UNION ALL
+    SELECT concept_id FROM `${staticTablesDataset}.prep_concept`
+    WHERE vocabulary_id = 'ICD9Proc'
+      AND DATE_DIFF(CAST(valid_end_date AS DATE), CURRENT_DATE(), DAY) > 0
+)
+
+UNION ALL
+
+SELECT oo.person_id, c.concept_id
+FROM `${omopDataset}.observation` AS oo
+         JOIN `${omopDataset}.concept` AS c
+              ON c.concept_id = oo.observation_source_concept_id
+WHERE c.vocabulary_id = 'ICD9CM'
+  AND c.concept_id NOT IN(
+    SELECT concept_id FROM `${omopDataset}.concept`
+    WHERE vocabulary_id = 'ICD9Proc'
+      AND DATE_DIFF(CAST(valid_end_date AS DATE), CURRENT_DATE(), DAY) > 0
+    UNION ALL
+    SELECT concept_id FROM `${staticTablesDataset}.prep_concept`
+    WHERE vocabulary_id = 'ICD9Proc'
+      AND DATE_DIFF(CAST(valid_end_date AS DATE), CURRENT_DATE(), DAY) > 0
+)
+
+UNION ALL
+
+SELECT po.person_id, c.concept_id
+FROM `${omopDataset}.procedure_occurrence` AS po
+JOIN `${omopDataset}.concept` AS c
+    ON c.concept_id = po.procedure_source_concept_id
+WHERE c.vocabulary_id = 'ICD9CM'
+  AND c.concept_id NOT IN(
+    SELECT concept_id FROM `${omopDataset}.concept`
+    WHERE vocabulary_id = 'ICD9CM'
+      AND DATE_DIFF(CAST(valid_end_date AS DATE), CURRENT_DATE(), DAY) > 0
+    UNION ALL
+    SELECT concept_id FROM `${staticTablesDataset}.prep_concept`
+    WHERE vocabulary_id = 'ICD9CM'
+      AND DATE_DIFF(CAST(valid_end_date AS DATE), CURRENT_DATE(), DAY) > 0
+)
