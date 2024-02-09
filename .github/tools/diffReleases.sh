ed46112220795fa2c6caa105eb46054f97f982e6
Mariko Medlock <marikomedlock@google.com>
2024-02-09 11:09:25 -0500

    Include diff for the underlay and indexer packages.

--- .github/tools/diffReleases.sh
+++ .github/tools/diffReleases.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 
-firstRelease=$1
-lastRelease=$2
+first_release=$1
+last_release=$2
 
-if [ -z $firstRelease ] || [ -z $lastRelease ];
+if [ -z $first_release ] || [ -z $last_release ];
 then
   echo "First and last release tags are required (e.g. 0.0.370).";
   exit 1;
 fi
 
-git log $1..$2 --grep='^bump' --invert-grep --pretty=format:"%h %as %an%x09%s" > diffReleases.txt
+diff_output=$(git diff $first_release..$last_release -- underlay/ indexer/)
+if [ -z $diff_output ];
+then
+  echo "REINDEXING NOT NEEDED" > diffReleases.txt
+else
+  echo "REINDEXING RECOMMENDED" > diffReleases.txt
+fi
+
+echo >> diffReleases.txt
+git log $1..$2 --grep='^bump' --invert-grep --pretty=format:"%h %as %an%x09%s" >> diffReleases.txt
