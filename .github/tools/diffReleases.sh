5f432b51510de3b4a5749861df96e1434ee0c468
Mariko Medlock <marikomedlock@google.com>
2024-02-09 11:43:16 -0500

    bash test.

--- .github/tools/diffReleases.sh
+++ .github/tools/diffReleases.sh
@@ -2,6 +2,7 @@
 
 first_release=$1
 last_release=$2
+output_file="diffReleases.txt"
 
 if [ -z $first_release ] || [ -z $last_release ];
 then
@@ -12,11 +13,17 @@ fi
 diff_output=$(git diff $first_release..$last_release -- underlay/ indexer/)
 if [ -z $diff_output ];
 then
-  echo "REINDEXING NOT NEEDED" > diffReleases.txt
+  echo "REINDEXING NOT NEEDED" > $output_file
 else
-  echo "REINDEXING RECOMMENDED" > diffReleases.txt
+  echo "REINDEXING RECOMMENDED" > $output_file
 fi
-echo "diff output done"
+echo >> $output_file
 
-echo >> diffReleases.txt
-git log $first_release..$last_release --grep='^bump' --invert-grep --pretty=format:"%h %as %an%x09%s" >> diffReleases.txt
+echo "diff_output done"
+echo "$diff_output"
+
+log_output=$(git log $first_release..$last_release --grep='^bump' --invert-grep --pretty=format:"%h %as %an%x09%s")
+echo $log_output >> $output_file
+
+echo "log_output done"
+echo "$log_output"
