b681001585e0b560e238e3db65ebc27632981dc6
Mariko Medlock <marikomedlock@google.com>
2024-02-14 10:09:35 -0500

    Set Java package name in protos. Remove placeholder schema.

--- underlay/src/test/java/bio/terra/tanagra/underlay/ProtoSchemaTest.java
+++ underlay/src/test/java/bio/terra/tanagra/underlay/ProtoSchemaTest.java
@@ -1,33 +1,65 @@
 package bio.terra.tanagra.underlay;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import bio.terra.tanagra.proto.criteriaselector.Simple;
+import bio.terra.tanagra.proto.criteriaselector.KeyOuterClass.Key;
+import bio.terra.tanagra.proto.criteriaselector.ValueDataOuterClass.ValueData;
+import bio.terra.tanagra.proto.criteriaselector.ValueOuterClass;
+import bio.terra.tanagra.proto.criteriaselector.ValueOuterClass.Value;
+import com.google.api.client.json.Json;
 import com.google.protobuf.InvalidProtocolBufferException;
 import com.google.protobuf.util.JsonFormat;
 import org.junit.jupiter.api.Test;
 
 public class ProtoSchemaTest {
   @Test
-  void deserializeConfig() throws InvalidProtocolBufferException {
-    String serialized = "{ \"displayName\":\"test name\", \"description\":\"test description\" }";
-    Simple.Config.Builder builder = Simple.Config.newBuilder();
+  void deserializeSimpleSchema() throws InvalidProtocolBufferException {
+    String serialized = "{ \"int64Key\":14 }";
+    Key.Builder builder = Key.newBuilder();
     JsonFormat.parser().ignoringUnknownFields().merge(serialized, builder);
-    Simple.Config config = builder.build();
-    assertNotNull(config);
-    assertEquals("test name", config.getDisplayName());
-    assertEquals("test description", config.getDescription());
+    Key key = builder.build();
+    assertNotNull(key);
+    assertFalse(key.hasStringKey());
+    assertEquals(14, key.getInt64Key());
   }
 
   @Test
-  void deserializeData() throws InvalidProtocolBufferException {
-    String serialized = "{ \"selectedId\":14, \"selectedDisplay\":\"test display\" }";
-    Simple.Data.Builder builder = Simple.Data.newBuilder();
+  void serializeSimpleSchema() throws InvalidProtocolBufferException {
+    Key key = Key.newBuilder().setInt64Key(45).build();
+    String serialized = JsonFormat.printer().omittingInsignificantWhitespace().print(key);
+    assertEquals("{\"int64Key\":\"45\"}", serialized);
+  }
+
+  @Test
+  void deserializeNestedSchema() throws InvalidProtocolBufferException {
+    String serialized = "{ \"attribute\": \"id\", \"numeric\": true,  \"range\": { \"id\": \"15\", \"min\": 0, \"max\": 89 } }";
+    ValueData.Builder builder = ValueData.newBuilder();
     JsonFormat.parser().ignoringUnknownFields().merge(serialized, builder);
-    Simple.Data data = builder.build();
-    assertNotNull(data);
-    assertEquals(14, data.getSelectedId());
-    assertEquals("test display", data.getSelectedDisplay());
+    ValueData valueData = builder.build();
+    assertNotNull(valueData);
+    assertEquals("id", valueData.getAttribute());
+    assertTrue(valueData.getNumeric());
+    assertEquals(0, valueData.getSelectedCount());
+    assertNotNull(valueData.getRange());
+    assertEquals("15", valueData.getRange().getId());
+    assertEquals(0, valueData.getRange().getMin());
+    assertEquals(89, valueData.getRange().getMax());
+  }
+
+  @Test
+  void serializeNestedSchema() throws InvalidProtocolBufferException {
+    ValueData valueData = ValueData.newBuilder()
+            .setAttribute("vocabulary")
+            .setNumeric(false)
+            .addSelected(
+              ValueData.Selection.newBuilder()
+                      .setValue(
+                              Value.newBuilder().setInt64Value(24).build())
+                      .setName("SNOMED").build()).build();
+    String serialized = JsonFormat.printer().omittingInsignificantWhitespace().print(valueData);
+    assertEquals("{\"attribute\":\"vocabulary\",\"selected\":[{\"value\":{\"int64Value\":\"24\"},\"name\":\"SNOMED\"}]}", serialized);
   }
 }
