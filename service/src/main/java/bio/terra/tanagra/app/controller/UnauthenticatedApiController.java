745abf6eeadb3a002706ad4d412f0556d287304e
Mariko Medlock <marikomedlock@google.com>
2024-03-08 13:21:50 -0500

    Remove endpoint for passing feature flags to UI.

--- service/src/main/java/bio/terra/tanagra/app/controller/UnauthenticatedApiController.java
+++ service/src/main/java/bio/terra/tanagra/app/controller/UnauthenticatedApiController.java
@@ -1,9 +1,7 @@
 package bio.terra.tanagra.app.controller;
 
-import bio.terra.tanagra.app.configuration.FeatureConfiguration;
 import bio.terra.tanagra.app.configuration.VersionConfiguration;
 import bio.terra.tanagra.generated.controller.UnauthenticatedApi;
-import bio.terra.tanagra.generated.model.ApiSystemFeatures;
 import bio.terra.tanagra.generated.model.ApiSystemVersion;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,21 +12,15 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class UnauthenticatedApiController implements UnauthenticatedApi {
   private final ApiSystemVersion currentVersion;
-  private final ApiSystemFeatures currentFeatures;
 
   @Autowired
-  public UnauthenticatedApiController(
-      VersionConfiguration versionConfiguration, FeatureConfiguration featureConfiguration) {
+  public UnauthenticatedApiController(VersionConfiguration versionConfiguration) {
     this.currentVersion =
         new ApiSystemVersion()
             .gitTag(versionConfiguration.getGitTag())
             .gitHash(versionConfiguration.getGitHash())
             .github(versionConfiguration.getGithubUrl())
             .build(versionConfiguration.getBuild());
-    this.currentFeatures =
-        new ApiSystemFeatures()
-            .activityLogEnabled(featureConfiguration.isActivityLogEnabled())
-            .backendFiltersEnabled(featureConfiguration.isBackendFiltersEnabled());
   }
 
   @Override
@@ -39,10 +31,5 @@ public class UnauthenticatedApiController implements UnauthenticatedApi {
   @Override
   public ResponseEntity<ApiSystemVersion> serviceVersion() {
     return new ResponseEntity<>(currentVersion, HttpStatus.OK);
-  }
-
-  @Override
-  public ResponseEntity<ApiSystemFeatures> serviceFeatures() {
-    return new ResponseEntity<>(currentFeatures, HttpStatus.OK);
   }
 }
