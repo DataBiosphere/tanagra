f5664690484a9f6ace3401a22987a359b1616dc5
marikomedlock <marikomedlock@users.noreply.github.com>
2024-03-08 12:28:02 -0500

    Add service config property to control multi-threading. (#759)
    * Add service config property to control multi-threading.
    
    * Add docs about how to disable multi-threading.
    
    * Update docs to include questions that came up in review.

--- service/src/main/java/bio/terra/tanagra/app/configuration/FeatureConfiguration.java
+++ service/src/main/java/bio/terra/tanagra/app/configuration/FeatureConfiguration.java
@@ -36,6 +36,22 @@ public class FeatureConfiguration {
       defaultValue = "false")
   private boolean activityLogEnabled;
 
+  @AnnotatedField(
+      name = "tanagra.feature.maxChildThreads",
+      markdown =
+          "The maximum number of child threads a single request can spawn. "
+              + "The application will only use multi-threading where it could improve performance, so just configuring "
+              + "a specific number here is not a guarantee that exactly that many or even any child threads will be "
+              + "spawned for a given request.\n\n "
+              + "When unset, the application will default to using multi-threading where it could improve performance. "
+              + "When set to 0, the application will only run things serially. "
+              + "When set to some N > 0 (e.g. 2), the application may spawn at most N child threads.\n\n "
+              + "(For export, spawning a single child thread would not improve performance, so 0 and 1 cause "
+              + "identical behavior, i.e. run serially in same thread as request.)",
+      environmentVariable = "TANAGRA_FEATURE_MAX_CHILD_THREADS",
+      optional = true)
+  private String maxChildThreads;
+
   public boolean isArtifactStorageEnabled() {
     return artifactStorageEnabled;
   }
@@ -44,12 +60,30 @@ public class FeatureConfiguration {
     return activityLogEnabled;
   }
 
+  public Integer getMaxChildThreads() {
+    try {
+      return Integer.parseInt(maxChildThreads);
+    } catch (NumberFormatException nfEx) {
+      // Don't throw an exception here, which would prevent the service from starting up.
+      LOGGER.warn("Invalid max child threads: {}", maxChildThreads);
+      return null;
+    }
+  }
+
+  public boolean hasMaxChildThreads() {
+    return getMaxChildThreads() != null;
+  }
+
   public void setArtifactStorageEnabled(boolean artifactStorageEnabled) {
     this.artifactStorageEnabled = artifactStorageEnabled;
   }
 
   public void setActivityLogEnabled(boolean activityLogEnabled) {
     this.activityLogEnabled = activityLogEnabled;
+  }
+
+  public void setMaxChildThreads(String maxChildThreads) {
+    this.maxChildThreads = maxChildThreads;
   }
 
   public void artifactStorageEnabledCheck() {
@@ -67,5 +101,6 @@ public class FeatureConfiguration {
   public void log() {
     LOGGER.info("Feature: artifact-storage-enabled: {}", isArtifactStorageEnabled());
     LOGGER.info("Feature: activity-log-enabled: {}", isActivityLogEnabled());
+    LOGGER.info("Feature: max-child-threads: {}", getMaxChildThreads());
   }
 }
