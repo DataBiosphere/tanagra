d99a5107721dd5b7f84b18fa8ca0c004179a0167
marikomedlock <marikomedlock@users.noreply.github.com>
2024-03-27 09:45:52 -0400

    Remove unused CriteriaGroup properties. (#788)
    * Remove unused CriteriaGroup properties.
    
    * Skip reading entity property from API criteria group.

--- service/src/test/java/bio/terra/tanagra/service/criteriaconstants/cmssynpuf/CriteriaGroup.java
+++ service/src/test/java/bio/terra/tanagra/service/criteriaconstants/cmssynpuf/CriteriaGroup.java
@@ -15,30 +15,25 @@ public final class CriteriaGroup {
       CohortRevision.CriteriaGroup.builder()
           .displayName("group gender")
           .criteria(List.of(GENDER_EQ_WOMAN.getValue()))
-          .entity(GENDER_EQ_WOMAN.getKey())
           .build();
   public static final CohortRevision.CriteriaGroup CRITERIA_GROUP_AGE =
       CohortRevision.CriteriaGroup.builder()
           .displayName("group gender")
           .criteria(List.of(GENDER_EQ_WOMAN.getValue()))
-          .entity(GENDER_EQ_WOMAN.getKey())
           .build();
   public static final CohortRevision.CriteriaGroup CRITERIA_GROUP_DEMOGRAPHICS =
       CohortRevision.CriteriaGroup.builder()
           .displayName("group 1")
           .criteria(List.of(GENDER_EQ_WOMAN.getValue(), ETHNICITY_EQ_HISPANIC_OR_LATINO.getValue()))
-          .entity(GENDER_EQ_WOMAN.getKey())
           .build();
   public static final CohortRevision.CriteriaGroup CRITERIA_GROUP_CONDITION =
       CohortRevision.CriteriaGroup.builder()
           .displayName("group condition")
           .criteria(List.of(CONDITION_EQ_TYPE_2_DIABETES.getValue()))
-          .entity(CONDITION_EQ_TYPE_2_DIABETES.getKey())
           .build();
   public static final CohortRevision.CriteriaGroup CRITERIA_GROUP_PROCEDURE =
       CohortRevision.CriteriaGroup.builder()
           .displayName("group procedure")
           .criteria(List.of(PROCEDURE_EQ_AMPUTATION.getValue()))
-          .entity(PROCEDURE_EQ_AMPUTATION.getKey())
           .build();
 }
