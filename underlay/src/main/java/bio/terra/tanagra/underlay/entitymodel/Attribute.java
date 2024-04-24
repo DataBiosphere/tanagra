21572a326762d2da9ec1af4ec0f235662e5edde6
Mariko Medlock <marikomedlock@google.com>
2024-04-24 14:08:28 -0400

    Add isSuppressedForExport flag to attribute.

--- underlay/src/main/java/bio/terra/tanagra/underlay/entitymodel/Attribute.java
+++ underlay/src/main/java/bio/terra/tanagra/underlay/entitymodel/Attribute.java
@@ -11,6 +11,7 @@ public final class Attribute {
   private final String runtimeSqlFunctionWrapper;
   private final DataType runtimeDataType;
   private final boolean isComputeDisplayHint;
+  private final boolean isSuppressedForExport;
   private final SourceQuery sourceQuery;
 
   @SuppressWarnings("checkstyle:ParameterNumber")
@@ -22,6 +23,7 @@ public final class Attribute {
       String runtimeSqlFunctionWrapper,
       DataType runtimeDataType,
       boolean isComputeDisplayHint,
+      boolean isSuppressedForExport,
       SourceQuery sourceQuery) {
     this.name = name;
     this.dataType = dataType;
@@ -30,6 +32,7 @@ public final class Attribute {
     this.runtimeSqlFunctionWrapper = runtimeSqlFunctionWrapper;
     this.runtimeDataType = runtimeDataType;
     this.isComputeDisplayHint = isComputeDisplayHint && !isId;
+    this.isSuppressedForExport = isSuppressedForExport;
     this.sourceQuery = sourceQuery;
   }
 
@@ -69,6 +72,10 @@ public final class Attribute {
     return isComputeDisplayHint;
   }
 
+  public boolean isSuppressedForExport() {
+    return isSuppressedForExport;
+  }
+
   public SourceQuery getSourceQuery() {
     return sourceQuery;
   }
@@ -85,6 +92,7 @@ public final class Attribute {
     return isValueDisplay == attribute.isValueDisplay
         && isId == attribute.isId
         && isComputeDisplayHint == attribute.isComputeDisplayHint
+        && isSuppressedForExport == attribute.isSuppressedForExport
         && name.equals(attribute.name)
         && dataType == attribute.dataType
         && Objects.equals(runtimeSqlFunctionWrapper, attribute.runtimeSqlFunctionWrapper)
@@ -102,12 +110,11 @@ public final class Attribute {
         runtimeSqlFunctionWrapper,
         runtimeDataType,
         isComputeDisplayHint,
+        isSuppressedForExport,
         sourceQuery);
   }
 
   public static class SourceQuery {
-    private final boolean isSuppressed;
-
     private final String valueFieldName;
 
     private final String displayFieldTable;
@@ -117,20 +124,14 @@ public final class Attribute {
     private final String displayFieldTableJoinFieldName;
 
     public SourceQuery(
-        boolean isSuppressed,
         String valueFieldName,
         String displayFieldTable,
         String displayFieldName,
         String displayFieldTableJoinFieldName) {
-      this.isSuppressed = isSuppressed;
       this.valueFieldName = valueFieldName;
       this.displayFieldTable = displayFieldTable;
       this.displayFieldName = displayFieldName;
       this.displayFieldTableJoinFieldName = displayFieldTableJoinFieldName;
-    }
-
-    public boolean isSuppressed() {
-      return isSuppressed;
     }
 
     public String getValueFieldName() {
@@ -166,8 +167,7 @@ public final class Attribute {
         return false;
       }
       SourceQuery that = (SourceQuery) o;
-      return isSuppressed == that.isSuppressed
-          && valueFieldName.equals(that.valueFieldName)
+      return valueFieldName.equals(that.valueFieldName)
           && Objects.equals(displayFieldTable, that.displayFieldTable)
           && Objects.equals(displayFieldName, that.displayFieldName)
           && Objects.equals(displayFieldTableJoinFieldName, that.displayFieldTableJoinFieldName);
@@ -176,11 +176,7 @@ public final class Attribute {
     @Override
     public int hashCode() {
       return Objects.hash(
-          isSuppressed,
-          valueFieldName,
-          displayFieldTable,
-          displayFieldName,
-          displayFieldTableJoinFieldName);
+          valueFieldName, displayFieldTable, displayFieldName, displayFieldTableJoinFieldName);
     }
   }
 }
