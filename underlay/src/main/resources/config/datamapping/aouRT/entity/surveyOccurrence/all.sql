aef4b9825d6c38e094c538aa6ff1a8ac2ee568e9
marikomedlock <marikomedlock@users.noreply.github.com>
2024-05-07 12:11:08 -0400

    First pass at Basics Survey config files. (#850)
    * First draft of surveyBasics entity, group and selector.
    
    * Filter on survey name.
    
    * New surveyOccurrence entity.
    
    * Missed a reverted line in obsOcc.
    
    * Add surveyOccurrence entity to top-level underlay file.

---
+++ underlay/src/main/resources/config/datamapping/aouRT/entity/surveyOccurrence/all.sql
@@ -0,0 +1,27 @@
+SELECT
+  o.observation_id,
+  o.person_id,
+  o.observation_datetime,
+  ps.id AS survey_item_id,
+
+  ps_survey.concept_id AS survey_concept_id,
+  sc.concept_name AS survey_concept_name,
+  o.observation_source_concept_id AS question_concept_id,
+  qc.concept_name AS question_concept_name,
+  o.value_source_concept_id AS answer_concept_id,
+  ac.concept_name AS answer_concept_name
+
+FROM `${omopDataset}.observation` AS o
+JOIN `${omopDataset}.prep_survey` AS ps
+    ON ps.concept_id = o.observation_source_concept_id
+    AND CAST(ps.value AS INT64) = o.value_source_concept_id
+
+JOIN `${omopDataset}.prep_survey` AS ps_survey
+    ON ps_survey.survey = ps.survey
+    AND ps_survey.subtype = 'SURVEY'
+LEFT JOIN `${omopDataset}.concept` AS sc
+    ON sc.concept_id = ps_survey.concept_id
+LEFT JOIN `${omopDataset}.concept` AS qc
+    ON qc.concept_id = o.observation_source_concept_id
+LEFT JOIN `${omopDataset}.concept` AS ac
+    ON ac.concept_id = o.value_source_concept_id
---
+++ underlay/src/main/resources/config/datamapping/aouRT/entitygroup/surveyBasicsPerson/primaryCriteria.sql
@@ -0,0 +1,10 @@
+SELECT
+  o.person_id,
+  ps.id AS survey_item_id
+
+FROM `${omopDataset}.observation` AS o
+JOIN `${omopDataset}.prep_survey` AS ps
+    ON ps.concept_id = o.observation_source_concept_id
+    AND CAST(ps.value AS INT64) = o.value_source_concept_id
+
+WHERE ps.survey = 'Basics'
