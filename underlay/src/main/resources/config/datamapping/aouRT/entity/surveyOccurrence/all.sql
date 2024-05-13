687c0824c7981b9c7a0d3cc678c39c455d22bfc8
marikomedlock <marikomedlock@users.noreply.github.com>
2024-05-13 11:43:35 -0400

    Add numeric value answer to AoU Basics survey. (#852)
    * Add observation.value_as_number attribute and enable instance level hints.
    
    * Filter surveyOccurrence for Basics only.

--- underlay/src/main/resources/config/datamapping/aouRT/entity/surveyOccurrence/all.sql
+++ underlay/src/main/resources/config/datamapping/aouRT/entity/surveyOccurrence/all.sql
@@ -2,6 +2,7 @@ SELECT
   o.observation_id,
   o.person_id,
   o.observation_datetime,
+  o.value_as_number,
   ps.id AS survey_item_id,
 
   ps_survey.concept_id AS survey_concept_id,
@@ -12,9 +13,15 @@ SELECT
   ac.concept_name AS answer_concept_name
 
 FROM `${omopDataset}.observation` AS o
+
 JOIN `${omopDataset}.prep_survey` AS ps
     ON ps.concept_id = o.observation_source_concept_id
-    AND CAST(ps.value AS INT64) = o.value_source_concept_id
+    AND (
+      (CAST(ps.value AS INT64) = o.value_source_concept_id)
+      OR
+      (ps.value IS NULL AND o.value_source_concept_id IS NULL)
+    )
+    AND ps.subtype = 'ANSWER'
 
 JOIN `${omopDataset}.prep_survey` AS ps_survey
     ON ps_survey.survey = ps.survey
@@ -25,3 +32,5 @@ LEFT JOIN `${omopDataset}.concept` AS qc
     ON qc.concept_id = o.observation_source_concept_id
 LEFT JOIN `${omopDataset}.concept` AS ac
     ON ac.concept_id = o.value_source_concept_id
+
+WHERE ps.survey = 'Basics'
