0895fcef396e8af4d1dd2518b8e01fabfde18192
dexamundsen <dexamundsen@verily.health>
2024-05-24 22:41:14 -0700

    Remove v2 prefix from version endpoint, add tests

---
+++ service/src/test/java/bio/terra/tanagra/app/controller/UnauthenticatedApiControllerTest.java
@@ -0,0 +1,42 @@
+package bio.terra.tanagra.app.controller;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import bio.terra.tanagra.generated.model.ApiSystemVersion;
+import bio.terra.tanagra.testing.BaseSpringUnitTest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+
+@Tag("regression-test")
+@AutoConfigureMockMvc
+public class UnauthenticatedApiControllerTest extends BaseSpringUnitTest {
+  @Autowired private MockMvc mvc;
+
+  @Test
+  public void serviceStatusTest() {
+    assertDoesNotThrow(
+        () -> this.mvc.perform(get("/status")).andExpect(status().isOk()).andReturn());
+  }
+
+  @Test
+  public void serviceVersionTest() throws Exception {
+    MockHttpServletResponse response =
+        this.mvc.perform(get("/version")).andExpect(status().isOk()).andReturn().getResponse();
+
+    ApiSystemVersion version =
+        new ObjectMapper().readValue(response.getContentAsString(), ApiSystemVersion.class);
+    assertNotNull(version);
+    assertNotNull(version.getGitTag());
+    assertNotNull(version.getGitHash());
+    assertNotNull(version.getGithub());
+    assertNotNull(version.getBuild());
+  }
+}
