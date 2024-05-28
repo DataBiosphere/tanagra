900ca01c7409ae348162d049dbce9d60b2db6201
dexamundsen <dexamundsen@verily.health>
2024-05-28 13:14:41 -0700

    Checkstyle fix

--- service/src/test/java/bio/terra/tanagra/app/controller/UnauthenticatedApiControllerTest.java
+++ service/src/test/java/bio/terra/tanagra/app/controller/UnauthenticatedApiControllerTest.java
@@ -8,7 +8,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import bio.terra.tanagra.generated.model.ApiSystemVersion;
 import bio.terra.tanagra.testing.BaseSpringUnitTest;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
