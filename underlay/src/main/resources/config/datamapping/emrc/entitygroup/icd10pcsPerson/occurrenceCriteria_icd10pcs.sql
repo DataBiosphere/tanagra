35c947c879bdbc4fdbd0bf72d65d8b5a5b708bd5
Chenchal Subraveti <c.subraveti@vumc.org>
2024-06-28 11:13:39 -0500

    updated sqls cpt4, icd9, icd10

--- underlay/src/main/resources/config/datamapping/emrc/entitygroup/icd10pcsPerson/occurrenceCriteria_icd10pcs.sql
+++ underlay/src/main/resources/config/datamapping/emrc/entitygroup/icd10pcsPerson/occurrenceCriteria_icd10pcs.sql
@@ -4,10 +4,12 @@ FROM `${omopDataset}.icd10_codes` AS c
 JOIN (
     SELECT
         pc.criteria_meta_seq as concept_id,
-        regexp_extract(pc.name, '(.*)-.*') as concept_code
+        regexp_extract(pc.name, '(.*)-.*') as concept_code,
+        pc.is_leaf
     FROM `${omopDataset}.icd10_criteria` pc
     WHERE pc.parent_seq >= (
         select criteria_meta_seq from `${omopDataset}.icd10_criteria`
         where starts_with(label, 'ICD10PCS')
         )
     ) cc ON c.code = cc.concept_code
+    and cc.is_leaf = true
