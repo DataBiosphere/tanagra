7ddc6800aeef7022f3321f294531a4a3f494af89
Chenchal Subraveti <c.subraveti@vumc.org>
2024-07-10 18:57:40 -0500

    updated for vitals

--- underlay/src/main/resources/config/datamapping/emerge/entity/height/all.sql
+++ underlay/src/main/resources/config/datamapping/emerge/entity/height/all.sql
@@ -2,8 +2,5 @@ SELECT
     ROW_NUMBER() OVER() AS height_id,
     bo.IND_SEQ as person_id,
     bo.height AS value_as_number,
-    1500 as value_as_concept_id,
-    'Height' as value_as_concept_name,
     bo.age_at_event AS age_at_occurrence
 FROM `${omopDataset}.bmi` bo
-
--- underlay/src/main/resources/config/datamapping/emerge/entity/weight/all.sql
+++ underlay/src/main/resources/config/datamapping/emerge/entity/weight/all.sql
@@ -2,8 +2,5 @@ SELECT
     ROW_NUMBER() OVER() AS weight_id,
     bo.IND_SEQ as person_id,
     bo.weight AS value_as_number,
-    1100 as value_as_concept_id,
-    'Weight' as value_as_concept_name,
     bo.age_at_event AS age_at_occurrence
 FROM `${omopDataset}.bmi` bo
-
