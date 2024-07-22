0ff238ed629e3bb5059f7ec4ace4317ac85ada8a
Will Dolbeer <william.dolbeer@vumc.org>
2024-07-22 14:49:08 -0400

    enable temporal for icd9

--- underlay/src/main/resources/config/datamapping/aouRT/entity/icd9cmConcept/all.sql
+++ underlay/src/main/resources/config/datamapping/aouRT/entity/icd9cmConcept/all.sql
@@ -1,25 +1,31 @@
 SELECT
-    concept_id,
-    concept_name,
-    vocabulary_id,
-    (CASE WHEN standard_concept IS NULL THEN 'Source' WHEN standard_concept = 'S' THEN 'Standard' ELSE 'Unknown' END) AS standard_concept,
-    concept_code,
-    CASE WHEN concept_code IS NULL THEN concept_name ELSE CONCAT(concept_code, ' ', concept_name) END AS label
-FROM `${omopDataset}.concept`
+    cc.concept_id,
+    cc.concept_name,
+    cc.vocabulary_id,
+    (CASE WHEN cc.standard_concept IS NULL THEN 'Source' WHEN cc.standard_concept = 'S' THEN 'Standard' ELSE 'Unknown' END) AS standard_concept,
+    cc.concept_code,
+    CASE WHEN cc.concept_code IS NULL THEN cc.concept_name ELSE CONCAT(cc.concept_code, ' ', cc.concept_name) END AS label,
+    co.condition_start_datetime,
+    co.visit_occurrence_id,
+FROM `${omopDataset}.concept` as cc
+JOIN `${omopDataset}.condition_occurrence` AS co ON co.condition_concept_id = cc.concept_id
 WHERE
-  vocabulary_id = 'ICD9CM'
-  AND DATE_DIFF(CAST(valid_end_date AS DATE), CURRENT_DATE(), DAY) > 0
+  cc.vocabulary_id = 'ICD9CM'
+  AND DATE_DIFF(CAST(cc.valid_end_date AS DATE), CURRENT_DATE(), DAY) > 0
 
 UNION ALL
 
 SELECT
-    concept_id,
-    concept_name,
-    vocabulary_id,
-    (CASE WHEN standard_concept IS NULL THEN 'Source' WHEN standard_concept = 'S' THEN 'Standard' ELSE 'Unknown' END) AS standard_concept,
-    concept_code,
-    CASE WHEN concept_code IS NULL THEN concept_name ELSE CONCAT(concept_code, ' ', concept_name) END AS label
-FROM `${staticTablesDataset}.prep_concept`
+    pc.concept_id,
+    pc.concept_name,
+    pc.vocabulary_id,
+    (CASE WHEN pc.standard_concept IS NULL THEN 'Source' WHEN pc.standard_concept = 'S' THEN 'Standard' ELSE 'Unknown' END) AS standard_concept,
+    pc.concept_code,
+    CASE WHEN pc.concept_code IS NULL THEN pc.concept_name ELSE CONCAT(pc.concept_code, ' ', pc.concept_name) END AS label,
+    co.condition_start_datetime,
+    co.visit_occurrence_id,
+FROM `${staticTablesDataset}.prep_concept` as pc
+JOIN `${omopDataset}.condition_occurrence` AS co ON co.condition_concept_id = pc.concept_id
 WHERE
-  vocabulary_id = 'ICD9CM'
-  AND DATE_DIFF(CAST(valid_end_date AS DATE), CURRENT_DATE(), DAY) > 0
+  pc.vocabulary_id = 'ICD9CM'
+  AND DATE_DIFF(CAST(pc.valid_end_date AS DATE), CURRENT_DATE(), DAY) > 0
