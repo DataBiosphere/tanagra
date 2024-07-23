c56d478bc8525aca35268bc221b6e4deb880aa7a
Mariko Medlock <marikomedlock@google.com>
2024-07-23 13:35:37 -0400

    Add test.

--- service/src/test/java/bio/terra/tanagra/service/AnnotationServiceTest.java
+++ service/src/test/java/bio/terra/tanagra/service/AnnotationServiceTest.java
@@ -301,6 +301,17 @@ public class AnnotationServiceTest {
         () ->
             annotationService.createAnnotationKey(
                 study1.getId(), cohort1.getId(), AnnotationKey.builder().dataType(DataType.INT64)));
+
+    // Display name length exceeds maximum.
+    assertThrows(
+        BadRequestException.class,
+        () ->
+            annotationService.createAnnotationKey(
+                study1.getId(),
+                cohort1.getId(),
+                AnnotationKey.builder()
+                    .dataType(DataType.INT64)
+                    .displayName("123456789012345678901234567890123456")));
   }
 
   @Test
