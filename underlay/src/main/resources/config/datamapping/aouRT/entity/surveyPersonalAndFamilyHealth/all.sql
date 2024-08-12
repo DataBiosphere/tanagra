30bcf8a0b851e02f050c8e947648d5be0e2d61be
Will Dolbeer <40036095+dolbeew@users.noreply.github.com>
2024-08-12 11:36:17 -0400

    Add AoU underlay config for all non-versioned surveys (#956)
    * add config for lifestyle survey
    
    * add separate occurrence for lifestyle survey
    
    * fix root node ids
    
    * fix typo
    
    * add config for overall health survey
    
    * add config for pfhh  healthcare access surveys
    
    * add config for sdoh survey
    
    * add survey configs to CT

---
+++ underlay/src/main/resources/config/datamapping/aouRT/entity/surveyPersonalAndFamilyHealth/all.sql
@@ -0,0 +1,3 @@
+SELECT id, name, code, subtype, concept_id, CAST(value AS INT64) AS value
+FROM `${omopDataset}.prep_survey`
+WHERE survey = 'PersonalAndFamilyHealth'
---
+++ underlay/src/main/resources/config/datamapping/aouRT/entitygroup/surveyOverallHealthPerson/primaryCriteria.sql
@@ -0,0 +1,15 @@
+SELECT
+  o.person_id,
+  ps.id AS survey_item_id
+
+FROM `${omopDataset}.observation` AS o
+JOIN `${omopDataset}.prep_survey` AS ps
+    ON ps.concept_id = o.observation_source_concept_id
+    AND (
+      (CAST(ps.value AS INT64) = o.value_source_concept_id)
+      OR
+      (ps.value IS NULL AND o.value_source_concept_id IS NULL)
+    )
+    AND ps.subtype = 'ANSWER'
+
+WHERE ps.survey = 'OverallHealth'
