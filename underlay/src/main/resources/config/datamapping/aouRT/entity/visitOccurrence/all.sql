12c33eee5dab3948aa7d0e555b42e32f9dfff571
freemabd <brian.d.freeman@gmail.com>
2024-09-09 09:02:07 -0500

    DT-601 adding more source query configuration

--- underlay/src/main/resources/config/datamapping/aouRT/entity/visitOccurrence/all.sql
+++ underlay/src/main/resources/config/datamapping/aouRT/entity/visitOccurrence/all.sql
@@ -2,16 +2,18 @@ SELECT
   vo.visit_occurrence_id,
   vo.person_id,
   vo.visit_concept_id,
-  vc.concept_name AS visit_concept_name,
-  vo.visit_start_date,
-  vo.visit_end_date,
+  vc.concept_name AS standard_concept_name,
+  vo.visit_start_datetime,
+  vo.visit_end_datetime,
   vo.visit_source_value,
   vo.visit_source_concept_id,
+  vsc.concept_name AS source_concept_name,
   CAST(FLOOR(TIMESTAMP_DIFF(vo.visit_start_datetime, p.birth_datetime, DAY) / 365.25) AS INT64) AS age_at_occurrence
 FROM `${omopDataset}.visit_occurrence` AS vo
 JOIN `${omopDataset}.person` AS p ON p.person_id = vo.person_id
 JOIN `${omopDataset}.concept` AS vc ON vc.concept_id = vo.visit_concept_id
     AND vc.domain_id = 'Visit'
     AND vc.standard_concept = 'S'
+JOIN `${omopDataset}.concept` AS vsc ON vsc.concept_id = vo.visit_source_concept_id
 WHERE vo.visit_concept_id IS NOT null
   AND vo.visit_concept_id != 0
