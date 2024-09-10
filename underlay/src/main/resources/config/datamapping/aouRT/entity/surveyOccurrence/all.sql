1ab306aa692f0840efd2c1cc82b225e55129445c
Will Dolbeer <william.dolbeer@vumc.org>
2024-09-10 10:40:51 -0400

    update surveyOccurrence entity sql for pfhh

--- underlay/src/main/resources/config/datamapping/aouRT/entity/surveyOccurrence/all.sql
+++ underlay/src/main/resources/config/datamapping/aouRT/entity/surveyOccurrence/all.sql
@@ -42,4 +42,47 @@ LEFT JOIN `${omopDataset}.survey_conduct` AS sv
 LEFT JOIN `${omopDataset}.concept` AS svc
     ON svc.concept_id = sv.survey_concept_id
 
-WHERE ps.survey IN ('Basics', 'Lifestyle', 'OverallHealth', 'PersonalAndFamilyHealth', 'HealthCareAccessUtiliza', 'SocialDeterminantsOfHea')
+WHERE ps.survey IN ('Basics', 'Lifestyle', 'OverallHealth', 'HealthCareAccessUtiliza', 'SocialDeterminantsOfHea')
+
+UNION ALL
+SELECT
+    ROW_NUMBER() OVER() AS observation_id,
+    po.person_id,
+    po.observation_datetime as survey_datetime,
+    po.value_as_number,
+    ps.id AS survey_item_id,
+
+    ps_survey.concept_id AS survey_concept_id,
+    sc.concept_name AS survey_concept_name,
+    po.observation_source_concept_id AS question_concept_id,
+    qc.name AS question_concept_name,
+    po.value_source_concept_id AS answer_concept_id,
+    ac.name AS answer_concept_name,
+    NULL as survey_version_concept_id,
+    CAST(NULL as string) as survey_version_name
+
+FROM `${omopDataset}.prep_pfhh_observation` AS po
+
+JOIN `${omopDataset}.prep_survey` AS ps
+    ON ps.concept_id = po.observation_source_concept_id
+    AND (
+     (CAST(ps.value AS INT64) = po.value_source_concept_id)
+         OR
+     (ps.value IS NULL AND po.value_source_concept_id IS NULL)
+     )
+    AND ps.subtype = 'ANSWER'
+
+JOIN `${omopDataset}.prep_survey` AS ps_survey
+    ON ps_survey.survey = ps.survey
+    AND ps_survey.subtype = 'SURVEY'
+LEFT JOIN `${omopDataset}.concept` AS sc
+    ON sc.concept_id = ps_survey.concept_id
+LEFT JOIN `${omopDataset}.prep_survey` AS qc
+    ON qc.concept_id = po.observation_source_concept_id
+    AND qc.subtype = 'QUESTION'
+LEFT JOIN `${omopDataset}.prep_survey` AS ac
+    ON ac.concept_id = po.observation_source_concept_id
+    AND CAST(ac.value AS INT64) = po.value_source_concept_id
+    AND ac.subtype = 'ANSWER'
+
+WHERE ps.survey = 'PersonalAndFamilyHealth'
