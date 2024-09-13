3b9ed7ae9d12d0dba3ad1d0f5655cfd168e2f420
marikomedlock <marikomedlock@users.noreply.github.com>
2024-09-13 09:17:29 -0400

    Variant search config. (#995)
    * Attribute isRepeated flag. ValidateDataTypes indexing job.
    
    * CreateEntityMainn indexing job.
    
    * WriteEntityLevelDisplayHints indexing job.
    
    * WriteTextSearchField indexing job.
    
    * Select repeated attr field. Result parsing test list query.
    
    * Fetch string-only hints. Result parsing test hint query.
    
    * Group by repeated attribute field. Sql building and results parsing tests count query.
    
    * Filter on repeated attribute field. Sql building test list query.
    
    * Only use FLATTENED alias for repeated attribute field.
    
    * OpenAPI repeated type in ValueDisplay. Bug in returning hint query results.
    
    * Remove variant config files.
    
    * Regenerate docs.
    
    * Check actual data rows in result parsing hint & count query tests.
    
    * Fix overlapping conditional in eldh indexing job.
    
    * Variant search config -- first pass.
    
    * Use t_item_count in criteria selector.
    
    * Put back rest of underlay.
    
    * Add multi-select for variant entity.
    
    * Revert whitespace changes.
    
    * Always populate repeatedValue property of ValueDisplay in OpenAPI.
    
    * Move variant config to testonly version of aouSC underlay.
    
    * Move variant config files into separate directories.
    
    ---------
    
    Co-authored-by: Dexter Amundsen <dexamundsen@verily.com>

---
+++ underlay/src/main/resources/config/datamapping/aouCT_testonly/entity/variant/all.sql
@@ -0,0 +1,12 @@
+SELECT ROW_NUMBER() OVER (ORDER BY vid) AS row_num,
+       vid,
+       gene_symbol,
+       dbsnp_rsid,
+       genomic_location,
+       consequence,
+       aa_change,
+       clinvar_classification,
+       gvs_all_ac,
+       gvs_all_an,
+       gvs_all_af
+FROM `${omopDataset}.prep_vat`
