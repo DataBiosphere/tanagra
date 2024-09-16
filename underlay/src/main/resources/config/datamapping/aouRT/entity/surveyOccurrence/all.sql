88e2ba36d856b20bc37031fbafa2d5289239f9a7
Will Dolbeer <william.dolbeer@vumc.org>
2024-09-16 15:37:09 -0400

    add entity and group for covid vaccine survey

--- underlay/src/main/resources/config/datamapping/aouRT/entity/surveyOccurrence/all.sql
+++ underlay/src/main/resources/config/datamapping/aouRT/entity/surveyOccurrence/all.sql
@@ -42,7 +42,14 @@ LEFT JOIN `${omopDataset}.survey_conduct` AS sv
 LEFT JOIN `${omopDataset}.concept` AS svc
     ON svc.concept_id = sv.survey_concept_id
 
-WHERE ps.survey IN ('Basics', 'Lifestyle', 'OverallHealth', 'HealthCareAccessUtiliza', 'SocialDeterminantsOfHea')
+WHERE ps.survey IN (
+    'Basics',
+    'Lifestyle',
+    'OverallHealth',
+    'HealthCareAccessUtiliza',
+    'SocialDeterminantsOfHea',
+    'NewYearMinuteSurveyOnCO'
+)
 
 UNION ALL
 SELECT
