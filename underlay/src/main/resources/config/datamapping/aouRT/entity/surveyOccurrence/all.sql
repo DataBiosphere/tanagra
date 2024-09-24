acee5c588bfcc9c585f16545edd22e4be3296cd1
Will Dolbeer <william.dolbeer@vumc.org>
2024-09-24 15:50:42 -0400

    add entity for cope survey

--- underlay/src/main/resources/config/datamapping/aouRT/entity/surveyOccurrence/all.sql
+++ underlay/src/main/resources/config/datamapping/aouRT/entity/surveyOccurrence/all.sql
@@ -31,20 +31,27 @@ JOIN `${omopDataset}.prep_survey` AS ps_survey
 LEFT JOIN `${omopDataset}.concept` AS sc
     ON sc.concept_id = ps_survey.concept_id
 LEFT JOIN `${omopDataset}.prep_survey` AS qc
-    ON qc.concept_id = o.observation_source_concept_id 
+    ON qc.concept_id = o.observation_source_concept_id
     AND qc.subtype = 'QUESTION'
 LEFT JOIN `${omopDataset}.prep_survey` AS ac
-    ON ac.concept_id = o.observation_source_concept_id 
-    AND CAST(ac.value AS INT64) = o.value_source_concept_id 
+    ON ac.concept_id = o.observation_source_concept_id
+    AND CAST(ac.value AS INT64) = o.value_source_concept_id
     AND ac.subtype = 'ANSWER'
 LEFT JOIN `${omopDataset}.survey_conduct` AS sv
     ON sv.survey_conduct_id = o.questionnaire_response_id
 LEFT JOIN `${omopDataset}.concept` AS svc
     ON svc.concept_id = sv.survey_concept_id
 
-WHERE ps.survey NOT IN ('PersonalAndFamilyHealth')
+WHERE ps.survey IN (
+    'Basics',
+    'Lifestyle',
+    'OverallHealth',
+    'HealthCareAccessUtiliza',
+    'SocialDeterminantsOfHea'
+)
 
 UNION ALL
+-- PFHH Survey
 SELECT
     ROW_NUMBER() OVER() AS observation_id,
     po.person_id,
@@ -86,3 +93,95 @@ LEFT JOIN `${omopDataset}.prep_survey` AS ac
     AND ac.subtype = 'ANSWER'
 
 WHERE ps.survey = 'PersonalAndFamilyHealth'
+
+UNION ALL
+-- Cope Surveys
+SELECT distinct
+    o.observation_id,
+    o.person_id,
+    o.observation_datetime as survey_datetime,
+    o.value_as_number,
+    ps.id AS survey_item_id,
+    ps_survey.concept_id AS survey_concept_id,
+    sc.concept_name AS survey_concept_name,
+    o.observation_source_concept_id AS question_concept_id,
+    qc.name AS question_concept_name,
+    o.value_source_concept_id AS answer_concept_id,
+    ac.name AS answer_concept_name,
+    sv.survey_concept_id as survey_version_concept_id,
+    svc.concept_name as survey_version_name
+FROM `all-of-us-ehr-dev.SR2023Q3R2.observation` AS o
+         JOIN `all-of-us-ehr-dev.SR2023Q3R2.prep_survey` AS ps
+              ON ps.concept_id = o.observation_source_concept_id
+                  AND (
+                     (CAST(ps.value AS INT64) = o.value_source_concept_id)
+                         OR
+                     (ps.value IS NULL AND o.value_source_concept_id IS NULL)
+                     )
+                  AND ps.subtype = 'ANSWER'
+                  AND survey IN ('February2021COVID19Part', 'July2020Covid19Particip', 'May2020Covid19Participa')
+         JOIN `all-of-us-ehr-dev.SR2023Q3R2.prep_survey` AS ps_survey
+              ON ps_survey.survey = ps.survey
+                  AND ps_survey.subtype = 'SURVEY'
+         LEFT JOIN `all-of-us-ehr-dev.SR2023Q3R2.concept` AS sc
+                   ON sc.concept_id = ps_survey.concept_id
+         LEFT JOIN `all-of-us-ehr-dev.SR2023Q3R2.prep_survey` AS qc
+                   ON qc.concept_id = o.observation_source_concept_id
+                       AND qc.subtype = 'QUESTION'
+         LEFT JOIN `all-of-us-ehr-dev.SR2023Q3R2.prep_survey` AS ac
+                   ON ac.concept_id = o.observation_source_concept_id
+                       AND CAST(ac.value AS INT64) = o.value_source_concept_id
+                       AND ac.subtype = 'ANSWER'
+         LEFT JOIN `all-of-us-ehr-dev.SR2023Q3R2.survey_conduct` AS sv
+                   ON sv.survey_conduct_id = o.questionnaire_response_id
+         LEFT JOIN `all-of-us-ehr-dev.SR2023Q3R2.concept` AS svc
+                   ON svc.concept_id = sv.survey_concept_id
+WHERE sv.survey_concept_id IN (
+    2100000002, 2100000003, 2100000004, 2100000005, 2100000006, 2100000007
+)
+
+UNION ALL
+-- Cope Minute Vaccine Surveys
+SELECT distinct
+    o.observation_id,
+    o.person_id,
+    o.observation_datetime as survey_datetime,
+    o.value_as_number,
+    ps.id AS survey_item_id,
+    ps_survey.concept_id AS survey_concept_id,
+    sc.concept_name AS survey_concept_name,
+    o.observation_source_concept_id AS question_concept_id,
+    qc.name AS question_concept_name,
+    o.value_source_concept_id AS answer_concept_id,
+    ac.name AS answer_concept_name,
+    sv.survey_concept_id as survey_version_concept_id,
+    svc.concept_name as survey_version_name
+FROM `all-of-us-ehr-dev.SR2023Q3R2.observation` AS o
+         JOIN `all-of-us-ehr-dev.SR2023Q3R2.prep_survey` AS ps
+              ON ps.concept_id = o.observation_source_concept_id
+                  AND (
+                     (CAST(ps.value AS INT64) = o.value_source_concept_id)
+                         OR
+                     (ps.value IS NULL AND o.value_source_concept_id IS NULL)
+                     )
+                  AND ps.subtype = 'ANSWER'
+                  AND survey IN ('NewYearMinuteSurveyOnCO')
+         JOIN `all-of-us-ehr-dev.SR2023Q3R2.prep_survey` AS ps_survey
+              ON ps_survey.survey = ps.survey
+                  AND ps_survey.subtype = 'SURVEY'
+         LEFT JOIN `all-of-us-ehr-dev.SR2023Q3R2.concept` AS sc
+                   ON sc.concept_id = ps_survey.concept_id
+         LEFT JOIN `all-of-us-ehr-dev.SR2023Q3R2.prep_survey` AS qc
+                   ON qc.concept_id = o.observation_source_concept_id
+                       AND qc.subtype = 'QUESTION'
+         LEFT JOIN `all-of-us-ehr-dev.SR2023Q3R2.prep_survey` AS ac
+                   ON ac.concept_id = o.observation_source_concept_id
+                       AND CAST(ac.value AS INT64) = o.value_source_concept_id
+                       AND ac.subtype = 'ANSWER'
+         LEFT JOIN `all-of-us-ehr-dev.SR2023Q3R2.survey_conduct` AS sv
+                   ON sv.survey_conduct_id = o.questionnaire_response_id
+         LEFT JOIN `all-of-us-ehr-dev.SR2023Q3R2.concept` AS svc
+                   ON svc.concept_id = sv.survey_concept_id
+WHERE sv.survey_concept_id IN (
+    905047, 905055, 765936, 1741006
+)
