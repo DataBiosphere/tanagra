22c9da69e4f299fdedb6df9c35273c406e191da7
Brian Freeman <brian.freeman@vumc.org>
2024-10-02 10:42:45 -0500

    DT-888 adding config for fitbit source export queries (#1023)
    * DT-888 adding config fix for entities that do not have a primary key in the source table.
    
    * DT-888 update comment
    
    * DT-888 fixing config for fitbit source queries

--- underlay/src/main/resources/config/datamapping/aouRT/entity/heartRateSummary/all.sql
+++ underlay/src/main/resources/config/datamapping/aouRT/entity/heartRateSummary/all.sql
@@ -1,10 +1,10 @@
 SELECT
-    ROW_NUMBER() OVER() AS row_id,
+    row_id,
+    person_id,
     date,
     zone_name,
     min_heart_rate,
     max_heart_rate,
     minute_in_zone,
-    calorie_count,
-    person_id
+    calorie_count
 FROM `${omopDataset}.heart_rate_summary`
