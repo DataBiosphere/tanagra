ec77b49af098b0b1f946fbb5a24435419c2740c6
Chenchal Subraveti <chenchals@users.noreply.github.com>
2024-10-28 08:57:37 -0500

    Updated for GIRA labs (#1055)

--- underlay/src/main/resources/config/datamapping/emerge/entitygroup/labPerson/primaryCriteria.sql
+++ underlay/src/main/resources/config/datamapping/emerge/entitygroup/labPerson/primaryCriteria.sql
@@ -1,5 +1,10 @@
 SELECT l.ind_seq as person_id,
-       cast(parse_numeric(lc.criteria_meta_seq) AS INT64) AS lab_id
+       lc.id AS lab_id
 FROM `${omopDataset}.labs` AS l
-JOIN `${omopDataset}.lab_criteria` lc
-    ON l.lab_name = lc.name
+JOIN (SELECT
+        ROW_NUMBER() OVER(ORDER BY loinc_code, lab_name) as id,
+        lab_name,
+        loinc_code
+      FROM `${omopDataset}.labs`
+      GROUP BY 2,3
+     ) lc  on l.lab_name=lc.lab_name
