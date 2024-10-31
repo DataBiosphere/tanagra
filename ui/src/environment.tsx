ad0e9345e4749843a092a263807ed9e0f517dca1
Yu Hu <93544261+yuhuyoyo@users.noreply.github.com>
2024-10-31 12:54:11 -0400

    Update environment.tsx

--- ui/src/environment.tsx
+++ ui/src/environment.tsx
@@ -13,7 +13,7 @@ type Environment = {
 
   REACT_APP_AUTH0_DOMAIN?: string;
   REACT_APP_AUTH0_CLIENT_ID?: string;
-  REACT_APP_AUTH0_AUDIENCE?: string
+  REACT_APP_AUTH0_AUDIENCE?: string;
 };
 
 declare global {
