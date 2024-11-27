de7109a10eb9d412176268648d4115998b3229a7
Timothy Jennison <tjennison@google.com>
2024-11-27 16:49:50 +0000

    Use omopDataset variable throughout servey SQL

--- underlay/src/main/resources/config/datamapping/aouRT/entity/surveyOccurrence/all.sql
+++ underlay/src/main/resources/config/datamapping/aouRT/entity/surveyOccurrence/all.sql
@@ -110,8 +110,8 @@ SELECT distinct
     ac.name AS answer_concept_name,
     sv.survey_concept_id as survey_version_concept_id,
     svc.concept_name as survey_version_name
-FROM `all-of-us-ehr-dev.SR2023Q3R2.observation` AS o
-         JOIN `all-of-us-ehr-dev.SR2023Q3R2.prep_survey` AS ps
+FROM `${omopDataset}.observation` AS o
+         JOIN `${omopDataset}.prep_survey` AS ps
               ON ps.concept_id = o.observation_source_concept_id
                   AND (
                      (CAST(ps.value AS INT64) = o.value_source_concept_id)
@@ -120,21 +120,21 @@ FROM `all-of-us-ehr-dev.SR2023Q3R2.observation` AS o
                      )
                   AND ps.subtype = 'ANSWER'
                   AND survey IN ('February2021COVID19Part', 'July2020Covid19Particip', 'May2020Covid19Participa')
-         JOIN `all-of-us-ehr-dev.SR2023Q3R2.prep_survey` AS ps_survey
+         JOIN `${omopDataset}.prep_survey` AS ps_survey
               ON ps_survey.survey = ps.survey
                   AND ps_survey.subtype = 'SURVEY'
-         LEFT JOIN `all-of-us-ehr-dev.SR2023Q3R2.concept` AS sc
+         LEFT JOIN `${omopDataset}.concept` AS sc
                    ON sc.concept_id = ps_survey.concept_id
-         LEFT JOIN `all-of-us-ehr-dev.SR2023Q3R2.prep_survey` AS qc
+         LEFT JOIN `${omopDataset}.prep_survey` AS qc
                    ON qc.concept_id = o.observation_source_concept_id
                        AND qc.subtype = 'QUESTION'
-         LEFT JOIN `all-of-us-ehr-dev.SR2023Q3R2.prep_survey` AS ac
+         LEFT JOIN `${omopDataset}.prep_survey` AS ac
                    ON ac.concept_id = o.observation_source_concept_id
                        AND CAST(ac.value AS INT64) = o.value_source_concept_id
                        AND ac.subtype = 'ANSWER'
-         LEFT JOIN `all-of-us-ehr-dev.SR2023Q3R2.survey_conduct` AS sv
+         LEFT JOIN `${omopDataset}.survey_conduct` AS sv
                    ON sv.survey_conduct_id = o.questionnaire_response_id
-         LEFT JOIN `all-of-us-ehr-dev.SR2023Q3R2.concept` AS svc
+         LEFT JOIN `${omopDataset}.concept` AS svc
                    ON svc.concept_id = sv.survey_concept_id
 WHERE sv.survey_concept_id IN (
     2100000002, 2100000003, 2100000004, 2100000005, 2100000006, 2100000007
@@ -156,8 +156,8 @@ SELECT distinct
     ac.name AS answer_concept_name,
     sv.survey_concept_id as survey_version_concept_id,
     svc.concept_name as survey_version_name
-FROM `all-of-us-ehr-dev.SR2023Q3R2.observation` AS o
-         JOIN `all-of-us-ehr-dev.SR2023Q3R2.prep_survey` AS ps
+FROM `${omopDataset}.observation` AS o
+         JOIN `${omopDataset}.prep_survey` AS ps
               ON ps.concept_id = o.observation_source_concept_id
                   AND (
                      (CAST(ps.value AS INT64) = o.value_source_concept_id)
@@ -166,21 +166,21 @@ FROM `all-of-us-ehr-dev.SR2023Q3R2.observation` AS o
                      )
                   AND ps.subtype = 'ANSWER'
                   AND survey IN ('NewYearMinuteSurveyOnCO')
-         JOIN `all-of-us-ehr-dev.SR2023Q3R2.prep_survey` AS ps_survey
+         JOIN `${omopDataset}.prep_survey` AS ps_survey
               ON ps_survey.survey = ps.survey
                   AND ps_survey.subtype = 'SURVEY'
-         LEFT JOIN `all-of-us-ehr-dev.SR2023Q3R2.concept` AS sc
+         LEFT JOIN `${omopDataset}.concept` AS sc
                    ON sc.concept_id = ps_survey.concept_id
-         LEFT JOIN `all-of-us-ehr-dev.SR2023Q3R2.prep_survey` AS qc
+         LEFT JOIN `${omopDataset}.prep_survey` AS qc
                    ON qc.concept_id = o.observation_source_concept_id
                        AND qc.subtype = 'QUESTION'
-         LEFT JOIN `all-of-us-ehr-dev.SR2023Q3R2.prep_survey` AS ac
+         LEFT JOIN `${omopDataset}.prep_survey` AS ac
                    ON ac.concept_id = o.observation_source_concept_id
                        AND CAST(ac.value AS INT64) = o.value_source_concept_id
                        AND ac.subtype = 'ANSWER'
-         LEFT JOIN `all-of-us-ehr-dev.SR2023Q3R2.survey_conduct` AS sv
+         LEFT JOIN `${omopDataset}.survey_conduct` AS sv
                    ON sv.survey_conduct_id = o.questionnaire_response_id
-         LEFT JOIN `all-of-us-ehr-dev.SR2023Q3R2.concept` AS svc
+         LEFT JOIN `${omopDataset}.concept` AS svc
                    ON svc.concept_id = sv.survey_concept_id
 WHERE sv.survey_concept_id IN (
     905047, 905055, 765936, 1741006
