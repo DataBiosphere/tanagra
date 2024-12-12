7eae4cfabe60f7b3f1ad14d47cc8820cec9b7347
dexamundsen <dexamundsen@verily.health>
2024-12-12 11:42:05 -0800

    move testonly entities to aouRT folder; use in underlay

--- underlay/src/main/resources/config/datamapping/aouCT_testonly/entity/sleepLevel/all.sql
+++
@@ -1,9 +0,0 @@
--- test data does not have row_id, compute it
-SELECT
-    ROW_NUMBER() OVER() AS row_id,
-    person_id,
-    sleep_date,
-    is_main_sleep,
-    level,
-    duration_in_min
-FROM `${omopDataset}.sleep_level`
