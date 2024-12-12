ea7a819ae1dd4d8852964f07516e8de2010475c2
dexamundsen <dexamundsen@verily.health>
2024-12-12 11:41:14 -0800

    move testonly entities to aouRT folder; use in underlay

---
+++ underlay/src/main/resources/config/datamapping/aouRT_testonly/entity/sleepDailySummary/all.sql
@@ -0,0 +1,16 @@
+-- test data does not have row_id, compute it
+SELECT
+    ROW_NUMBER() OVER() AS row_id,
+    person_id,
+    sleep_date,
+    is_main_sleep,
+    minute_in_bed,
+    minute_asleep,
+    minute_after_wakeup,
+    minute_awake,
+    minute_restless,
+    minute_deep,
+    minute_light,
+    minute_rem,
+    minute_wake
+FROM `${omopDataset}.sleep_daily_summary`
---
+++ underlay/src/main/resources/config/datamapping/aouRT_testonly/entity/sleepLevel/all.sql
@@ -0,0 +1,9 @@
+-- test data does not have row_id, compute it
+SELECT
+    ROW_NUMBER() OVER() AS row_id,
+    person_id,
+    sleep_date,
+    is_main_sleep,
+    level,
+    duration_in_min
+FROM `${omopDataset}.sleep_level`
