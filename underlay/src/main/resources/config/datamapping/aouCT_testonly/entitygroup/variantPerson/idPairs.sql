2d7dd6c12e8c9cf9785d7bd180fac53724a25cc0
dexamundsen <dexamundsen@verily.health>
2024-12-11 23:21:45 -0800

    fix sql

--- underlay/src/main/resources/config/datamapping/aouCT_testonly/entitygroup/variantPerson/idPairs.sql
+++ underlay/src/main/resources/config/datamapping/aouCT_testonly/entitygroup/variantPerson/idPairs.sql
@@ -1,3 +1,3 @@
 SELECT DISTINCT vid AS variant_id, flattened_person_id
-FROM `${omopDataset}.variant_to_person`)
+FROM `${omopDataset}.variant_to_person`
 CROSS JOIN UNNEST(person_ids) AS flattened_person_id
