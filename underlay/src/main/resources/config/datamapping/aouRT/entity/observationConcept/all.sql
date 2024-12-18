84a982e7498b8e00e8c39e946456b7d4ae1856c1
freemabd <brian.d.freeman@gmail.com>
2024-12-18 14:41:04 -0600

    DT-681 removing concepts that do not have data

--- underlay/src/main/resources/config/datamapping/aouRT/entity/observationConcept/all.sql
+++ underlay/src/main/resources/config/datamapping/aouRT/entity/observationConcept/all.sql
@@ -1,11 +1,13 @@
 SELECT
-    concept_id,
-    concept_name,
-    vocabulary_id,
-    concept_code,
-    'Standard' AS standard_concept
-FROM `${omopDataset}.concept`
-WHERE domain_id = 'Observation'
-    AND standard_concept = 'S'
-    AND vocabulary_id != 'PPI'
-    AND concept_class_id != 'Survey'
+    DISTINCT c.concept_id,
+             c.concept_name,
+             c.vocabulary_id,
+             v.vocabulary_name,
+             c.concept_code,
+             'Standard' AS standard_concept
+FROM `${omopDataset}.observation` AS o
+JOIN `${omopDataset}.concept` AS c ON c.concept_id = o.observation_concept_id
+WHERE c.domain_id = 'Observation'
+  AND c.standard_concept = 'S'
+  AND c.vocabulary_id != 'PPI'
+  AND c.concept_class_id != 'Survey'
