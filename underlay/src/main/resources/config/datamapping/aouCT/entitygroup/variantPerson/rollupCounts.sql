798ad01e58cef44cfc45f6bb77ce7ec47ba0d1bb
freemabd <brian.d.freeman@gmail.com>
2024-12-18 08:52:16 -0600

    DT-323 adding config for variants

---
+++ underlay/src/main/resources/config/datamapping/aouCT/entitygroup/variantPerson/rollupCounts.sql
@@ -0,0 +1,3 @@
+SELECT vid AS variant_id, ARRAY_LENGTH(person_ids) AS num_persons
+/* Wrap variant_to_person table in a SELECT DISTINCT because there is a duplicate row in the test data. */
+FROM (SELECT DISTINCT vid, person_ids FROM `${omopDataset}.cb_variant_to_person`)
