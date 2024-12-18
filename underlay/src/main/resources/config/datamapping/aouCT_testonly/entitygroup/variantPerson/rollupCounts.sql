c4564380d6819e4db153b4a74d0e1499ea1986b4
dexamundsen <dexamundsen@verily.health>
2024-12-17 22:37:35 -0800

    Update sql

--- underlay/src/main/resources/config/datamapping/aouCT_testonly/entitygroup/variantPerson/rollupCounts.sql
+++ underlay/src/main/resources/config/datamapping/aouCT_testonly/entitygroup/variantPerson/rollupCounts.sql
@@ -1,3 +1,3 @@
 SELECT vid AS variant_id, ARRAY_LENGTH(person_ids) AS num_persons
 /* Wrap variant_to_person table in a SELECT DISTINCT because there is a duplicate row in the test data. */
-FROM (SELECT DISTINCT vid, person_ids FROM `${omopDataset}.variant_to_person` WHERE REGEXP_CONTAINS(id, r"{indexIdRegex}") )
+FROM (SELECT DISTINCT vid, person_ids FROM `${omopDataset}.variant_to_person` WHERE REGEXP_CONTAINS(vid, r"{indexIdRegex}") )
