8fd7f14046d712b5ef1a8993906c405331d47eed
freemabd <brian.d.freeman@gmail.com>
2025-01-21 10:20:46 -0600

    updating config for variant search

--- underlay/src/main/resources/config/datamapping/aouCT/entitygroup/variantPerson/idPairs.sql
+++ underlay/src/main/resources/config/datamapping/aouCT/entitygroup/variantPerson/idPairs.sql
@@ -1,3 +1,3 @@
-SELECT DISTINCT vid AS variant_id, flattened_person_id
+SELECT DISTINCT vid, flattened_person_id
 FROM `${omopDataset}.cb_variant_to_person`
 CROSS JOIN UNNEST(person_ids) AS flattened_person_id
