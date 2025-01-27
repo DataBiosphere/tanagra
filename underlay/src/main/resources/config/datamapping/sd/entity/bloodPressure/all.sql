6ddf437a4588127ddf0014a54163aecba8d6228c
Will Dolbeer <william.dolbeer@vumc.org>
2025-01-27 14:47:52 -0500

    update existing export entities

--- underlay/src/main/resources/config/datamapping/sd/entity/bloodPressure/all.sql
+++ underlay/src/main/resources/config/datamapping/sd/entity/bloodPressure/all.sql
@@ -1,5 +1,5 @@
 SELECT
-  ROW_NUMBER() OVER (ORDER BY xbp.person_id, xbp.measurement_datetime) AS id,
+  ROW_NUMBER() OVER (ORDER BY xbp.person_id, xbp.measurement_datetime) AS row_id,
   xbp.person_id,
   p.person_source_value,
   xbp.measurement_datetime,
