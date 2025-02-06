99f31255221374900c5ada410f9128101aa242cd
Dexter Amundsen <dexamundsen@verily.health>
2025-02-06 06:54:54 -0800

    fix sql

--- underlay/src/test/resources/sql/BQHintQueryTest/instanceLevelHint.sql
+++ underlay/src/test/resources/sql/BQHintQueryTest/instanceLevelHint.sql
@@ -4,4 +4,9 @@
     FROM
         ${ILDH_measurementOccurrence_measurementLoinc}      
     WHERE
-        entity_id = @relatedEntityId0
+        entity_id = @relatedEntityId0      
+    ORDER BY
+        attribute_name,
+        enum_value,
+        enum_display,
+        enum_count
