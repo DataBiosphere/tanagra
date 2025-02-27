77be27262930663d2dc3e1612aaf8a115585135c
Michael Spector <mspector@verily.com>
2025-02-27 16:31:03 +0000

    first attempt at adding survey data

--- underlay/src/main/resources/config/datamapping/bhs/entity/person/all.sql
+++ underlay/src/main/resources/config/datamapping/bhs/entity/person/all.sql
@@ -1,7 +1,26 @@
 SELECT
-    SUBJID,
-    CAST(age_at_enrollment as INT64) as age_at_enrollment,
-    SEX as sex,
-    RACE as race
+    d.SUBJID,
+    CAST(d.age_at_enrollment as INT64) as age_at_enrollment,
+    d.SEX as sex,
+    d.RACE as race,
+    phq.VISIT,
+    phq.questionnaire_version,
+    phq.phq9_1,
+    phq.phq9_2,
+    phq.phq9_3,
+    phq.phq9_4
 
 FROM `${omopDataset}.DM` d
+
+LEFT JOIN (
+    SELECT
+        p.*
+    FROM
+        `${omopDataset}.PHQ9A` p
+    JOIN
+        (SELECT SUBJID, MAX(VISITNUM) AS MaxVisit FROM `${omopDataset}.PHQ9A` GROUP BY SUBJID) AS max_visits
+    ON
+        p.SUBJID = max_visits.SUBJID AND p.VISITNUM = max_visits.MaxVisit
+) phq
+ON phq.SUBJID = d.SUBJID
+
