9f5f3f16994bbfc4025192acb0fcf471d43a1752
Peter Su <psu@verily.com>
2025-02-27 23:51:37 +0000

    updated person entity to use diagnosis_at_baseline as a criteria selector

--- underlay/src/main/resources/config/datamapping/amp_pd/entity/person/all.sql
+++ underlay/src/main/resources/config/datamapping/amp_pd/entity/person/all.sql
@@ -1,8 +1,12 @@
 SELECT
-    participant_id,
-    CAST(age_at_baseline as INT64) as age,
-    sex,
-    race,
-    ethnicity
-
+    d.participant_id,
+    CAST(d.age_at_baseline as INT64) as age,
+    d.sex,
+    d.race,
+    d.ethnicity,
+    c.diagnosis_at_baseline,
+    c.diagnosis_latest,
+    c.case_control_other_at_baseline,
+    c.case_control_other_latest
 FROM `${omopDataset}.Demographics` d
+LEFT JOIN `${omopDataset}.amp_pd_case_control` c on c.participant_id=d.participant_id
