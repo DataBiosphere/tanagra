21812fe5683aeab1ad5397025523bd68595765ba
Chase Burden <122058662+C-Burden@users.noreply.github.com>
2025-03-11 12:00:25 -0500

    Updating emerge to index icd-10 from concept table (#1187)

--- underlay/src/main/resources/config/datamapping/emerge/entity/icd10cm/rootNode.sql
+++ underlay/src/main/resources/config/datamapping/emerge/entity/icd10cm/rootNode.sql
@@ -1,7 +1,7 @@
-SELECT pc.criteria_meta_seq as id
-FROM `${omopDataset}.icd10_criteria` pc
-WHERE pc.parent_seq = (
-    select criteria_meta_seq from `${omopDataset}.icd10_criteria`
-    where starts_with(label, 'ICD10CM')
-    )
-
+SELECT pcr.concept_id_2 as id
+FROM `${staticTablesDataset}.prep_concept_relationship` pcr
+WHERE pcr.concept_id_1 = (SELECT concept_id
+                          FROM `${staticTablesDataset}.prep_concept` c
+                          WHERE c.concept_class_id = 'ICD10CM Hierarchy'
+                            AND c.concept_code IS NULL
+)
