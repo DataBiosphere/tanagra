32ec0037925b32a9ed6eab5fbcf28078775d2c33
Will Dolbeer <william.dolbeer@vumc.org>
2025-03-12 14:11:01 -0400

    fix month in date formatter

--- ui/src/util/date.ts
+++ ui/src/util/date.ts
@@ -1,6 +1,6 @@
 // Formats a date (without time) similar to RFC 3339 but in the local time zone.
 export function standardDateString(date: Date): string {
-  return `${date.getFullYear()}-${String(date.getMonth()).padStart(
+  return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(
     2,
     "0"
   )}-${String(date.getDate()).padStart(2, "0")}`;
