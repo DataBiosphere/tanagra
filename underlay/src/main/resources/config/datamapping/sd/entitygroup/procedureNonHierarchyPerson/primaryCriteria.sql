bacf7747f9ff84687e83696a8113b1a6a6d5c77f
Chenchal Subraveti <c.subraveti@vumc.org>
2025-04-25 14:56:15 -0500

    add non-hierarchy for condition. procedure

---
+++ underlay/src/main/resources/config/datamapping/sd/entitygroup/procedureNonHierarchyPerson/primaryCriteria.sql
@@ -0,0 +1,10 @@
+SELECT
+  po.person_id,
+  po.procedure_concept_id
+FROM `${omopDataset}.procedure_occurrence` AS po
+WHERE po.procedure_concept_id
+  IN (SELECT concept_id
+      FROM `${omopDataset}.concept` c
+      WHERE c.domain_id = 'Procedure'
+        AND c.vocabulary_id IN ('LOINC', 'HCPCS')
+        AND c.standard_concept = 'S')
