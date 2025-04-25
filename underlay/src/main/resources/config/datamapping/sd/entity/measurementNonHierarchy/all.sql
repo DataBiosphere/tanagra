a4eca726e4fe8690144193a3f70239bdbab4cfd9
Chenchal Subraveti <c.subraveti@vumc.org>
2025-04-25 15:38:40 -0500

    add non-hierarchy for measurement

---
+++ underlay/src/main/resources/config/datamapping/sd/entity/measurementNonHierarchy/all.sql
@@ -0,0 +1,13 @@
+SELECT
+    concept_id,
+    concept_name,
+    vocabulary_id,
+    concept_code,
+    'Standard' AS standard_concept
+FROM (SELECT DISTINCT measurement_concept_id FROM `${omopDataset}.measurement`) m
+         JOIN `${omopDataset}.concept` c ON m.measurement_concept_id = c.concept_id
+WHERE c.domain_id = 'Measurement'
+  AND (c.vocabulary_id = 'HCPCS'
+           OR (c.vocabulary_id = 'LOINC' AND c.concept_class_id = 'Clinical Observation')
+      )
+  AND c.standard_concept = 'S'
