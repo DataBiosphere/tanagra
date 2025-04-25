28fe35b65aa8a9546936a1d5a2f70e4fc1a664d5
Chenchal Subraveti <c.subraveti@vumc.org>
2025-04-25 15:38:40 -0500

    add non-hierarchy for measurement

---
+++ underlay/src/main/resources/config/datamapping/sd/entitygroup/measurementNonHierarchyPerson/primaryCriteria.sql
@@ -0,0 +1,12 @@
+SELECT
+  mo.person_id,
+  mo.measurement_concept_id
+FROM `${omopDataset}.measurement` AS mo
+WHERE mo.measurement_concept_id
+  IN (SELECT concept_id
+      FROM `${omopDataset}.concept` c
+      WHERE c.domain_id = 'Measurement'
+        AND (c.vocabulary_id = 'HCPCS'
+              OR (c.vocabulary_id = 'LOINC' AND c.concept_class_id = 'Clinical Observation')
+            )
+        AND c.standard_concept = 'S')
