40cca98e7e9731b67d21e0079eff5401b3418e88
Chenchal Subraveti <c.subraveti@vumc.org>
2025-04-25 14:28:46 -0500

    add non-hierarchy for condition.

--- underlay/src/main/resources/config/datamapping/sd/entity/condition/all.sql
+++ underlay/src/main/resources/config/datamapping/sd/entity/condition/all.sql
@@ -4,7 +4,5 @@ SELECT
     vocabulary_id,
     concept_code,
     (CASE WHEN standard_concept IS NULL THEN 'Source' WHEN standard_concept = 'S' THEN 'Standard' ELSE 'Unknown' END) AS standard_concept
-
 FROM `${omopDataset}.concept`
-
 WHERE domain_id = 'Condition'
