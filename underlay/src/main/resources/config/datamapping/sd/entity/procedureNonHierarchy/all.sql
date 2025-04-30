45670f5b309c1de85e0af5a4b41a6f0164d7d74d
Chenchal Subraveti <c.subraveti@vumc.org>
2025-04-30 09:28:35 -0500

    updated for DISTINCT

--- underlay/src/main/resources/config/datamapping/sd/entity/procedureNonHierarchy/all.sql
+++ underlay/src/main/resources/config/datamapping/sd/entity/procedureNonHierarchy/all.sql
@@ -1,17 +1,9 @@
 SELECT DISTINCT
-<<<<<<< HEAD
     c.concept_id,
     c.concept_name,
     c.vocabulary_id,
     'Standard' as standard_concept,
     c.concept_code
-=======
-    concept_id,
-    concept_name,
-    vocabulary_id,
-    'Standard' as standard_concept,
-    concept_code
->>>>>>> bacf7747 (add non-hierarchy for condition. procedure)
 FROM `${omopDataset}.procedure_occurrence` po
 JOIN `${omopDataset}.concept` c ON po.procedure_concept_id = c.concept_id
     AND c.domain_id = 'Procedure'
