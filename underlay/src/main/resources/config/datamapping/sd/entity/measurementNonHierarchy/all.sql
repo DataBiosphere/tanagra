63fab25f05787768098dfb4c1476871deda3432a
Chenchal Subraveti <c.subraveti@vumc.org>
2025-04-30 09:28:35 -0500

    updated for DISTINCT

--- underlay/src/main/resources/config/datamapping/sd/entity/measurementNonHierarchy/all.sql
+++ underlay/src/main/resources/config/datamapping/sd/entity/measurementNonHierarchy/all.sql
@@ -1,11 +1,11 @@
 SELECT
-    concept_id,
-    concept_name,
-    vocabulary_id,
-    concept_code,
+    c.concept_id,
+    c.concept_name,
+    c.vocabulary_id,
+    c.concept_code,
     'Standard' AS standard_concept
 FROM (SELECT DISTINCT measurement_concept_id FROM `${omopDataset}.measurement`) m
-         JOIN `${omopDataset}.concept` c ON m.measurement_concept_id = c.concept_id
+JOIN `${omopDataset}.concept` c ON m.measurement_concept_id = c.concept_id
 WHERE c.domain_id = 'Measurement'
   AND (c.vocabulary_id = 'HCPCS'
            OR (c.vocabulary_id = 'LOINC' AND c.concept_class_id = 'Clinical Observation')
