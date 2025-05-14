f3f82d59c58d54744c3f6778609e1946e77f9920
Chenchal Subraveti <c.subraveti@vumc.org>
2025-05-14 09:17:06 -0500

    rebase condition on-hierarchy

--- underlay/src/main/resources/config/datamapping/sd/entity/conditionNonHierarchy/all.sql
+++ underlay/src/main/resources/config/datamapping/sd/entity/conditionNonHierarchy/all.sql
@@ -6,5 +6,6 @@ SELECT
     'Standard' AS standard_concept
 FROM (SELECT DISTINCT condition_concept_id FROM `${omopDataset}.condition_occurrence`) co
 JOIN `${omopDataset}.concept` c on co.condition_concept_id = c.concept_id
+    AND c.domain_id = 'Condition'
     AND c.vocabulary_id != 'SNOMED'
     AND c.standard_concept = 'S'
