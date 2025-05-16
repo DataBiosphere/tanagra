95816a52fa3d9f6cdb3fdb656670ea8b7ddf11dc
Chenchal Subraveti <c.subraveti@vumc.org>
2025-05-16 15:25:37 -0500

    commented out non-heirarchy sql snippets

--- underlay/src/main/resources/config/datamapping/sd/entity/measurementLoinc/all.sql
+++ underlay/src/main/resources/config/datamapping/sd/entity/measurementLoinc/all.sql
@@ -9,35 +9,35 @@ WHERE
     vocabulary_id = 'LOINC'
     AND concept_class_id IN ('LOINC Hierarchy', 'LOINC Component', 'Lab Test')
 
-UNION ALL
--- non-hierarchy LOINC concepts
-SELECT -- 165
-       concept_id,
-       concept_name,
-       vocabulary_id,
-       concept_code,
-       'Standard' AS standard_concept
-FROM `${omopDataset}.concept` c
-WHERE c.domain_id = 'Measurement'
-  AND c.vocabulary_id = 'LOINC'
-  AND c.standard_concept = 'S'
-  AND c.concept_class_id = 'Clinical Observation'
-  AND c.concept_id IN (
-    SELECT DISTINCT measurement_concept_id FROM `${omopDataset}.measurement`
-)
-
-UNION ALL
--- non-hierarchy HCPCS concepts
-SELECT -- 23
-       concept_id,
-       concept_name,
-       vocabulary_id,
-       concept_code,
-       'Standard' AS standard_concept
-FROM `${omopDataset}.concept` c
-WHERE c.domain_id = 'Measurement'
-  AND c.vocabulary_id = 'HCPCS'
-  AND c.standard_concept = 'S'
-  AND c.concept_id IN (
-    SELECT DISTINCT measurement_concept_id FROM `${omopDataset}.measurement`
-)
+-- UNION ALL
+-- -- non-hierarchy LOINC concepts
+-- SELECT -- 165
+--        concept_id,
+--        concept_name,
+--        vocabulary_id,
+--        concept_code,
+--        'Standard' AS standard_concept
+-- FROM `${omopDataset}.concept` c
+-- WHERE c.domain_id = 'Measurement'
+--   AND c.vocabulary_id = 'LOINC'
+--   AND c.standard_concept = 'S'
+--   AND c.concept_class_id = 'Clinical Observation'
+--   AND c.concept_id IN (
+--     SELECT DISTINCT measurement_concept_id FROM `${omopDataset}.measurement`
+-- )
+--
+-- UNION ALL
+-- -- non-hierarchy HCPCS concepts
+-- SELECT -- 23
+--        concept_id,
+--        concept_name,
+--        vocabulary_id,
+--        concept_code,
+--        'Standard' AS standard_concept
+-- FROM `${omopDataset}.concept` c
+-- WHERE c.domain_id = 'Measurement'
+--   AND c.vocabulary_id = 'HCPCS'
+--   AND c.standard_concept = 'S'
+--   AND c.concept_id IN (
+--     SELECT DISTINCT measurement_concept_id FROM `${omopDataset}.measurement`
+-- )
