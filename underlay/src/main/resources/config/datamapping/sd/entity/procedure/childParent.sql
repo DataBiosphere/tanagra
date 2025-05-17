496772333c51cc3bea1e47a8d297f96a98dbb7e1
Chenchal Subraveti <c.subraveti@vumc.org>
2025-05-16 23:20:58 -0500

    updated childParent sql

--- underlay/src/main/resources/config/datamapping/sd/entity/procedure/childParent.sql
+++ underlay/src/main/resources/config/datamapping/sd/entity/procedure/childParent.sql
@@ -5,8 +5,8 @@ FROM `${omopDataset}.concept_relationship` cr
 JOIN `${omopDataset}.relationship` r ON cr.relationship_id = r.relationship_id
 JOIN `${omopDataset}.concept` chld  ON cr.concept_id_2 = chld.concept_id
 WHERE cr.relationship_id = 'Subsumes'
-  AND r.is_hierarchical = '1'
-  AND r.defines_ancestry = '1'
+  AND r.is_hierarchical = 1
+  AND r.defines_ancestry = 1
   AND chld.domain_id = 'Procedure'
   AND chld.vocabulary_id = 'SNOMED'
   AND chld.standard_concept = 'S'
