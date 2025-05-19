28727d888abb088e960204aa5905a59803c0df46
Chenchal Subraveti <c.subraveti@vumc.org>
2025-05-19 07:13:45 -0500

    Update child-parent for standard measurement entities

--- underlay/src/main/resources/config/datamapping/sd/entity/measurementLoinc/childParent.sql
+++ underlay/src/main/resources/config/datamapping/sd/entity/measurementLoinc/childParent.sql
@@ -1,11 +1,12 @@
 SELECT
-  cr.concept_id_1 AS parent,
-  cr.concept_id_2 AS child,
+    cr.concept_id_1 AS parent,
+    cr.concept_id_2 AS child,
 FROM `${omopDataset}.concept_relationship` cr
-JOIN `${omopDataset}.relationship` r ON cr.relationship_id = r.relationship_id
-JOIN `${omopDataset}.concept` chld  ON cr.concept_id_2 = chld.concept_id
-WHERE cr.relationship_id = 'Subsumes'
-  AND r.is_hierarchical = 1
-  AND r.defines_ancestry = 1
+         JOIN `${omopDataset}.concept` parnt  ON cr.concept_id_1 = parnt.concept_id
+         JOIN `${omopDataset}.concept` chld  ON cr.concept_id_2 = chld.concept_id
+WHERE
+    cr.relationship_id = 'Subsumes'
+  AND parnt.vocabulary_id = chld.vocabulary_id
+  AND parnt.vocabulary_id = 'LOINC'
+  AND parnt.concept_class_id IN ('LOINC Hierarchy', 'LOINC Component', 'Lab Test')
   AND chld.concept_class_id IN ('LOINC Hierarchy', 'LOINC Component', 'Lab Test')
-  AND chld.vocabulary_id = 'LOINC'
--- underlay/src/main/resources/config/datamapping/sd/entity/measurementSnomed/childParent.sql
+++ underlay/src/main/resources/config/datamapping/sd/entity/measurementSnomed/childParent.sql
@@ -2,10 +2,11 @@ SELECT
     cr.concept_id_1 AS parent,
     cr.concept_id_2 AS child,
 FROM `${omopDataset}.concept_relationship` cr
-         JOIN `${omopDataset}.relationship` r ON cr.relationship_id = r.relationship_id
-         JOIN `${omopDataset}.concept` chld  ON cr.concept_id_2 = chld.concept_id
+JOIN `${omopDataset}.concept` chld  ON cr.concept_id_2 = chld.concept_id
+JOIN `${omopDataset}.concept` parnt  ON cr.concept_id_1 = parnt.concept_id
 WHERE cr.relationship_id = 'Subsumes'
-  AND r.is_hierarchical = 1
-  AND r.defines_ancestry = 1
-  AND chld.concept_class_id IN ('LOINC Hierarchy', 'LOINC Component', 'Lab Test')
-  AND chld.vocabulary_id = 'SNOMED'
+  AND parnt.vocabulary_id = chld.vocabulary_id
+  AND parnt.vocabulary_id = 'SNOMED'
+  AND parnt.domain_id = chld.domain_id
+  AND parnt.domain_id = 'Measurement'
+  AND parnt.standard_concept = 'S'
