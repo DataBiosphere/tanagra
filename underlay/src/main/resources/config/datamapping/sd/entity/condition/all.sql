9e950e15fcf473bd56233e59e2b5bf200911ace3
Chenchal Subraveti <c.subraveti@vumc.org>
2025-05-19 06:37:14 -0500

    Removed non-hierarchies from entity all.sql scripts

--- underlay/src/main/resources/config/datamapping/sd/entity/condition/all.sql
+++ underlay/src/main/resources/config/datamapping/sd/entity/condition/all.sql
@@ -8,18 +8,3 @@ FROM `${omopDataset}.concept`
 WHERE domain_id = 'Condition'
   AND vocabulary_id = 'SNOMED'
   AND standard_concept = 'S'
-
--- UNION DISTINCT
--- -- non-hierarchy concepts
--- SELECT DISTINCT
---                 concept_id,
---                 concept_name,
---                 vocabulary_id,
---                 concept_code,
---                 'Standard' AS standard_concept
--- FROM `${omopDataset}.concept` c
--- WHERE c.concept_id in (
---     SELECT DISTINCT condition_concept_id from `${omopDataset}.condition_occurrence`
---     )
---     AND c.vocabulary_id != 'SNOMED'
---     AND c.standard_concept = 'S'
--- underlay/src/main/resources/config/datamapping/sd/entity/procedure/all.sql
+++ underlay/src/main/resources/config/datamapping/sd/entity/procedure/all.sql
@@ -8,19 +8,3 @@ FROM `${omopDataset}.concept`
 WHERE domain_id = 'Procedure'
   AND standard_concept = 'S'
   AND vocabulary_id = 'SNOMED'
-
--- UNION DISTINCT
--- -- 'HCPCS','LOINC' for non-hierarchy concepts
--- SELECT
---     concept_id,
---     concept_name,
---     vocabulary_id,
---     concept_code,
---     'Standard' AS standard_concept
--- FROM `${omopDataset}.concept` c
--- WHERE c.concept_id in (
---     SELECT DISTINCT procedure_concept_id from `${omopDataset}.procedure_occurrence`
---   )
---   AND domain_id = 'Procedure'
---   AND c.vocabulary_id IN ('HCPCS','LOINC')
---   AND c.standard_concept = 'S'
