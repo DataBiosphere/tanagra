c05a31d9cb9d9add3c468fd399a5e99c35a4fa56
C-Burden <chase.burden@vumc.org>
2025-07-29 09:45:05 -0500

    VDA-1752 Update CPT4 datamapping to include more codes.

--- underlay/src/main/resources/config/datamapping/sd/entitygroup/cpt4Person/occurrenceCriteria_ingredient.sql
+++ underlay/src/main/resources/config/datamapping/sd/entitygroup/cpt4Person/occurrenceCriteria_ingredient.sql
@@ -1,6 +1,6 @@
 SELECT io.drug_exposure_id, pc.id AS cpt4_id
 FROM `${omopDataset}.drug_exposure` AS io
-JOIN `${staticTablesDataset}.prep_cpt` AS pc
+RIGHT JOIN `${staticTablesDataset}.prep_cpt` AS pc
     ON pc.concept_id = io.drug_source_concept_id
 WHERE pc.type = 'CPT4'
   AND io.drug_source_concept_id IS NOT null
