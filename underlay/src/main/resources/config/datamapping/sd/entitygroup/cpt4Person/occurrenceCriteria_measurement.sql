d78e5c21771a483a864217185d191b32b65304a1
C-Burden <chase.burden@vumc.org>
2025-07-30 09:32:35 -0500

    Rolling back changes that are no longer needed

--- underlay/src/main/resources/config/datamapping/sd/entitygroup/cpt4Person/occurrenceCriteria_measurement.sql
+++ underlay/src/main/resources/config/datamapping/sd/entitygroup/cpt4Person/occurrenceCriteria_measurement.sql
@@ -1,6 +1,6 @@
 SELECT mo.measurement_id, pc.id AS cpt4_id
 FROM `${omopDataset}.measurement` AS mo
-RIGHT JOIN `${staticTablesDataset}.prep_cpt` AS pc
+JOIN `${staticTablesDataset}.prep_cpt` AS pc
     ON pc.concept_id = mo.measurement_source_concept_id
 WHERE pc.type = 'CPT4'
   AND mo.measurement_source_concept_id IS NOT null
