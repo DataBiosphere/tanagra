0e61856b928bbb7748b76214f6b01a48ede711a4
Chenchal Subraveti <c.subraveti@vumc.org>
2025-07-31 09:15:39 -0500

    fix person entity

--- underlay/src/main/resources/config/datamapping/sd/entity/person/all.sql
+++ underlay/src/main/resources/config/datamapping/sd/entity/person/all.sql
@@ -1,14 +1,14 @@
 SELECT
-    p.person_id, p.person_source_value, p.year_of_birth, p.birth_datetime,
+    p.person_id, p.person_source_value,
+    p.year_of_birth, p.month_of_birth, p.day_of_birth, p.birth_datetime,
     p.gender_concept_id, gc.concept_name AS gender_concept_name,
     p.race_source_concept_id, rc.concept_name AS race_concept_name,
     p.ethnicity_concept_id, ec.concept_name AS ethnicity_concept_name,
-
     /* Add BioVU sample columns. The way x_biovu_sample_status is created, there should be at
        most one row per person. */
     EXISTS
         (SELECT 1 FROM `${omopDataset}.x_biovu_sample_status` x WHERE p.person_id = x.person_id AND x.dna_yield_ind > 0)
-                                                                                                        AS has_biovu_sample,
+        AS has_biovu_sample,
     x.dna_yield_ind AS biovu_sample_dna_yield,
     /* As a courtesy, convert string fields to boolean: 0 -> No, 1 -> Yes */
     CASE WHEN x.compromise_ind = '1' THEN true WHEN x.compromise_ind = '0' THEN false ELSE null END AS biovu_sample_is_compromised,
@@ -16,24 +16,24 @@ SELECT
     CASE WHEN x.plasma_ind = '1' THEN true WHEN x.plasma_ind = '0' THEN false ELSE null END AS biovu_sample_has_plasma,
     EXISTS
         (SELECT 1 FROM `${omopDataset}.genotype_result` gr WHERE p.person_id = gr.person_id
-                  AND gr.assay_name = 'agd whole genome sequencing'  ) AS has_agd_genotype_result,
-    CASE WHEN d.death_date is null THEN false ELSE true END AS is_deceased
-
+                  AND gr.assay_name = 'agd whole genome sequencing'  )
+        AS has_agd_genotype_result,
+    CASE WHEN d.death_date is null THEN false ELSE true END AS is_deceased,
+    -- age computation
+    CASE WHEN d.death_date is null
+         THEN CAST(FLOOR(TIMESTAMP_DIFF(CURRENT_TIMESTAMP(), p.birth_datetime, DAY) / 365.25) AS INT64)
+         ELSE CAST(FLOOR(TIMESTAMP_DIFF(d.death_date, p.birth_datetime, DAY) / 365.25) AS INT64)
+    END AS age
 FROM `${omopDataset}.person` p
-
 LEFT JOIN `${omopDataset}.concept` gc
 ON gc.concept_id = p.gender_concept_id
-
 LEFT JOIN `${omopDataset}.concept` rc
 ON rc.concept_id = p.race_source_concept_id
-
 LEFT JOIN `${omopDataset}.concept` ec
 ON ec.concept_id = p.ethnicity_concept_id
-
 LEFT JOIN (SELECT person_id, max(death_date) as death_date
            FROM `${omopDataset}.death` GROUP BY person_id) d
           ON (p.person_id = d.person_id)
-
 LEFT OUTER JOIN
     (
         /* Get rid of duplicate rows in x_biovu_sample_status. For example, person
