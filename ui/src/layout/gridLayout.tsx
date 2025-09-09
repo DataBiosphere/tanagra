cd169283986c4fe40d5af990d2d50acb7cdc4350
Chase Burden <122058662+C-Burden@users.noreply.github.com>
2025-09-09 08:14:12 -0500

    VDA-2351 Preview Table Scrollbar (#1240)
    * VDA-2351 Preview Table Scrollbar
    
    * VDA-2351 Preview Table Scrollbar
    
    * VDA-2351 Test issues

--- ui/src/layout/gridLayout.tsx
+++ ui/src/layout/gridLayout.tsx
@@ -104,6 +104,7 @@ export default function GridLayout(props: PropsWithChildren<GridLayoutProps>) {
           gridTemplateColumns: colTmpl,
           alignItems: translateDirection(ra),
           justifyItems: translateDirection(ca),
+          overflowY: "auto",
         },
         ...(Array.isArray(props.sx) ? props.sx : [props.sx]),
       ]}
